baacca5ebeb80c89137f13f1e68c9fd3286e02b7
Matthew Lohbihler <ml@serotoninsoftware.com>
2012-11-05 11:00:12 -0500

    Added handler interceptor definitions for modules.

---
+++ Core/src/com/serotonin/m2m2/module/HandlerInterceptorDefinition.java
@@ -0,0 +1,23 @@
+/*
+    Copyright (C) 2006-2011 Serotonin Software Technologies Inc. All rights reserved.
+    @author Matthew Lohbihler
+ */
+package com.serotonin.m2m2.module;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * A handler interceptor definition intercepts the processing of a page at multiple points - pre-handle, post-handle,
+ * and after completion - allowing arbitrary treatment of the request at those time.
+ * 
+ * @author Matthew Lohbihler
+ */
+abstract public class HandlerInterceptorDefinition extends ModuleElementDefinition {
+    /**
+     * An instance of the handler interceptor. Called once upon startup, so the instance must be reusable and thread
+     * safe. Cannot be null.
+     * 
+     * @return an instance of the handler interceptor
+     */
+    abstract public HandlerInterceptor getInterceptor();
+}
--- Core/src/com/serotonin/m2m2/web/mvc/BlabberUrlHandlerMapping.java
+++ Core/src/com/serotonin/m2m2/web/mvc/BlabberUrlHandlerMapping.java
@@ -5,11 +5,21 @@
 package com.serotonin.m2m2.web.mvc;
 
 import org.springframework.beans.BeansException;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
 public class BlabberUrlHandlerMapping extends SimpleUrlHandlerMapping {
     @Override
     public void registerHandler(String urlPath, Object handler) throws BeansException, IllegalStateException {
         super.registerHandler(urlPath, handler);
+    }
+
+    public void addInterceptor(HandlerInterceptor interceptor) {
+        setInterceptors(new Object[] { interceptor });
+    }
+
+    @Override
+    public void initInterceptors() {
+        super.initInterceptors();
     }
 }
