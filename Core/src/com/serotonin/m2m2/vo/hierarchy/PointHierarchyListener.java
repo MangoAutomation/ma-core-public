9b7fc4a1ac5b8829c26303e60ffb9c913caef98c
Matthew Lohbihler <ml@serotoninsoftware.com>
2013-02-25 22:06:14 -0500

    Added notifier method for point hierarchy cleared.

--- Core/src/com/serotonin/m2m2/vo/hierarchy/PointHierarchyListener.java
+++ Core/src/com/serotonin/m2m2/vo/hierarchy/PointHierarchyListener.java
@@ -4,6 +4,20 @@
  */
 package com.serotonin.m2m2.vo.hierarchy;
 
-public interface PointHierarchyListener {
-    void pointHierarchySaved(PointFolder root);
+abstract public class PointHierarchyListener {
+    /**
+     * The point hierarchy is cleared whenever a point is saved. Note that this may not mean the hierarchy has actually
+     * changed.
+     */
+    public void pointHierarchyCleared() {
+        // Override as required
+    }
+
+    /**
+     * 
+     * @param root
+     */
+    public void pointHierarchySaved(PointFolder root) {
+        // Override as required
+    }
 }
