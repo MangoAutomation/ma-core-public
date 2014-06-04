fb210f49e805b27ff602e2a7f53912e2acfc192a
Matthew Lohbihler <ml@serotoninsoftware.com>
2014-06-04 18:08:21 -0400

    Modified the data source edit context ability for the new data source editing interface.

--- Core/src/com/serotonin/m2m2/web/mvc/controller/DataSourcePropertiesController.java
+++ Core/src/com/serotonin/m2m2/web/mvc/controller/DataSourcePropertiesController.java
@@ -42,9 +42,9 @@ import com.serotonin.m2m2.vo.permission.Permissions;
  * 
  * 
  * @author Terry Packer
- *
+ * 
  */
-public class DataSourcePropertiesController extends ParameterizableViewController{
+public class DataSourcePropertiesController extends ParameterizableViewController {
     private String errorViewName;
 
     public void setErrorViewName(String errorViewName) {
@@ -57,7 +57,7 @@ public class DataSourcePropertiesController extends ParameterizableViewControlle
         DataSourceVO<?> dataSourceVO = null;
         User user = Common.getUser(request);
         // Create the model.
-        Map<String, Object> model = new HashMap<String, Object>();
+        Map<String, Object> model = new HashMap<>();
 
         // Get the id.
         int id = Common.NEW_ID;
@@ -66,119 +66,118 @@ public class DataSourcePropertiesController extends ParameterizableViewControlle
         String pidStr = request.getParameter("dpid");
         //Get the Type
         String type = request.getParameter("typeId");
-        
+
         //For legacy use of PID from the point details page
-        if(request.getParameter("pid")!=null)
-        	pidStr = request.getParameter("pid");
-        
+        if (request.getParameter("pid") != null)
+            pidStr = request.getParameter("pid");
+
         //If type and dsid and pid are null then don't perform any actions here
-        if((idStr == null)&&(pidStr == null)&&(type == null)){
+        if ((idStr == null) && (pidStr == null) && (type == null)) {
             return new ModelAndView(getViewName(), model);
         }
-        
-      //Is this a new datasource?
-        if(type != null){
-	        if(StringUtils.isBlank(type)){
-	        	model.put("key", "dsEdit.error.noTypeProvided");
-	            return new ModelAndView(new RedirectView(errorViewName), model);
-	        }else{
-	        	//Prepare the DS        
-	        	Permissions.ensureAdmin(user);
-	
-	            // A new data source
-	            DataSourceDefinition def = ModuleRegistry.getDataSourceDefinition(type);
-	            if (def == null)
-	                return new ModelAndView(new RedirectView(errorViewName));
-	            dataSourceVO = def.baseCreateDataSourceVO();
-	            dataSourceVO.setId(Common.NEW_ID);
-	            dataSourceVO.setXid(new DataSourceDao().generateUniqueXid());
-	        }
+
+        //Is this a new datasource?
+        if (type != null) {
+            if (StringUtils.isBlank(type)) {
+                model.put("key", "dsEdit.error.noTypeProvided");
+                return new ModelAndView(new RedirectView(errorViewName), model);
+            }
+
+            //Prepare the DS        
+            Permissions.ensureAdmin(user);
+
+            // A new data source
+            DataSourceDefinition def = ModuleRegistry.getDataSourceDefinition(type);
+            if (def == null)
+                return new ModelAndView(new RedirectView(errorViewName));
+            dataSourceVO = def.baseCreateDataSourceVO();
+            dataSourceVO.setId(Common.NEW_ID);
+            dataSourceVO.setXid(new DataSourceDao().generateUniqueXid());
         }
-        
+
         //Are we going to be making a copy?
         String copyStr = request.getParameter("copy");
-        
-        //Are we editing a point?
-        if(pidStr != null){
-        	int pid = Integer.parseInt(pidStr);
-            DataPointVO dp = new DataPointDao().getDataPoint(pid);
-            if (dp == null){
-                // The requested data point doesn't exist. Return to the list page.
-            	model.put("key", "dsEdit.error.pointDNE");
-            	model.put("params", pid);
-                return new ModelAndView(new RedirectView(errorViewName),model);
-            }else{
-            	
-                id = dp.getDataSourceId();
-                
-                //Now check to see if we are making a copy of this
-                
-                if(copyStr != null){
-                	Boolean copy = Boolean.parseBoolean(copyStr);
-                	model.put("copy", copy);
-                	if(copy){
-                        String name = StringUtils.abbreviate(
-                                TranslatableMessage.translate(Common.getTranslations(), "common.copyPrefix", dp.getName()), 40);
 
-                        //Setup the Copy
-                        DataPointVO copyDp = dp.copy();
-                        copyDp.setId(Common.NEW_ID);
-                        copyDp.setName(name);
-                        copyDp.setXid(DataPointDao.instance.generateUniqueXid());
-                        model.put("dataPoint", copyDp);
-                	}
-                	
-                }else
-                	model.put("dataPoint", dp);
+        //Are we editing a point?
+        if (pidStr != null) {
+            int pid = Integer.parseInt(pidStr);
+            DataPointVO dp = new DataPointDao().getDataPoint(pid);
+            if (dp == null) {
+                // The requested data point doesn't exist. Return to the list page.
+                model.put("key", "dsEdit.error.pointDNE");
+                model.put("params", pid);
+                return new ModelAndView(new RedirectView(errorViewName), model);
             }
+
+            id = dp.getDataSourceId();
+
+            //Now check to see if we are making a copy of this
+
+            if (copyStr != null) {
+                Boolean copy = Boolean.parseBoolean(copyStr);
+                model.put("copy", copy);
+                if (copy) {
+                    String name = StringUtils.abbreviate(
+                            TranslatableMessage.translate(Common.getTranslations(), "common.copyPrefix", dp.getName()),
+                            40);
+
+                    //Setup the Copy
+                    DataPointVO copyDp = dp.copy();
+                    copyDp.setId(Common.NEW_ID);
+                    copyDp.setName(name);
+                    copyDp.setXid(DataPointDao.instance.generateUniqueXid());
+                    model.put("dataPoint", copyDp);
+                }
+
+            }
+            else
+                model.put("dataPoint", dp);
         }
-        
-        if(idStr != null){
-        	 // An existing configuration or copy
+
+        if (idStr != null) {
+            // An existing configuration or copy
             id = Integer.parseInt(idStr);
-           
-            
+
         }
-        
-        
+
         if (id != Common.NEW_ID) {
             dataSourceVO = Common.runtimeManager.getDataSource(id);
-            
-            if(copyStr != null){
-            	Boolean copy = Boolean.parseBoolean(copyStr);
-            	model.put("copy", copy);
-            	if(copy){
+
+            if (copyStr != null) {
+                Boolean copy = Boolean.parseBoolean(copyStr);
+                model.put("copy", copy);
+                if (copy) {
                     String name = StringUtils.abbreviate(
-                            TranslatableMessage.translate(Common.getTranslations(), "common.copyPrefix", dataSourceVO.getName()), 40);
+                            TranslatableMessage.translate(Common.getTranslations(), "common.copyPrefix",
+                                    dataSourceVO.getName()), 40);
 
                     //Setup the Copy
                     dataSourceVO = dataSourceVO.copy();
                     dataSourceVO.setId(Common.NEW_ID);
                     dataSourceVO.setName(name);
                     dataSourceVO.setXid(DataSourceDao.instance.generateUniqueXid());
-            	}
+                }
             }
 
-            
-            if (dataSourceVO == null){
+            if (dataSourceVO == null) {
                 // The requested data source doesn't exist. Return to the list page.
-            	model.put("key", "dsEdit.error.dataSourceDNE");
-            	model.put("params", id);
+                model.put("key", "dsEdit.error.dataSourceDNE");
+                model.put("params", id);
                 return new ModelAndView(new RedirectView(errorViewName), model);
 
             }
             Permissions.ensureDataSourcePermission(user, id);
         }
-        
 
-        
-        
         // Set the id of the data source in the user object for the DWR.
         user.setEditDataSource(dataSourceVO);
 
         // The data source
-        model.put("dataSource", dataSourceVO);
-        model.put("modulePath", dataSourceVO.getDefinition().getModule().getWebPath());
+        if (dataSourceVO != null) {
+            model.put("dataSource", dataSourceVO);
+            model.put("modulePath", dataSourceVO.getDefinition().getModule().getWebPath());
+            dataSourceVO.addEditContext(model);
+        }
 
         // Reference data
         try {
@@ -189,8 +188,8 @@ public class DataSourcePropertiesController extends ParameterizableViewControlle
         }
 
         List<DataPointVO> allPoints = new DataPointDao().getDataPoints(DataPointExtendedNameComparator.instance, false);
-        List<DataPointVO> userPoints = new LinkedList<DataPointVO>();
-        List<DataPointVO> analogPoints = new LinkedList<DataPointVO>();
+        List<DataPointVO> userPoints = new LinkedList<>();
+        List<DataPointVO> analogPoints = new LinkedList<>();
         for (DataPointVO dp : allPoints) {
             if (Permissions.hasDataPointReadPermission(user, dp)) {
                 userPoints.add(dp);
