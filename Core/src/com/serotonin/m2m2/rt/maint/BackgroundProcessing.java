0d69d3db32ee2ca29b3086c53404483b62d6df25
Matthew Lohbihler <ml@serotoninsoftware.com>
2015-02-03 22:34:47 -0500

    Allow access to medium and low priority queues

--- Core/src/com/serotonin/m2m2/rt/maint/BackgroundProcessing.java
+++ Core/src/com/serotonin/m2m2/rt/maint/BackgroundProcessing.java
@@ -4,8 +4,9 @@
  */
 package com.serotonin.m2m2.rt.maint;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -27,7 +28,7 @@ public class BackgroundProcessing implements ILifecycle {
     final Log log = LogFactory.getLog(BackgroundProcessing.class);
 
     private ThreadPoolExecutor mediumPriorityService;
-    private ExecutorService lowPriorityService;
+    private ThreadPoolExecutor lowPriorityService;
 
     public void addWorkItem(final WorkItem item) {
         Runnable runnable = new Runnable() {
@@ -51,13 +52,35 @@ public class BackgroundProcessing implements ILifecycle {
             Common.timer.execute(runnable);
         else if (item.getPriority() == WorkItem.PRIORITY_MEDIUM)
             mediumPriorityService.execute(runnable);
-        else {
+        else
             lowPriorityService.execute(runnable);
-        }
     }
 
     public int getMediumPriorityServiceQueueSize() {
         return mediumPriorityService.getQueue().size();
+    }
+
+    public Map<String, Integer> getMediumPriorityServiceQueueClassCounts() {
+        return getClassCounts(mediumPriorityService);
+    }
+
+    public Map<String, Integer> getLowPriorityServiceQueueClassCounts() {
+        return getClassCounts(lowPriorityService);
+    }
+
+    private Map<String, Integer> getClassCounts(ThreadPoolExecutor e) {
+        Map<String, Integer> classCounts = new HashMap<>();
+        Iterator<Runnable> iter = e.getQueue().iterator();
+        while (iter.hasNext()) {
+            Runnable r = iter.next();
+            String s = r.getClass().getName();
+            Integer count = classCounts.get(s);
+            if (count == null)
+                count = 0;
+            count++;
+            classCounts.put(s, count);
+        }
+        return classCounts;
     }
 
     @Override
@@ -65,7 +88,8 @@ public class BackgroundProcessing implements ILifecycle {
         mediumPriorityService = new ThreadPoolExecutor(3, 30, 60L, TimeUnit.SECONDS,
                 new LinkedBlockingQueue<Runnable>());
         mediumPriorityService.allowCoreThreadTimeOut(true);
-        lowPriorityService = Executors.newSingleThreadExecutor();
+        lowPriorityService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
     }
 
     @Override
