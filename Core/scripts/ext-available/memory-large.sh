fb400a5aa47144864d82f83a060205cf9cadc12a
terrypacker <username@users.noreply.github.com>
2015-06-08 13:58:25 -1000

    Adding memory and garbage collection configurations as Startup Extensions.

---
+++ Core/scripts/ext-available/memory-large.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+case "$1" in
+    init)
+        # Startup with Java Memory setup for Large installation
+        JAVAOPTS=' -Xms10g -Xmx10g'
+        ;;
+esac
---
+++ Core/scripts/ext-available/memory-medium.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+case "$1" in
+    init)
+        # Startup with Java Memory setup for Medium size installation
+        # The heap is set to non-expanding for increased performance.  If memory use is a factor
+        # set the Minimum heap size to a lower number and let the JVM adjust when necessary.
+        JAVAOPTS=' -Xms5g -Xmx5g'
+        ;;
+esac
