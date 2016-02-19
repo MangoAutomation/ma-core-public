f03d1d70b65f3596feb052db4eec21dc9714d1b8
Phillip Dunlap <phillip@infiniteautomation.com>
2016-02-19 15:25:34 -0700

    Fixing delete lib path in upgrade script

--- Core/scripts/upgrade.sh
+++ Core/scripts/upgrade.sh
@@ -7,7 +7,7 @@
 # Upgrades Mango Automation core.
 
 # Delete jars and work dir
-rm -f"$MA_HOME"/lib/*.jar
+rm -f "$MA_HOME"/lib/*.jar
 rm -Rf "$MA_HOME"/work
 
 # Unzip core. The exact name is unknown, but there should only be one, so iterate
