065491cde270aa1112d69dffae9131552e8a28db
terrypacker <username@users.noreply.github.com>
2016-02-29 10:18:39 -1000

    Changing Core Version to 2.7.6 Upgrading POMs for Mango 2.7.6

--- Core/src/com/serotonin/m2m2/Common.java
+++ Core/src/com/serotonin/m2m2/Common.java
@@ -149,7 +149,7 @@ public class Common {
     }
 
     public static final int getMicroVersion() {
-        return 5;
+        return 6;
     }
 
     public static final int getDatabaseSchemaVersion() {
