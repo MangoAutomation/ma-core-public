83be93fc66cd0a1cad64d4834e9b2e6b83ec3dd0
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-04-18 12:47:17 +1000

    Use BCrypt password hashing
    # Conflicts - Manually Merged:
    #	Core/src/com/serotonin/m2m2/db/upgrade/Upgrade12.java

--- Core/src/com/serotonin/m2m2/db/upgrade/Upgrade12.java
+++ Core/src/com/serotonin/m2m2/db/upgrade/Upgrade12.java
@@ -7,6 +7,7 @@ package com.serotonin.m2m2.db.upgrade;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.db.DatabaseProxy;
 /**
  * Upgrade to add template system
@@ -16,13 +17,65 @@ import com.serotonin.m2m2.db.DatabaseProxy;
  */
 public class Upgrade12 extends DBUpgrade {
 
+
     @Override
     public void upgrade() throws Exception {
+    	// get hash algorithm using the old default
+    	String hashAlgorithm = Common.envProps.getString("security.hashAlgorithm", "SHA-1");
+
+    	String[] mysqlScript = {
+	        "ALTER TABLE users MODIFY password VARCHAR(255) NOT NULL;",
+	        "UPDATE users SET password  = CONCAT('{" + hashAlgorithm + "}', password);",
+	   
+	    	"ALTER TABLE dataPoints ADD INDEX nameIndex (name ASC);",
+	    	"ALTER TABLE dataPoints ADD INDEX deviceNameIndex (deviceName ASC);",
+	    	"ALTER TABLE dataPoints ADD INDEX pointFolderIndex (pointFolderId ASC);",
+	    	"ALTER TABLE dataPoints ADD INDEX dataSourceIndex (dataSourceId ASC);",
+	    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
+	        "UPDATE jsonData SET publicData='N';",
+	
+	    };
+	    String[] derbyScript = {
+	        "ALTER TABLE users ALTER COLUMN password SET DATA TYPE VARCHAR(255);",
+	        "UPDATE users SET password  = '{" + hashAlgorithm + "}' || password;",
+	    
+	   		"CREATE INDEX nameIndex on dataPoints (name ASC);",
+	   		"CREATE INDEX deviceNameIndex on dataPoints (deviceName ASC);",
+	   		"CREATE INDEX pointFolderIndex on dataPoints (pointFolderId ASC);",
+	   		"CREATE INDEX dataSourceIndex on dataPoints (dataSourceId ASC);",
+	    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
+	        "UPDATE jsonData SET publicData='N';",
+	    };    
+	
+	    String[] h2Script = {
+	        "ALTER TABLE users ALTER COLUMN password VARCHAR(255) NOT NULL;",
+	        "UPDATE users SET password  = CONCAT('{" + hashAlgorithm + "}', password);",
+	
+	    	"CREATE INDEX nameIndex on dataPoints (`name` ASC);",
+	    	"CREATE INDEX deviceNameIndex on dataPoints (`deviceName` ASC);",
+	    	"CREATE INDEX pointFolderIndex on dataPoints (`pointFolderId` ASC);",
+	    	"CREATE INDEX dataSourceIndex on dataPoints (`dataSourceId` ASC);", 
+	    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
+	        "UPDATE jsonData SET publicData='N';",
+	    };
+	    
+	   	String[] mssqlScript = {
+	        "ALTER TABLE users ALTER COLUMN password nvarchar(255) NOT NULL;",
+	        "UPDATE users SET password  = CONCAT('{" + hashAlgorithm + "}', password);",
+	        
+	   		"CREATE INDEX nameIndex on dataPoints (name ASC);",
+	   		"CREATE INDEX deviceNameIndex on dataPoints (deviceName ASC);",
+	   		"CREATE INDEX pointFolderIndex on dataPoints (pointFolderId ASC);",
+	   		"CREATE INDEX dataSourceIndex on dataPoints (dataSourceId ASC);",
+	    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
+	        "UPDATE jsonData SET publicData='N';",    
+	    };
+    
         // Run the script.
         Map<String, String[]> scripts = new HashMap<>();
         scripts.put(DatabaseProxy.DatabaseType.DERBY.name(), derbyScript);
         scripts.put(DatabaseProxy.DatabaseType.MYSQL.name(), mysqlScript);
-        scripts.put(DatabaseProxy.DatabaseType.MSSQL.name(), derbyScript);
+        scripts.put(DatabaseProxy.DatabaseType.MSSQL.name(), mssqlScript);
         scripts.put(DatabaseProxy.DatabaseType.H2.name(), h2Script);
         scripts.put(DatabaseProxy.DatabaseType.POSTGRES.name(), mysqlScript);
         runScript(scripts);
@@ -33,32 +86,4 @@ public class Upgrade12 extends DBUpgrade {
     protected String getNewSchemaVersion() {
         return "13";
     }
-
-    private final String[] mysqlScript = {
-    	"ALTER TABLE dataPoints ADD INDEX nameIndex (name ASC);",
-    	"ALTER TABLE dataPoints ADD INDEX deviceNameIndex (deviceName ASC);",
-    	"ALTER TABLE dataPoints ADD INDEX pointFolderIndex (pointFolderId ASC);",
-    	"ALTER TABLE dataPoints ADD INDEX dataSourceIndex (dataSourceId ASC);",
-    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
-        "UPDATE jsonData SET publicData='N';",
-
-    };
-    private final String[] derbyScript = {
-   		"CREATE INDEX nameIndex on dataPoints (name ASC);",
-   		"CREATE INDEX deviceNameIndex on dataPoints (deviceName ASC);",
-   		"CREATE INDEX pointFolderIndex on dataPoints (pointFolderId ASC);",
-   		"CREATE INDEX dataSourceIndex on dataPoints (dataSourceId ASC);",
-    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
-        "UPDATE jsonData SET publicData='N';",
-    };    
-
-    private final String[] h2Script = {
-    	"CREATE INDEX nameIndex on dataPoints (`name` ASC);",
-    	"CREATE INDEX deviceNameIndex on dataPoints (`deviceName` ASC);",
-    	"CREATE INDEX pointFolderIndex on dataPoints (`pointFolderId` ASC);",
-    	"CREATE INDEX dataSourceIndex on dataPoints (`dataSourceId` ASC);", 
-    	"ALTER TABLE jsonData ADD COLUMN publicData char(1);",
-        "UPDATE jsonData SET publicData='N';",
-    };
-
 }
