7a0fe158e1a5af2d640ccdc9066e2d9d590eeb37
terrypacker <username@users.noreply.github.com>
2016-04-19 17:12:55 -1000

    Adding missing files from EventHandler modularization commit.

---
+++ Core/src/com/serotonin/m2m2/module/definitions/EmailEventHandlerDefinition.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2016 Infinite Automation Software. All rights reserved.
+ * @author Terry Packer
+ */
+package com.serotonin.m2m2.module.definitions;
+
+import com.serotonin.m2m2.module.EventHandlerDefinition;
+import com.serotonin.m2m2.vo.event.AbstractEventHandlerVO;
+import com.serotonin.m2m2.vo.event.EmailEventHandlerVO;
+
+/**
+ * @author Terry Packer
+ *
+ */
+public class EmailEventHandlerDefinition extends EventHandlerDefinition{
+
+	public static final String TYPE_NAME = "EMAIL";
+	public static final String DESC_KEY = "eventHandlers.type.email";
+	
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.EventHandlerDefinition#getEventHandlerTypeName()
+	 */
+	@Override
+	public String getEventHandlerTypeName() {
+		return TYPE_NAME;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.EventHandlerDefinition#getDescriptionKey()
+	 */
+	@Override
+	public String getDescriptionKey() {
+		return DESC_KEY;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.EventHandlerDefinition#createEventHandlerVO()
+	 */
+	@Override
+	protected AbstractEventHandlerVO createEventHandlerVO() {
+		return new EmailEventHandlerVO();
+	}
+
+}
