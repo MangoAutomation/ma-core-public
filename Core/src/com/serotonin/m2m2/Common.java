a9975f5d5417261fcf8cc1fc2580abb98025dff1
terrypacker <username@users.noreply.github.com>
2016-09-19 14:22:55 -0500

    Bumping core to 2.7.13 for development testing.

--- Core/src/com/serotonin/m2m2/Common.java
+++ Core/src/com/serotonin/m2m2/Common.java
@@ -149,7 +149,7 @@ public class Common {
     }
 
     public static final int getMicroVersion() {
-        return 12;
+        return 13;
     }
 
     public static final int getDatabaseSchemaVersion() {
