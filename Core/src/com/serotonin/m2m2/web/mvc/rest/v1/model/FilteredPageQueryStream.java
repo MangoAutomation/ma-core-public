320de891f416285126f7e91bf190f3b7ef4b67ba
terrypacker <username@users.noreply.github.com>
2016-10-12 14:56:09 -0500

    Reverting Admin no limit in Filtered Query Stream

--- Core/src/com/serotonin/m2m2/web/mvc/rest/v1/model/FilteredPageQueryStream.java
+++ Core/src/com/serotonin/m2m2/web/mvc/rest/v1/model/FilteredPageQueryStream.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.serotonin.m2m2.db.dao.AbstractBasicDao;
-import com.serotonin.m2m2.vo.User;
 import com.serotonin.m2m2.web.mvc.rest.v1.MangoVoRestController;
 import com.serotonin.m2m2.web.mvc.rest.v1.csv.CSVPojoWriter;
 
@@ -21,7 +20,6 @@ import net.jazdw.rql.parser.ASTNode;
  */
 public class FilteredPageQueryStream<VO, MODEL, DAO extends AbstractBasicDao<VO>> extends QueryStream<VO, MODEL, DAO> implements QueryDataPageStream<VO>{
 
-	protected User user;
 	protected CountQueryStreamCallback countCallback;
 	
 	/**
@@ -30,10 +28,9 @@ public class FilteredPageQueryStream<VO, MODEL, DAO extends AbstractBasicDao<VO>
 	 * @param node
 	 * @param queryCallback
 	 */
-	public FilteredPageQueryStream(DAO dao, MangoVoRestController<VO, MODEL, DAO> controller, User user, ASTNode node,
+	public FilteredPageQueryStream(DAO dao, MangoVoRestController<VO, MODEL, DAO> controller, ASTNode node,
 			FilteredQueryStreamCallback<VO> queryCallback) {
 		super(dao, controller, node, queryCallback);
-		this.user = user;
 		this.countCallback = new CountQueryStreamCallback();
 	}
 
@@ -42,7 +39,7 @@ public class FilteredPageQueryStream<VO, MODEL, DAO extends AbstractBasicDao<VO>
 	 */
 	@Override
 	public void setupQuery(){
-		this.results = this.dao.createQuery(root, queryCallback, countCallback, controller.getModelMap(), controller.getAppenders(), user.isAdmin());
+		this.results = this.dao.createQuery(root, queryCallback, countCallback, controller.getModelMap(), controller.getAppenders(), false);
 		List<Object> args = this.results.getLimitOffsetArgs();
 		if((args != null)&&(args.size() > 0)){
 			FilteredQueryStreamCallback<VO> callback = (FilteredQueryStreamCallback<VO>)this.queryCallback;
