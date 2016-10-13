f706fe4af19d006b91a546407df1e5fe67a4f03e
terrypacker <username@users.noreply.github.com>
2016-10-13 15:39:19 -0500

    Adding better logging to rejected tasks via reflection. (Until 2.8)

--- Core/src/com/serotonin/m2m2/util/timeout/RejectedTaskLogger.java
+++ Core/src/com/serotonin/m2m2/util/timeout/RejectedTaskLogger.java
@@ -4,6 +4,7 @@
  */
 package com.serotonin.m2m2.util.timeout;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.RejectedExecutionException;
 
 import org.apache.commons.logging.Log;
@@ -26,9 +27,26 @@ public class RejectedTaskLogger implements RejectedTaskHandler{
 	@Override
 	public void rejected(RejectedTaskReason reason){
 		
-		RejectedExecutionException exception = new RejectedExecutionException("Task " + reason.getTask().toString() +
-                " rejected from " +
-                reason.getExecutor().toString());
+		String taskName;
+		if(reason.getTask() instanceof TimeoutTask){
+			try {
+				TimeoutTask task = (TimeoutTask)reason.getTask();
+				Field f;
+				f = task.getClass().getDeclaredField("client");
+				f.setAccessible(true);
+				Class<?> c = f.get(task).getClass();
+				taskName = c.getName();
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				taskName = reason.getTask().toString();
+			}
+
+		}else{
+			taskName = reason.getTask().toString();
+		} //TODO Other types?
+		
+		
+		RejectedExecutionException exception = new RejectedExecutionException("Task " + taskName +
+                " rejected from " + reason.getExecutor().toString());
 		LOG.fatal(exception.getMessage(), exception);
 	}
 	
