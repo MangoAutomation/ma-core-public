5465830a037f6b33df40f317eae4f1fe2226b006
Phillip Dunlap <phillip@infiniteautomation.com>
2017-02-07 15:39:44 -0700

    Adding call to transform values for multiply+add situations

--- Core/src/com/serotonin/m2m2/rt/dataSource/DataSourceRT.java
+++ Core/src/com/serotonin/m2m2/rt/dataSource/DataSourceRT.java
@@ -117,13 +117,20 @@ abstract public class DataSourceRT<VO extends DataSourceVO<?>> extends AbstractR
     }
     
     public void setPointValue(DataPointRT dataPoint, PointValueTime valueTime, SetPointSource source) {
-    	if(dataPoint.getVO().isPreventSetExtremeValues() && (valueTime.getDoubleValue() > dataPoint.getVO().getSetExtremeHighLimit() 
-    			|| valueTime.getDoubleValue() < dataPoint.getVO().getSetExtremeLowLimit()))
+    	if(dataPoint.getVO().isPreventSetExtremeValues()) {
+    		double transformedValue = transformOutboundValue(dataPoint, valueTime);
+    		if(transformedValue > dataPoint.getVO().getSetExtremeHighLimit() 
+    			|| transformedValue < dataPoint.getVO().getSetExtremeLowLimit())
     		return;
+    	}
     	setPointValueImpl(dataPoint, valueTime, source);
     }
 
     abstract public void setPointValueImpl(DataPointRT dataPoint, PointValueTime valueTime, SetPointSource source);
+    
+    protected double transformOutboundValue(DataPointRT dataPoint, PointValueTime valueTime) {
+    	return valueTime.getDoubleValue();
+    }
 
     public void relinquish(DataPointRT dataPoint) {
         throw new ShouldNeverHappenException("not implemented in " + getClass());
