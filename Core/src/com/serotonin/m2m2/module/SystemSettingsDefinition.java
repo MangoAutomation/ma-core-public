d850fe6e945971c5e2be89606b6b3be9d1756fa3
Phillip Dunlap <phillip@infiniteautomation.com>
2017-04-03 13:17:32 -0600

    Adding methods to make fixing validation easier.

--- Core/src/com/serotonin/m2m2/module/SystemSettingsDefinition.java
+++ Core/src/com/serotonin/m2m2/module/SystemSettingsDefinition.java
@@ -15,6 +15,29 @@ import com.serotonin.m2m2.i18n.ProcessResult;
  * @author Matthew Lohbihler
  */
 abstract public class SystemSettingsDefinition extends ModuleElementDefinition {
+    public static Integer getIntegerValue(Object value) {
+    	if(value == null)
+    		return null;
+    	if(value instanceof Number)
+    		return ((Number)value).intValue();
+    	if(value instanceof String)
+    		return Integer.parseInt((String)value);
+    	if(value instanceof Integer)
+    		return (Integer)value;
+    	return null;
+    }
+    
+    public static Double getDoubleValue(Object value) {
+    	if(value == null)
+    		return null;
+    	if(value instanceof Number)
+    		return ((Number)value).doubleValue();
+    	if(value instanceof String)
+    		return Double.parseDouble((String)value);
+    	if(value instanceof Double)
+    		return (Double)value;
+    	return null;
+    }
     /**
      * The reference key to the description used as the section header.
      * 
