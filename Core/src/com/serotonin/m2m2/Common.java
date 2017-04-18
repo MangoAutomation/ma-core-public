977ff91e822264983031e47dfb1681dc4b1ca1d2
terrypacker <username@users.noreply.github.com>
2017-04-19 09:59:02 +1000

    Bumping version to 2.8.8

--- Core/src/com/serotonin/m2m2/Common.java
+++ Core/src/com/serotonin/m2m2/Common.java
@@ -148,7 +148,7 @@ public class Common {
     }
 
     public static final int getMicroVersion() {
-        return 6;
+        return 8;
     }
 
     public static final int getDatabaseSchemaVersion() {
