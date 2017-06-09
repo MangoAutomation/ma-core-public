4c862c123a8c87af4549fbd58b47af8cf6e85782
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-06-09 15:00:54 -0600

    Add a public file store

---
+++ Core/src/com/serotonin/m2m2/module/definitions/permissions/PublicFileStoreWritePermissionDefinition.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2017 Infinite Automation Software. All rights reserved.
+ *
+ */
+package com.serotonin.m2m2.module.definitions.permissions;
+
+import com.serotonin.m2m2.module.PermissionDefinition;
+
+/**
+ * 
+ * @author Jared Wiltshire
+ */
+public class PublicFileStoreWritePermissionDefinition extends PermissionDefinition{
+
+	public static final String TYPE_NAME = "filestore.public.write";
+	
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionKey()
+	 */
+	@Override
+	public String getPermissionKey() {
+		return "filestore.public.permission.write";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionTypeName()
+	 */
+	@Override
+	public String getPermissionTypeName() {
+		return TYPE_NAME;
+	}
+
+}
