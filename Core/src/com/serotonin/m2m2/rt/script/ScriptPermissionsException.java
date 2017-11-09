b8dce38733c983682a5eece2c65ddb6893e91384
Phillip Dunlap <phillip@infiniteautomation.com>
2017-11-09 12:54:37 -0700

    Override getMessage in script permission exception

--- Core/src/com/serotonin/m2m2/rt/script/ScriptPermissionsException.java
+++ Core/src/com/serotonin/m2m2/rt/script/ScriptPermissionsException.java
@@ -1,5 +1,6 @@
 package com.serotonin.m2m2.rt.script;
 
+import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
 
 public class ScriptPermissionsException extends RuntimeException {
@@ -10,5 +11,12 @@ public class ScriptPermissionsException extends RuntimeException {
 	}
 	public TranslatableMessage getTranslatableMessage() {
 		return tm;
+	}
+	
+	@Override
+	public String getMessage() {
+	    if(tm != null)
+	        return tm.translate(Common.getTranslations());
+	    return null;
 	}
 }
