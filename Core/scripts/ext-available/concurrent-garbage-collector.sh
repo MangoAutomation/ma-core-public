96a37ed2ac2008bbe4f8bf4de8f6f4f6f0626e2d
Phillip Dunlap <phillip@infiniteautomation.com>
2018-01-24 15:07:50 -0700

    Script typo

--- Core/scripts/ext-available/concurrent-garbage-collector.sh
+++ Core/scripts/ext-available/concurrent-garbage-collector.sh
@@ -3,7 +3,7 @@ case "$1" in
     init)
         # Concurrent Garbage Collection
         # Medium to large size data sets and multi cpu systems where response time is more important
-        # than overall throughput.  The techniques used to imimize pauses can reduce application
+        # than overall throughput.  The techniques used to minimize pauses can reduce application
         # performance.
         JAVAOPTS=$JAVAOPTS' -XX:+UseConcMarkSweepGC'
         # Print the details from the collector's runs
