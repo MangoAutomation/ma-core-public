9ecddba1036a60603e0b75f03370fc6aa14b02d9
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-04-13 14:23:45 -0600

    Add new cache option for resource files with v=xxx query parameters

--- Core/src/com/serotonin/m2m2/web/filter/MangoShallowEtagHeaderFilter.java
+++ Core/src/com/serotonin/m2m2/web/filter/MangoShallowEtagHeaderFilter.java
@@ -5,6 +5,7 @@
 package com.serotonin.m2m2.web.filter;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -29,54 +30,63 @@ public class MangoShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
     public static final String MAX_AGE_TEMPLATE = "max-age=%d, must-revalidate";
     public static final String NO_STORE = "no-cache, no-store, max-age=0, must-revalidate";
 
-	final RequestMatcher restMatcher;
+    public static final Pattern VERSION_QUERY_PARAMETER = Pattern.compile("(?:^|&)v=");
+
+    final RequestMatcher restMatcher;
     final RequestMatcher resourcesMatcher;
     final RequestMatcher getMatcher;
-	final String defaultHeader;
+    final String defaultHeader;
     final String restHeader;
     final String resourcesHeader;
-	
-	public MangoShallowEtagHeaderFilter() {
-	    getMatcher = new AntPathRequestMatcher("/**", HttpMethod.GET.name());
-	    restMatcher = new AntPathRequestMatcher("/rest/**", HttpMethod.GET.name());
+    final String versionedResourcesHeader;
 
-	    resourcesMatcher = new OrRequestMatcher(
-	            new AntPathRequestMatcher("/resources/**", HttpMethod.GET.name()),
+    public MangoShallowEtagHeaderFilter() {
+        getMatcher = new AntPathRequestMatcher("/**", HttpMethod.GET.name());
+        restMatcher = new AntPathRequestMatcher("/rest/**", HttpMethod.GET.name());
+
+        resourcesMatcher = new OrRequestMatcher(
+                new AntPathRequestMatcher("/resources/**", HttpMethod.GET.name()),
                 new AntPathRequestMatcher("/modules/*/web/**", HttpMethod.GET.name()),
                 new AntPathRequestMatcher("/images/**", HttpMethod.GET.name()),
                 new AntPathRequestMatcher("/audio/**", HttpMethod.GET.name()),
                 new AntPathRequestMatcher("/dwr/**", HttpMethod.GET.name()));
-	    
-	    boolean noStore = Common.envProps.getBoolean("web.cache.noStore", false);
+
+        boolean noStore = Common.envProps.getBoolean("web.cache.noStore", false);
         boolean restNoStore = Common.envProps.getBoolean("web.cache.noStore.rest", true);
         boolean resourcesNoStore = Common.envProps.getBoolean("web.cache.noStore.resources", false);
-	    
-	    defaultHeader = noStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge", 0L));
+
+        defaultHeader = noStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge", 0L));
         restHeader = restNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.rest", 0L));
         resourcesHeader = resourcesNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.resources", 86400L));
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.springframework.web.filter.ShallowEtagHeaderFilter#doFilterInternal(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
-	 */
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-	    
-	    String header = null;
-	    
-	    if (restMatcher.matches(request)) {
-	        header = restHeader;
-	    } else if (resourcesMatcher.matches(request)) {
-            header = resourcesHeader;
-	    } else if (getMatcher.matches(request)) {
-	        header = defaultHeader;
-	    } else {
-	        header = NO_STORE;
-	    }
-	    
-	    response.addHeader(HttpHeaders.CACHE_CONTROL, header);
-	    
+        versionedResourcesHeader = resourcesNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.versionedResources", 31536000L));
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.web.filter.ShallowEtagHeaderFilter#doFilterInternal(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = null;
+
+        if (restMatcher.matches(request)) {
+            header = restHeader;
+        } else if (resourcesMatcher.matches(request)) {
+            String queryString = request.getQueryString();
+            if (queryString != null && VERSION_QUERY_PARAMETER.matcher(queryString).find()) {
+                header = versionedResourcesHeader;
+            } else {
+                header = resourcesHeader;
+            }
+        } else if (getMatcher.matches(request)) {
+            header = defaultHeader;
+        } else {
+            header = NO_STORE;
+        }
+
+        response.addHeader(HttpHeaders.CACHE_CONTROL, header);
+
         super.doFilterInternal(request, response, filterChain);
-	}
+    }
 }
