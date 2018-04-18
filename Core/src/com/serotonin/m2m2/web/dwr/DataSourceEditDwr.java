469dcc781d80e6171c5213bf5da14207ab903c06
terrypacker <terrypacker@users.noreply.github.com>
2018-04-17 14:54:09 -1000

    Fix for validating copied Data Point XID length to 100 not 50

--- Core/src/com/serotonin/m2m2/web/dwr/DataSourceEditDwr.java
+++ Core/src/com/serotonin/m2m2/web/dwr/DataSourceEditDwr.java
@@ -217,8 +217,8 @@ public class DataSourceEditDwr extends DataSourceListDwr {
             dp.setId(Common.NEW_ID);
             if (StringUtils.isBlank(xid))
                 response.addContextualMessage("xid", "validate.required");
-            else if (StringValidation.isLengthGreaterThan(xid, 50))
-                response.addMessage("xid", new TranslatableMessage("validate.notLongerThan", 50));
+            else if (StringValidation.isLengthGreaterThan(xid, 100))
+                response.addMessage("xid", new TranslatableMessage("validate.notLongerThan", 100));
             else if (!DataPointDao.instance.isXidUnique(xid, id))
                 response.addContextualMessage("xid", "validate.xidUsed");
 
