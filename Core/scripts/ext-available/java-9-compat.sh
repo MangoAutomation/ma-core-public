81c92f6cbe180073ea6c888108cb17e8415559c1
Phillip <puckfist@gmail.com>
2018-04-24 12:35:05 -0600

    Fix ext script comment

--- Core/scripts/ext-available/java-9-compat.sh
+++ Core/scripts/ext-available/java-9-compat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case "$1" in
     init)
-    	# Enable garbageless configuration for high performance logging 
+    	# Add required java modules not inherently loaded in Java 9, comma separated
         JAVAOPTS="$JAVAOPTS --add-modules java.xml.bind"
         ;;
 esac
