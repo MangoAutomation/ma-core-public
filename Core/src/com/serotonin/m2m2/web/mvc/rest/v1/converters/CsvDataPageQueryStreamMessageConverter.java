dcad49b3886161d6339059e3dad19ed5fd64f4fb
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-05-23 12:28:54 -0600

    Fix up converters so they dont try and read

--- Core/src/com/serotonin/m2m2/web/mvc/rest/v1/converters/CsvDataPageQueryStreamMessageConverter.java
+++ Core/src/com/serotonin/m2m2/web/mvc/rest/v1/converters/CsvDataPageQueryStreamMessageConverter.java
@@ -1,70 +1,83 @@
 /**
- * Copyright (C) 2015 Infinite Automation Software. All rights reserved.
- * @author Terry Packer
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
  */
 package com.serotonin.m2m2.web.mvc.rest.v1.converters;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 
+import org.springframework.core.ResolvableType;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
-import au.com.bytecode.opencsv.CSVWriter;
-
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.web.mvc.rest.v1.csv.CSVPojoWriter;
+import com.serotonin.m2m2.web.mvc.rest.v1.model.QueryArrayStream;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.QueryDataPageStream;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 /**
  * @author Terry Packer
- *
+ * @author Jared Wiltshire
  */
-public class CsvDataPageQueryStreamMessageConverter extends AbstractHttpMessageConverter<QueryDataPageStream<?>>
+public class CsvDataPageQueryStreamMessageConverter extends AbstractGenericHttpMessageConverter<QueryDataPageStream<?>>
 {
-	  public static final MediaType MEDIA_TYPE = new MediaType("text", "csv");
-	  private final char separator, quote;
+    public static final MediaType MEDIA_TYPE = new MediaType("text", "csv");
+    private final char separator, quote;
 
-	  public CsvDataPageQueryStreamMessageConverter() {
-	    this(CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER);
-	  }
+    public CsvDataPageQueryStreamMessageConverter() {
+        this(CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER);
+    }
 
-	  public CsvDataPageQueryStreamMessageConverter(char separator, char quote) {
-	    super(MEDIA_TYPE);
-	    this.separator = separator;
-	    this.quote = quote;
-	  }
+    public CsvDataPageQueryStreamMessageConverter(char separator, char quote) {
+        super(MEDIA_TYPE);
+        this.separator = separator;
+        this.quote = quote;
+    }
 
-	  @Override
-	  protected boolean supports(Class<?> clazz) {
-		  return QueryDataPageStream.class.isAssignableFrom(clazz);
-	  }
+    @Override
+    public boolean canRead(Type type, Class<?> contextClass, MediaType mediaType) {
+        return false;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#readInternal(java.lang.Class, org.springframework.http.HttpInputMessage)
-	 */
-	@Override
-	protected QueryDataPageStream<?> readInternal(Class<? extends QueryDataPageStream<?>> clazz,
-			HttpInputMessage inputMessage) throws IOException,
-			HttpMessageNotReadableException {
-		return null; //Not implemented
-	}
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return supports(type) && canWrite(mediaType);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#writeInternal(java.lang.Object, org.springframework.http.HttpOutputMessage)
-	 */
-	@SuppressWarnings({ "rawtypes" })
-	@Override
-	protected void writeInternal(QueryDataPageStream<?> stream, HttpOutputMessage outputMessage)
-			throws IOException, HttpMessageNotWritableException {
-			CSVPojoWriter out = new CSVPojoWriter(new CSVWriter(new OutputStreamWriter(outputMessage.getBody(), Common.UTF8_CS),separator,quote));
-			stream.streamData(out);
-			out.close();
-	}
+    private boolean supports(Type type) {
+        ResolvableType resolvedType = ResolvableType.forType(type);
+        return QueryArrayStream.class.isAssignableFrom(resolvedType.getRawClass());
+    }
 
+    @Override
+    public QueryDataPageStream<?> read(Type type, Class<?> contextClass, HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+
+        return null;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Override
+    protected void writeInternal(QueryDataPageStream<?> stream, Type type, HttpOutputMessage outputMessage)
+            throws IOException, HttpMessageNotWritableException {
+
+        CSVPojoWriter out = new CSVPojoWriter(new CSVWriter(new OutputStreamWriter(outputMessage.getBody(), Common.UTF8_CS),separator,quote));
+        stream.streamData(out);
+        out.close();
+    }
+
+    @Override
+    protected QueryDataPageStream<?> readInternal(Class<? extends QueryDataPageStream<?>> clazz, HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+
+        return null; //Not implemented
+    }
 
 }
--- Core/src/com/serotonin/m2m2/web/mvc/rest/v1/converters/CsvQueryArrayStreamMessageConverter.java
+++ Core/src/com/serotonin/m2m2/web/mvc/rest/v1/converters/CsvQueryArrayStreamMessageConverter.java
@@ -1,72 +1,90 @@
 /**
- * Copyright (C) 2015 Infinite Automation Software. All rights reserved.
- * @author Terry Packer
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
  */
 package com.serotonin.m2m2.web.mvc.rest.v1.converters;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 
+import org.springframework.core.ResolvableType;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-
-import au.com.bytecode.opencsv.CSVWriter;
 
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.web.mvc.rest.v1.csv.CSVPojoWriter;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.QueryArrayStream;
+import com.serotonin.m2m2.web.mvc.rest.v1.model.QueryDataPageStream;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 /**
  * @author Terry Packer
- *
+ * @author Jared Wiltshire
  */
-public class CsvQueryArrayStreamMessageConverter extends AbstractHttpMessageConverter<QueryArrayStream>
+public class CsvQueryArrayStreamMessageConverter extends AbstractGenericHttpMessageConverter<QueryArrayStream<?>>
 {
-	  public static final MediaType MEDIA_TYPE = new MediaType("text", "csv");
-	  private final char separator, quote;
+    public static final MediaType MEDIA_TYPE = new MediaType("text", "csv");
+    private final char separator, quote;
 
-	  public CsvQueryArrayStreamMessageConverter() {
-	    this(CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER);
-	  }
+    public CsvQueryArrayStreamMessageConverter() {
+        this(CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER);
+    }
 
-	  public CsvQueryArrayStreamMessageConverter(char separator, char quote) {
-	    super(MEDIA_TYPE);
-	    this.separator = separator;
-	    this.quote = quote;
-	  }
+    public CsvQueryArrayStreamMessageConverter(char separator, char quote) {
+        super(MEDIA_TYPE);
+        this.separator = separator;
+        this.quote = quote;
+    }
 
-	  @Override
-	  protected boolean supports(Class<?> clazz) {
-		  return QueryArrayStream.class.isAssignableFrom(clazz);
-	  }
+    @Override
+    public boolean canRead(Type type, Class<?> contextClass, MediaType mediaType) {
+        return false;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#readInternal(java.lang.Class, org.springframework.http.HttpInputMessage)
-	 */
-	@SuppressWarnings({ "rawtypes" })
-	@Override
-	protected QueryArrayStream readInternal(Class<? extends QueryArrayStream> clazz,
-			HttpInputMessage inputMessage) throws IOException,
-			HttpMessageNotReadableException {
-		return null; //Not implemented
-	}
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return supports(type) && canWrite(mediaType);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.http.converter.AbstractHttpMessageConverter#writeInternal(java.lang.Object, org.springframework.http.HttpOutputMessage)
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Override
-	protected void writeInternal(QueryArrayStream stream, HttpOutputMessage outputMessage)
-			throws IOException, HttpMessageNotWritableException {
-		
-			CSVPojoWriter out = new CSVPojoWriter(new CSVWriter(new OutputStreamWriter(outputMessage.getBody(), Common.UTF8_CS),separator,quote));
-			stream.streamData(out);
-			out.close();
-	}
+    private boolean supports(Type type) {
+        ResolvableType resolvedType = ResolvableType.forType(type);
 
+        // dont support query page data stream, there's another converter for that that includes count
+        if (QueryDataPageStream.class.isAssignableFrom(resolvedType.getRawClass())) {
+            return false;
+        }
+
+
+        return QueryArrayStream.class.isAssignableFrom(resolvedType.getRawClass());
+    }
+
+    @Override
+    public QueryArrayStream<?> read(Type type, Class<?> contextClass, HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+
+        return null;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Override
+    protected void writeInternal(QueryArrayStream<?> stream, Type type, HttpOutputMessage outputMessage)
+            throws IOException, HttpMessageNotWritableException {
+
+        CSVPojoWriter out = new CSVPojoWriter(new CSVWriter(new OutputStreamWriter(outputMessage.getBody(), Common.UTF8_CS),separator,quote));
+        stream.streamData(out);
+        out.close();
+    }
+
+    @Override
+    protected QueryArrayStream<?> readInternal(Class<? extends QueryArrayStream<?>> clazz, HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+
+        return null; //Not implemented
+    }
 
 }
