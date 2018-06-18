2ab1b80a1b137aadf8c9c23dc2380487eae16e60
Phillip <puckfist@gmail.com>
2018-06-18 12:34:00 -0600

    Fix upgrade21 to use the DAO for user deleting, del relational info #1289

--- Core/src/com/serotonin/m2m2/db/upgrade/Upgrade21.java
+++ Core/src/com/serotonin/m2m2/db/upgrade/Upgrade21.java
@@ -10,13 +10,13 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 
 import com.serotonin.m2m2.db.DatabaseProxy;
+import com.serotonin.m2m2.db.dao.UserDao;
 
 /**
  * @author Jared Wiltshire
@@ -48,27 +48,15 @@ public class Upgrade21 extends DBUpgrade {
                 
             });
             
-    
-            StringBuilder deleteSQL = new StringBuilder("DELETE FROM users WHERE id IN (");
-            MutableInt count = new MutableInt(toRemove.keySet().size());
-            toRemove.keySet().stream().forEach((key) -> {
-                LOG.warn("Removing duplicate user '" + toRemove.get(key) + "' with id " + key);
-                PrintWriter pw = new PrintWriter(out);
-                pw.write("WARN: Removing duplicate user '" + toRemove.get(key) + "' with id " + key + "\n");
-                pw.flush();
-                count.decrement();
-                deleteSQL.append(key);
-                if(count.getValue() > 0)
-                    deleteSQL.append(",");
-            });
-            deleteSQL.append(");");
             Map<String, String[]> scripts = new HashMap<>();
             if(toRemove.keySet().size() > 0) {
-                scripts.put(DatabaseProxy.DatabaseType.MYSQL.name(), new String[] {deleteSQL.toString()});
-                scripts.put(DatabaseProxy.DatabaseType.H2.name(), new String[] {deleteSQL.toString()});
-                scripts.put(DatabaseProxy.DatabaseType.MSSQL.name(), new String[] {deleteSQL.toString()});
-                scripts.put(DatabaseProxy.DatabaseType.POSTGRES.name(), new String[] {deleteSQL.toString()});
-                runScript(scripts, out);
+                toRemove.keySet().stream().forEach((key) -> {
+                    LOG.warn("Removing duplicate user '" + toRemove.get(key) + "' with id " + key);
+                    UserDao.instance.deleteUser(key);
+                    PrintWriter pw = new PrintWriter(out);
+                    pw.write("WARN: Removing duplicate user '" + toRemove.get(key) + "' with id " + key + "\n");
+                    pw.flush();
+                });
             } else {
                 LOG.info("No duplicate users to remove.");
             }
