4951c3f122afae3ae76e141ffa63662097d97548
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-07-31 10:34:31 -0600

    Improve the lazy initialized util

--- Core/src/com/infiniteautomation/mango/util/LazyInitializer.java
+++ Core/src/com/infiniteautomation/mango/util/LazyInitializer.java
@@ -3,12 +3,13 @@
  */
 package com.infiniteautomation.mango.util;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
  * Thread safe lazy initializer that can be reset.
  * Uses double checked locking.
- * 
+ *
  * @author Jared Wiltshire
  */
 public class LazyInitializer<T> {
@@ -17,11 +18,13 @@ public class LazyInitializer<T> {
 
     /**
      * Get the already initialized value or use the provided supplier to initialize it.
-     * 
-     * @param supplier
+     *
+     * @param supplier must not return null or the supplier will be called on every get()
      * @return
      */
     public T get(final Supplier<T> supplier) {
+        Objects.requireNonNull(supplier);
+
         T result = value;
         if (result == null) {
             synchronized(this) {
@@ -33,7 +36,14 @@ public class LazyInitializer<T> {
         }
         return result;
     }
-    
+
+    /**
+     * @return the value if it is already initialized, otherwise null
+     */
+    public T getIfInitialized() {
+        return value;
+    }
+
     /**
      * Reset the value so that next time get() is called it re-initializes the value.
      */
