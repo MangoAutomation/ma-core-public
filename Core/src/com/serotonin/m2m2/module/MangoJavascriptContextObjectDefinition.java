0ae5c4ca3d06bc20eb3179bdc277f80d5dfc3bec
terrypacker <terrypacker@users.noreply.github.com>
2018-08-07 15:02:56 -1000

    Adding Javascript Context Module Definition

---
+++ Core/src/com/serotonin/m2m2/module/MangoJavascriptContextObjectDefinition.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
+ */
+package com.serotonin.m2m2.module;
+
+import com.serotonin.m2m2.vo.permission.PermissionHolder;
+
+/**
+ *
+ * @author Terry Packer
+ */
+public abstract class MangoJavascriptContextObjectDefinition extends ModuleElementDefinition {
+
+    /**
+     * Get the context key for the Object in the Context
+     * 
+     * @return
+     */
+    abstract public String getContextKey();
+    
+    /**
+     * Get the object to use in the context
+     * @return
+     */
+    abstract public Object getContextObject();
+}
