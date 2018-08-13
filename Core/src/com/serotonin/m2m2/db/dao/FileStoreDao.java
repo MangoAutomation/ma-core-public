4e68eb41f49df6447a932e93cec9e7d28c521ead
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-08-13 10:44:14 -0600

    Enable injecting FileStoreDao

--- Core/src/com/serotonin/m2m2/db/dao/FileStoreDao.java
+++ Core/src/com/serotonin/m2m2/db/dao/FileStoreDao.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018 Infinite Automation Software. All rights reserved. 
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
  *
  */
 package com.serotonin.m2m2.db.dao;
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import com.infiniteautomation.mango.util.LazyInitSupplier;
 import com.serotonin.m2m2.Common;
@@ -23,31 +24,35 @@ import com.serotonin.m2m2.vo.FileStore;
  *
  * @author Phillip Dunlap
  */
+@Repository
 public class FileStoreDao extends BaseDao {
     private static final String SELECT_FILE_STORE_DEFINITIONS = "SELECT storeName, readPermission, writePermission FROM fileStores ";
 
-    private static final LazyInitSupplier<FileStoreDao> instance = new LazyInitSupplier<>(() -> {
-        return new FileStoreDao();
+    private static final LazyInitSupplier<FileStoreDao> springInstance = new LazyInitSupplier<>(() -> {
+        FileStoreDao dao = Common.getRuntimeContext().getBean(FileStoreDao.class);
+        if (dao == null)
+            throw new IllegalStateException("DAO not initialized in Spring Runtime Context");
+        return dao;
     });
-    
+
     private FileStoreDao() { }
-    
+
     public static FileStoreDao getInstance() {
-        return instance.get();
+        return springInstance.get();
     }
-    
+
     public List<FileStore> getUserFileStores() {
         return ejt.query(SELECT_FILE_STORE_DEFINITIONS, new FileStoreRowMapper());
     }
-    
+
     public FileStore getUserFileStore(String storeName) {
         return ejt.queryForObject(SELECT_FILE_STORE_DEFINITIONS + " where storeName=?", new Object[] {storeName}, new int[] {Types.VARCHAR}, new FileStoreRowMapper(), null);
     }
-    
+
     public FileStore getUserFileStoreById(int storeId) {
         return ejt.queryForObject(SELECT_FILE_STORE_DEFINITIONS + " where id=?", new Object[] {storeId}, new int[] {Types.INTEGER}, new FileStoreRowMapper(), null);
     }
-    
+
     public Map<String, FileStoreDefinition> getFileStoreMap() {
         List<FileStore> fileStores = getUserFileStores();
         Map<String, FileStoreDefinition> definitionsMap = new HashMap<String, FileStoreDefinition>();
@@ -56,7 +61,7 @@ public class FileStoreDao extends BaseDao {
         definitionsMap.putAll(ModuleRegistry.getFileStoreDefinitions());
         return definitionsMap;
     }
-    
+
     public FileStoreDefinition getFileStoreDefinition(String storeName) {
         FileStoreDefinition fsd = ModuleRegistry.getFileStoreDefinition(storeName);
         if(fsd == null) {
@@ -65,9 +70,9 @@ public class FileStoreDao extends BaseDao {
                 return null;
             return fs.toDefinition();
         }
-        return fsd; 
+        return fsd;
     }
-    
+
     private class FileStoreRowMapper implements RowMapper<FileStore> {
 
         /* (non-Javadoc)
@@ -82,13 +87,13 @@ public class FileStoreDao extends BaseDao {
             result.setWritePermission(rs.getString(++i));
             return result;
         }
-        
+
     }
-    
+
     private static final String INSERT_FILE_STORE = "INSERT INTO fileStores (storeName, readPermission, writePermission) values (?, ?, ?)";
     private static final String UPDATE_FILE_STORE = "UPDATE fileStores SET storeName=?, readPermission=?, writePermission=? where id=?";
     private static final String DELETE_FILE_STORE = "DELETE FROM fileStores WHERE ";
-    
+
     public int saveFileStore(FileStore fs) {
         if(fs.getId() == Common.NEW_ID)
             insert(fs);
@@ -96,21 +101,21 @@ public class FileStoreDao extends BaseDao {
             update(fs);
         return fs.getId();
     }
-    
+
     private void insert(FileStore fs) {
-        fs.setId(ejt.doInsert(INSERT_FILE_STORE, new Object[] {fs.getStoreName(), fs.getReadPermission(), fs.getWritePermission()}, 
+        fs.setId(ejt.doInsert(INSERT_FILE_STORE, new Object[] {fs.getStoreName(), fs.getReadPermission(), fs.getWritePermission()},
                 new int[] {Types.VARCHAR, Types.VARCHAR, Types.VARCHAR}));
     }
-    
+
     private void update(FileStore fs) {
-        ejt.update(UPDATE_FILE_STORE, new Object[] {fs.getStoreName(), fs.getReadPermission(), fs.getWritePermission(), fs.getId()}, 
+        ejt.update(UPDATE_FILE_STORE, new Object[] {fs.getStoreName(), fs.getReadPermission(), fs.getWritePermission(), fs.getId()},
                 new int[] {Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER});
     }
-    
+
     public int deleteFileStore(String storeName) {
         return ejt.update(DELETE_FILE_STORE + "storeName='" + storeName + "'");
     }
-    
+
     public int deleteFileStore(int storeId) {
         return ejt.update(DELETE_FILE_STORE + "id='" + storeId + "'");
     }
