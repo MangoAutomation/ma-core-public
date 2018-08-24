ad9ca93c378231b452b805cde86776738277f9a5
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-08-24 11:01:43 -0600

    Add uncaught exception handler class

---
+++ Core/src/com/infiniteautomation/mango/exceptionHandling/MangoUncaughtExceptionHandler.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.exceptionHandling;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Jared Wiltshire
+ */
+public class MangoUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        log.error("Uncaught exception in thread " + t.getName(), e);
+    }
+
+}
