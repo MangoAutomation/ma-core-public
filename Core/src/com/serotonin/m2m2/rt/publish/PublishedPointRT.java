3275bc750a972111a788a36911ec7aa68fcc4942
terrypacker <terrypacker@users.noreply.github.com>
2018-09-04 12:08:01 -1000

    Publish reliability as true if DNE, publish on start/stop of point.

--- Core/src/com/serotonin/m2m2/rt/publish/PublishedPointRT.java
+++ Core/src/com/serotonin/m2m2/rt/publish/PublishedPointRT.java
@@ -4,12 +4,14 @@
  */
 package com.serotonin.m2m2.rt.publish;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.rt.dataImage.DataPointListener;
 import com.serotonin.m2m2.rt.dataImage.DataPointRT;
 import com.serotonin.m2m2.rt.dataImage.PointValueTime;
+import com.serotonin.m2m2.rt.dataSource.DataSourceRT;
 import com.serotonin.m2m2.vo.publish.PublishedPointVO;
 import com.serotonin.m2m2.vo.publish.PublisherVO.PublishType;
 
@@ -27,27 +29,28 @@ public class PublishedPointRT<T extends PublishedPointVO> implements DataPointLi
         Common.runtimeManager.addDataPointListener(vo.getDataPointId(), this);
         DataPointRT rt = Common.runtimeManager.getDataPoint(vo.getDataPointId());
         pointEnabled = rt != null;
-        parent.attributeChanged(this, rt.getAttributes());
+        publishAttributes(rt, false);
     }
 
     public void terminate() {
         Common.runtimeManager.removeDataPointListener(vo.getDataPointId(), this);
     }
 
+    @Override
     public void pointChanged(PointValueTime oldValue, PointValueTime newValue) {
         if (parent.getVo().getPublishType() == PublishType.CHANGES_ONLY)
             parent.publish(vo, newValue);
     }
-
+    @Override
     public void pointSet(PointValueTime oldValue, PointValueTime newValue) {
         // no op. Everything gets handled in the other methods.
     }
-
+    @Override
     public void pointUpdated(PointValueTime newValue) {
         if (parent.getVo().getPublishType() == PublishType.ALL)
             parent.publish(vo, newValue);
     }
-
+    @Override
     public void pointBackdated(PointValueTime value) {
         // no op
     }
@@ -55,15 +58,22 @@ public class PublishedPointRT<T extends PublishedPointVO> implements DataPointLi
     public boolean isPointEnabled() {
         return pointEnabled;
     }
-
+    @Override
     public void pointInitialized() {
         pointEnabled = true;
         parent.pointInitialized(this);
+        DataPointRT rt = Common.runtimeManager.getDataPoint(vo.getDataPointId());
+        if(rt != null)
+            publishAttributes(rt, false);
     }
-
+    @Override
     public void pointTerminated() {
         pointEnabled = false;
         parent.pointTerminated(this);
+        //Publish that its unreliable
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put(DataSourceRT.ATTR_UNRELIABLE_KEY, true);
+        parent.attributeChanged(this, attributes);
     }
 
     @Override
@@ -83,5 +93,21 @@ public class PublishedPointRT<T extends PublishedPointVO> implements DataPointLi
     
     public T getVo() {
         return vo;
+    }
+    
+    /**
+     * Helper to publish attributes ensuring that the UNRELIABLE flag exists
+     */
+    protected void publishAttributes(DataPointRT rt, boolean unreliable) {
+        if(rt != null && parent.getVo().isPublishAttributeChanges()) {
+            //Ensure that the reliability attribute exists, if DNE then assume reliable.
+            if(rt.getAttribute(DataSourceRT.ATTR_UNRELIABLE_KEY) == null) {
+                Map<String, Object> attributes = new HashMap<>(rt.getAttributes());
+                attributes.put(DataSourceRT.ATTR_UNRELIABLE_KEY, unreliable);
+                parent.attributeChanged(this, attributes);
+            }else {
+                parent.attributeChanged(this, rt.getAttributes());
+            }
+        }
     }
 }
