74b4927dd442166c5819ffc4f44dbae611fa2b22
Phillip <puckfist@gmail.com>
2018-09-11 15:15:10 -0600

    Some IPv6 support in IpAddressUtils #107

--- Core/src/com/serotonin/util/IpAddressUtils.java
+++ Core/src/com/serotonin/util/IpAddressUtils.java
@@ -4,10 +4,17 @@
  */
 package com.serotonin.util;
 
+import java.util.regex.Pattern;
+
 /**
  * @author Matthew Lohbihler
  */
 public class IpAddressUtils {
+    private static final Pattern IPV6_REGEX = Pattern.compile("[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}"+
+            ":[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}");
+    private static final int IPV4_BASE = 10;
+    private static final int IPV6_BASE = 16;
+    
     public static boolean ipWhiteListCheck(String allowedIp, String remoteIp) throws IpWhiteListException {
         String[] remoteIpParts = remoteIp.split("\\.");
         if (remoteIpParts.length != 4)
@@ -17,11 +24,21 @@ public class IpAddressUtils {
     
     public static boolean ipWhiteListCheck(String[] allowedIps, String remoteIp) throws IpWhiteListException {
         String[] remoteIpParts = remoteIp.split("\\.");
-        if (remoteIpParts.length != 4)
-            throw new IpWhiteListException("Invalid remote IP address: "+ remoteIp);
+        if (remoteIpParts.length != 4) {
+            remoteIpParts = remoteIp.split(":");
+            if(remoteIpParts.length != 8)
+                throw new IpWhiteListException("Invalid remote IP address: "+ remoteIp);
+            
+            for (int i=0; i<allowedIps.length; i++) {
+                if (isIpv6(allowedIps[i]) && ipv6WhiteListCheckImpl(allowedIps[i], remoteIp, remoteIpParts))
+                    return true;
+            }
+            
+            return false;
+        }
         
         for (int i=0; i<allowedIps.length; i++) {
-            if (ipWhiteListCheckImpl(allowedIps[i], remoteIp, remoteIpParts))
+            if (!isIpv6(allowedIps[i]) && ipWhiteListCheckImpl(allowedIps[i], remoteIp, remoteIpParts))
                 return true;
         }
         
@@ -34,13 +51,29 @@ public class IpAddressUtils {
         if (allowedIpParts.length != 4)
             throw new IpWhiteListException("Invalid allowed IP address: "+ allowedIp);
             
-        return validateIpPart(allowedIpParts[0], remoteIpParts[0], allowedIp, remoteIp) &&
-                validateIpPart(allowedIpParts[1], remoteIpParts[1], allowedIp, remoteIp) &&
-                validateIpPart(allowedIpParts[2], remoteIpParts[2], allowedIp, remoteIp) &&
-                validateIpPart(allowedIpParts[3], remoteIpParts[3], allowedIp, remoteIp);
+        return validateIpPart(allowedIpParts[0], remoteIpParts[0], allowedIp, remoteIp, IPV4_BASE) &&
+                validateIpPart(allowedIpParts[1], remoteIpParts[1], allowedIp, remoteIp, IPV4_BASE) &&
+                validateIpPart(allowedIpParts[2], remoteIpParts[2], allowedIp, remoteIp, IPV4_BASE) &&
+                validateIpPart(allowedIpParts[3], remoteIpParts[3], allowedIp, remoteIp, IPV4_BASE);
     }
     
-    private static boolean validateIpPart(String allowed, String remote, String allowedIp, String remoteIp)
+    private static boolean ipv6WhiteListCheckImpl(String allowedIp, String remoteIp, String[] remoteIpParts)
+            throws IpWhiteListException {
+        String[] allowedIpParts = allowedIp.split(":");
+        if (allowedIpParts.length != 8)
+            throw new IpWhiteListException("Invalid allowed IPv6 address: "+ allowedIp);
+        
+        return validateIpPart(allowedIpParts[0], remoteIpParts[0], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[1], remoteIpParts[1], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[2], remoteIpParts[2], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[3], remoteIpParts[3], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[4], remoteIpParts[4], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[5], remoteIpParts[5], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[6], remoteIpParts[6], allowedIp, remoteIp, IPV6_BASE) &&
+               validateIpPart(allowedIpParts[7], remoteIpParts[7], allowedIp, remoteIp, IPV6_BASE);
+    }
+    
+    private static boolean validateIpPart(String allowed, String remote, String allowedIp, String remoteIp, int base)
             throws IpWhiteListException {
         if ("*".equals(allowed))
             return true;
@@ -48,23 +81,39 @@ public class IpAddressUtils {
         int dash = allowed.indexOf('-');
         try {
             if (dash == -1)
-                return Integer.parseInt(allowed) == Integer.parseInt(remote);
+                return Integer.parseInt(allowed, base) == Integer.parseInt(remote, base);
             
-            int from = Integer.parseInt(allowed.substring(0, dash));
-            int to = Integer.parseInt(allowed.substring(dash + 1));
-            int rem = Integer.parseInt(remote);
+            int from = Integer.parseInt(allowed.substring(0, dash), base);
+            int to = Integer.parseInt(allowed.substring(dash + 1), base);
+            int rem = Integer.parseInt(remote, base);
             
             return from <= rem && rem <= to;
         }
         catch (NumberFormatException e) {
-            throw new IpWhiteListException("Integer parsing error. allowed="+ allowedIp + ", remote="+ remoteIp);
+            throw new IpWhiteListException("Integer parsing error. allowed="+ allowedIp + ", remote="+ remoteIp + ", base="+ base);
         }
+    }
+    
+    private static boolean isIpv6(String address) {
+        return address != null && IPV6_REGEX.matcher(address).matches();
     }
     
     public static String checkIpMask(String ip) {
         String[] ipParts = ip.split("\\.");
-        if (ipParts.length != 4)
-            return "IP address must have 4 parts";
+        if (ipParts.length != 4) {
+            ipParts = ip.split(":");
+            if(ipParts.length != 8)
+                return "IPv4 address must have 4 parts, IPv6 address must have 8 parts";
+            else {
+                String message;
+                for(int i = 0; i < 8; i+=1) {
+                    message = checkIpv6MaskPart(ipParts[i]);
+                    if(message != null)
+                        return message;
+                }
+                return null;
+            }
+        }
         
         String message = checkIpMaskPart(ipParts[0]);
         if (message != null)
@@ -89,16 +138,16 @@ public class IpAddressUtils {
         int dash = part.indexOf('-');
         try {
             if (dash == -1) {
-                int value = Integer.parseInt(part);
+                int value = Integer.parseInt(part, IPV4_BASE);
                 if (value < 0 || value > 255)
                     return "Value out of range in '"+ part +"'";
             }
             else {
-                int from = Integer.parseInt(part.substring(0, dash));
+                int from = Integer.parseInt(part.substring(0, dash), IPV4_BASE);
                 if (from < 0 || from > 255)
                     return "'From' value out of range in '"+ part +"'";
                 
-                int to = Integer.parseInt(part.substring(dash + 1));
+                int to = Integer.parseInt(part.substring(dash + 1), IPV4_BASE);
                 if (to < 0 || to > 255)
                     return "'To' value out of range in '"+ part +"'";
                 
@@ -113,7 +162,36 @@ public class IpAddressUtils {
         return null;
     }
     
-    public static byte[] toIpAddress(String addr) throws IllegalArgumentException {
+    private static String checkIpv6MaskPart(String part) {
+        if ("*".equals(part))
+            return null;
+        
+        int dash = part.indexOf('-');
+        try {
+            if (dash == -1) {
+                int value = Integer.parseInt(part, IPV6_BASE);
+                if (value < 0 || value > 65535)
+                    return "Value out of range in '"+ part +"'";
+            } else {
+                int from = Integer.parseInt(part.substring(0, dash), IPV6_BASE);
+                if (from < 0 || from > 65535)
+                    return "'From' value out of range in '"+ part +"'";
+                
+                int to = Integer.parseInt(part.substring(dash + 1), IPV6_BASE);
+                if (to < 0 || to > 65535)
+                    return "'To' value out of range in '"+ part +"'";
+                
+                if (from > to)
+                    return "'From' value is greater than 'To' value in '"+ part +"'";
+            }
+        } catch (NumberFormatException e) {
+            return "Integer parsing error in '"+ part +"'";
+        }
+        
+        return null;
+    }
+    
+    public static byte[] toIpAddress(String addr) throws IllegalArgumentException { //TODO IPv6
         if (addr == null)
             throw new IllegalArgumentException("Invalid address: (null)");
         
@@ -137,7 +215,7 @@ public class IpAddressUtils {
         return ip;
     }
     
-    public static String toIpString(byte[] b) throws IllegalArgumentException {
+    public static String toIpString(byte[] b) throws IllegalArgumentException { //TODO IPv6
         if (b.length != 4)
             throw new IllegalArgumentException("IP address must have 4 parts");
         
