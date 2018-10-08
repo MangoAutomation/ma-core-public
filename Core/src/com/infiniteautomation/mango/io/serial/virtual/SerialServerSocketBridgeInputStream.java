5364f72d6ba881b094aec548a8b294cba4bfd061
Phillip <puckfist@gmail.com>
2018-10-08 11:00:55 -0600

    Fix virtual server socket sync bugs

--- Core/src/com/infiniteautomation/mango/io/serial/virtual/SerialServerSocketBridgeInputStream.java
+++ Core/src/com/infiniteautomation/mango/io/serial/virtual/SerialServerSocketBridgeInputStream.java
@@ -29,54 +29,62 @@ public class SerialServerSocketBridgeInputStream extends SerialPortInputStream {
 	}
 	
 	public int bufferRead() throws IOException, SerialServerSocketConnectionClosedException {
-		if(stream == null)
-			return -1;
-		if(bufferStream != null)
-			return bufferStream.available();
-		//Add one so we can insert a -1 at the end and close our buffer stream
-		byte[] data = new byte[bufferSize];
-		int read = stream.read(data, 0, data.length);
-		if(read == -1)
-			throw new SerialServerSocketConnectionClosedException();
-		currentBuffered = read;
-		currentPosition.set(0);
-		bufferStream = new ByteArrayInputStream(data);
-		return read;
+	    synchronized(this) {	
+	        if(stream == null)
+    			return -1;
+    		if(bufferStream != null)
+    			return bufferStream.available();
+    		//Add one so we can insert a -1 at the end and close our buffer stream
+    		byte[] data = new byte[bufferSize];
+    		int read = stream.read(data, 0, data.length);
+    		if(read == -1)
+    			throw new SerialServerSocketConnectionClosedException();
+    		currentBuffered = read;
+    		currentPosition.set(0);
+    		bufferStream = new ByteArrayInputStream(data);
+    	    return read;
+		}
 	}
 	
 	@Override
 	public int read() throws IOException {
-		if(this.bufferStream == null) {
-			try { 
-				bufferRead(); 
-			} catch(SerialServerSocketConnectionClosedException e) {
-				return -1;
-			}
-			if(this.bufferStream == null)
-				return -1;
-		}
-		
-		if(currentPosition.getAndIncrement() == currentBuffered) {
-			this.bufferStream.close();
-			this.bufferStream = null;
-			return -1;
-		}
-		return this.bufferStream.read();
+	    synchronized(this) {
+    		if(this.bufferStream == null) {
+    			try { 
+    				bufferRead(); 
+    			} catch(SerialServerSocketConnectionClosedException e) {
+    				return -1;
+    			}
+    			if(this.bufferStream == null)
+    				return -1;
+    		}
+    		
+    		if(currentPosition.getAndIncrement() == currentBuffered) {
+    			this.bufferStream.close();
+    			this.bufferStream = null;
+    			return -1;
+    		}
+    		return this.bufferStream.read();
+	    }
 	}
 
 	@Override
 	public int available() throws IOException {
-		if(this.bufferStream == null)
-			return 0;
-		return this.bufferStream.available();
+	    synchronized(this) {
+    		if(this.bufferStream == null)
+    			return 0;
+    		return this.bufferStream.available();
+	    }
 	}
 
 	@Override
 	public void closeImpl() throws IOException {
-		if(this.stream != null)
-			this.stream.close();
-		if(this.bufferStream != null)
-			this.bufferStream.close();
+		synchronized(this) {
+		    if(this.stream != null)
+	            this.stream.close();
+    		if(this.bufferStream != null)
+    			this.bufferStream.close();
+		}
 	}
 
 	@Override
