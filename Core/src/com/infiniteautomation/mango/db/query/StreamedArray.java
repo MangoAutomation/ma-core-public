49ff5f022f480e00de118b228de272911d2b4ed8
terrypacker <terrypacker@users.noreply.github.com>
2018-10-13 13:40:50 +1100

    Moving StreamedArray/StreamedArrayWithTotal to Core

---
+++ Core/src/com/infiniteautomation/mango/db/query/StreamedArray.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2017 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.db.query;
+
+/**
+ * @author Jared Wiltshire
+ */
+public interface StreamedArray {
+}
