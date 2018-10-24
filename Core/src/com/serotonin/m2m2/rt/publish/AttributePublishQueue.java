4a6ec66904cf1dd7f2f88d562480014ba4e2918f
terrypacker <terrypacker@users.noreply.github.com>
2018-10-24 11:48:48 -1000

    Change AttributePublishQueue to be more threadsafe

--- Core/src/com/serotonin/m2m2/rt/publish/AttributePublishQueue.java
+++ Core/src/com/serotonin/m2m2/rt/publish/AttributePublishQueue.java
@@ -3,13 +3,13 @@
  */
 package com.serotonin.m2m2.rt.publish;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.serotonin.ShouldNeverHappenException;
 import com.serotonin.m2m2.vo.publish.PublishedPointVO;
 
 /**
@@ -24,7 +24,8 @@ import com.serotonin.m2m2.vo.publish.PublishedPointVO;
  */
 public class AttributePublishQueue<T extends PublishedPointVO> extends PublishQueue<T, Map<String,Object>> {
 
-    protected ConcurrentHashMap<T, Map<String, Object>> attributesToBePublished;
+    //TODO keep keys as integer, values as PublishQueueEntry
+    protected ConcurrentHashMap<Integer, PublishQueueEntry<T, Map<String, Object>>> attributesToBePublished;
     
     /**
      * @param owner
@@ -37,67 +38,46 @@ public class AttributePublishQueue<T extends PublishedPointVO> extends PublishQu
     }
 
     public void add(T vo, Map<String,Object> value) {
-        attributesToBePublished.put(vo, value);
+        attributesToBePublished.put(vo.getDataPointId(), new PublishQueueEntry<T, Map<String, Object>>(vo, value));
     }
 
     public void add(T vo, List<Map<String,Object>> value) {
-        //TODO N/A as this can't happen with attributes
+        throw new ShouldNeverHappenException("Unimplemented");
     }
 
     public PublishQueueEntry<T,Map<String,Object>> next() {
-        if(attributesToBePublished.isEmpty())
+        Iterator<Entry<Integer ,PublishQueueEntry<T, Map<String, Object>>>> iter = attributesToBePublished.entrySet().iterator();
+        Entry<Integer, PublishQueueEntry<T, Map<String, Object>>> entry = iter.next();
+        if(entry != null) {
+            attributesToBePublished.compute(entry.getKey(), (k, v) -> {
+                if (v == entry.getValue())
+                    return null;
+                return v;
+            });
+            return entry.getValue();
+        }else {
             return null;
-        Iterator<Entry<T,Map<String,Object>>> iter = attributesToBePublished.entrySet().iterator();
-        Entry<T,Map<String,Object>> entry = iter.next();
-        if(entry != null)
-            return new PublishQueueEntry<T, Map<String, Object>>(entry.getKey(), entry.getValue());
-        else
-            return null;
+        }
     }
 
     public List<PublishQueueEntry<T,Map<String,Object>>> get(int max) {
-        if(attributesToBePublished.isEmpty())
-            return null;
-            
-        Iterator<Entry<T,Map<String,Object>>> iter = attributesToBePublished.entrySet().iterator();
-        List<PublishQueueEntry<T,Map<String,Object>>> result = new ArrayList<>(max);
-        while (iter.hasNext() && result.size() < max) {
-            Entry<T,Map<String,Object>> entry = iter.next();
-            result.add(new PublishQueueEntry<T, Map<String, Object>>(entry.getKey(), entry.getValue()));
-        }
-
-        return result;
+        throw new ShouldNeverHappenException("Unimplemented");
     }
 
     public void remove(PublishQueueEntry<T,Map<String,Object>> e) {
-        if(e == null)
-            return;
-        //TODO Mango 3.6 Make more efficient
-        //We cannot rely on the equals/hashcode for T so we must search for data point id
-        Iterator<T>  it = attributesToBePublished.keySet().iterator();
-        while(it.hasNext()) {
-            T next = it.next();
-            if(next.getDataPointId() == e.getVo().getDataPointId()) {
-                it.remove();
-                break;
-            }
-        }
-        attributesToBePublished.remove(e.getVo());
+        throw new ShouldNeverHappenException("Unimplemented");
     }
 
     public void removeAll(List<PublishQueueEntry<T,Map<String,Object>>> list) {
-        if(list != null) {
-            for(PublishQueueEntry<T,Map<String,Object>> e : list)
-                remove(e);
-        }
+        throw new ShouldNeverHappenException("Unimplemented");
     }
     
     public void removeAll() {
-        queue.clear();
+        attributesToBePublished.clear();
     }
 
     public int getSize() {
-        return queue.size();
+        return attributesToBePublished.size();
     }
     
     
