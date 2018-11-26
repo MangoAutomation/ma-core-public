29db48eb11644a333c7433e494547856b3c020fb
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-11-26 15:08:34 -0700

    Make alarmLevel volatile and protect description

--- Core/src/com/serotonin/m2m2/vo/event/EventTypeVO.java
+++ Core/src/com/serotonin/m2m2/vo/event/EventTypeVO.java
@@ -8,18 +8,16 @@ import com.serotonin.m2m2.i18n.TranslatableMessage;
 import com.serotonin.m2m2.rt.event.type.EventType;
 
 public class EventTypeVO {
-    
-    private final EventType eventType;
-    
-    private TranslatableMessage description;
-    private int alarmLevel;
 
-//    public EventTypeVO() {
-//        
-//    }
-    
-    public EventTypeVO(EventType eventType, TranslatableMessage description,
-            int alarmLevel) {
+    private final EventType eventType;
+
+    private final TranslatableMessage description;
+    /**
+     * Alarm levels can be changed via system settings
+     */
+    private volatile int alarmLevel;
+
+    public EventTypeVO(EventType eventType, TranslatableMessage description, int alarmLevel) {
         this.eventType = eventType;
         this.description = description;
         this.alarmLevel = alarmLevel;
@@ -38,9 +36,6 @@ public class EventTypeVO {
         return eventType.getEventType();
     }
 
-//    public void setType(String type) {
-//        this.type = type;
-//    }
     /**
      * Use getEventType().get... instead
      * @return
@@ -50,9 +45,6 @@ public class EventTypeVO {
         return eventType.getEventSubtype();
     }
 
-//    public void setSubtype(String subtype) {
-//        this.subtype = subtype;
-//    }
     /**
      * Use getEventType().get... instead
      * @return
@@ -62,9 +54,6 @@ public class EventTypeVO {
         return eventType.getReferenceId1();
     }
 
-//    public void setTypeRef1(int typeRef1) {
-//        this.typeRef1 = typeRef1;
-//    }
     /**
      * Use getEventType().get... instead
      * @return
@@ -74,22 +63,19 @@ public class EventTypeVO {
         return eventType.getReferenceId2();
     }
 
-//    public void setTypeRef2(int typeRef2) {
-//        this.typeRef2 = typeRef2;
-//    }
-
     public TranslatableMessage getDescription() {
         return description;
-    }
-
-    public void setDescription(TranslatableMessage description) {
-        this.description = description;
     }
 
     public int getAlarmLevel() {
         return alarmLevel;
     }
 
+    /**
+     * Alarm levels can be changed via system settings
+     *
+     * @param alarmLevel
+     */
     public void setAlarmLevel(int alarmLevel) {
         this.alarmLevel = alarmLevel;
     }
