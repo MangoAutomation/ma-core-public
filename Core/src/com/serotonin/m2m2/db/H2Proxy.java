003d1db7fce9adf66b9bd8d706dd948c5f7bfe87
terrypacker <terrypacker@users.noreply.github.com>
2019-04-30 14:57:39 -1000

    Upgrade h2, fix LOB issue on startup.  Does not handle SQL restore yet
    as per #1344

--- Core/src/com/serotonin/m2m2/db/H2Proxy.java
+++ Core/src/com/serotonin/m2m2/db/H2Proxy.java
@@ -10,7 +10,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +27,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.h2.jdbcx.JdbcConnectionPool;
 import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.Recover;
 import org.h2.tools.Server;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -34,12 +42,18 @@ import com.serotonin.util.StringUtils;
 public class H2Proxy extends AbstractDatabaseProxy {
     private static final Log LOG = LogFactory.getLog(H2Proxy.class);
 
+    //Select the version of H2 that created this database.
+    public static final String H2_CREATE_VERSION_SELECT = "SELECT value FROM information_schema.settings WHERE name='CREATE_BUILD' LIMIT 1";
+    
     private JdbcConnectionPool dataSource;
     private Server web; //web UI
     
     @Override
     protected void initializeImpl(String propertyPrefix) {
         LOG.info("Initializing H2 connection manager");
+        
+        upgradePageStore(propertyPrefix);
+        
         JdbcDataSource jds = new JdbcDataSource();
         jds.setURL(getUrl(propertyPrefix));
         jds.setDescription("maDataSource");
@@ -56,6 +70,7 @@ public class H2Proxy extends AbstractDatabaseProxy {
         dataSource.setMaxConnections(
                 Common.envProps.getInt(propertyPrefix + "db.pool.maxActive", 100));
 
+        
         if (Common.envProps.getBoolean(propertyPrefix + "db.web.start", false)) {
             LOG.info("Initializing H2 web server");
             String webArgs[] = new String[4];
@@ -68,6 +83,88 @@ public class H2Proxy extends AbstractDatabaseProxy {
                 this.web.start();
             } catch (SQLException e) {
                 LOG.error(e);
+            }
+        }
+    }
+
+    /**
+     * Potentially upgrade the h2 pagestore database from v196 
+     * @param propertyPrefix
+     */
+    private void upgradePageStore(String propertyPrefix) {
+        
+        //Parse out the useful sections of the url
+        String url = Common.envProps.getString(propertyPrefix + "db.url");
+        url = StringUtils.replaceMacros(url, System.getProperties());
+        String [] jdbcParts = url.split(":");
+        String [] commandParts = jdbcParts[2].split(";");
+        Path dbPath = Paths.get(commandParts[0]);
+        Path existingDbPath = dbPath.getParent().resolve(dbPath.getFileName() + ".h2.db");
+        
+        //TODO Determine a good place for these files
+        Path dumpPath = dbPath.getParent().resolve(dbPath.getFileName() + ".h2.196.sql.zip");
+        Path bakDbPath = dbPath.getParent().resolve("mah2.h2.196.db");
+        
+        //If the backup database from a previous attempt is still hanging around we will 
+        
+        //We need to do this if a db exists and is version 196 or below
+        if(existingDbPath.toFile().exists()) {
+            
+            try {
+                //Did a previous attempt fail?
+                if(bakDbPath.toFile().exists()){
+                    //TODO is this logic ideal?
+                    //Failed attempt assume the backup is the best one to keep so replace the existing
+                    Files.copy(bakDbPath, existingDbPath);
+                }else {
+                    //Move existing db (for safety)
+                    Files.copy(existingDbPath, bakDbPath);
+                }
+
+                //First we will try to get the version from the existing db
+                org.h2.Driver.load();
+                String fullUrl = getUrl(propertyPrefix);
+                String user = Common.envProps.getString(propertyPrefix + "db.username", null);
+                String password = Common.envProps.getString(propertyPrefix + "db.password", null);
+                
+
+                
+                try(Connection conn = DriverManager.getConnection(fullUrl, user, password);){
+                    Statement stat = conn.createStatement();
+                    ResultSet rs = stat.executeQuery(H2_CREATE_VERSION_SELECT);
+                    if(rs.next()) {
+                        int version = rs.getInt(1);
+                        if(version >= 197)
+                            return;
+                    }
+                    stat.executeQuery("SCRIPT DROP TO '" + dumpPath.toString() + "' COMPRESSION ZIP");
+                }
+                
+                //Delete existing so we can re-create it using the dump script
+                Files.delete(existingDbPath);
+
+                //Open a connection and import the dump script
+                String runScript = fullUrl + ";init=RUNSCRIPT FROM '" + dumpPath.toString() + "' COMPRESSION ZIP";
+                try(Connection conn = DriverManager.getConnection(runScript, user, password);){
+                    Statement stat = conn.createStatement();
+                    //Might as well do a compaction here
+                    stat.execute("shutdown compact");
+                    stat.close();
+                }
+                
+                try {
+                    Files.deleteIfExists(dumpPath);
+                } catch (IOException e) {
+                    LOG.warn("Unable to delete un-necessary h2 dump file " + dumpPath.toString(), e);
+                }
+                
+                try {
+                    Files.deleteIfExists(bakDbPath);
+                } catch (IOException e) {
+                    LOG.warn("Unable to delete un-necessary h2 backup file " + bakDbPath.toString(), e);
+                }
+            }catch(Exception e) {
+                throw new ShouldNeverHappenException(e);
             }
         }
     }
@@ -86,7 +183,7 @@ public class H2Proxy extends AbstractDatabaseProxy {
         }
         return url;
     }
-
+    
     @Override
     public DataSource getDataSource() {
         return dataSource;
