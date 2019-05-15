7e84ceff3bcca99e2529aba23ea88b945f4528f2
Terry Packer <terrypacker@users.noreply.github.com>
2019-05-15 06:41:16 -1000

    Setup common ForkJoinPool to use classloader with access to module code (#1430)
    * Setup common ForkJoinPool to use classloader with access to module code
    * Use static in common to store module class loader

---
+++ Core/src/com/infiniteautomation/mango/spring/components/executors/MangoForkJoinWorkerThreadFactory.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2019 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.spring.components.executors;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory;
+import java.util.concurrent.ForkJoinWorkerThread;
+
+import com.serotonin.m2m2.Common;
+
+/**
+ * Ensure the Mango Module class loader is available to all ForkJoinWorker Threads
+ *
+ * @author Terry Packer
+ *
+ */
+public class MangoForkJoinWorkerThreadFactory implements ForkJoinWorkerThreadFactory {
+
+    @Override
+    public ForkJoinWorkerThread newThread(ForkJoinPool pool) {
+        return new MangoForkJoinWorkerThread(pool);
+    }
+
+    /**
+     * Thread that will set its context class loader prior to execution
+     * @author Terry Packer
+     *
+     */
+    class MangoForkJoinWorkerThread extends ForkJoinWorkerThread {
+
+        protected MangoForkJoinWorkerThread(ForkJoinPool pool) {
+            super(pool);
+            this.setContextClassLoader(Common.getModuleClassLoader());
+        }
+
+    }
+
+}
