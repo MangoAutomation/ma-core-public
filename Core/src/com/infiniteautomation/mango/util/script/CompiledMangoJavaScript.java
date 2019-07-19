4835240d7271ad547b175538be02eb1bdaeec149
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-07-19 15:46:49 -0600

    Change import

--- Core/src/com/infiniteautomation/mango/util/script/CompiledMangoJavaScript.java
+++ Core/src/com/infiniteautomation/mango/util/script/CompiledMangoJavaScript.java
@@ -12,6 +12,8 @@ import javax.script.CompiledScript;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
+import org.springframework.util.Assert;
+
 import com.infiniteautomation.mango.spring.service.MangoJavaScriptService;
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.db.dao.DataPointDao;
@@ -30,28 +32,26 @@ import com.serotonin.m2m2.rt.script.ScriptPointValueSetter;
 import com.serotonin.m2m2.vo.DataPointVO;
 import com.serotonin.m2m2.vo.permission.PermissionHolder;
 
-import io.jsonwebtoken.lang.Assert;
-
 /**
- * Container for Compiled Java Scripts to aid in state management 
+ * Container for Compiled Java Scripts to aid in state management
  * of the various scopes and to help simplify user code.
- * 
+ *
  * Usage:
  * 1. Constructe compiled script
  * 2. call compile()
  * 3. call initialize() (anytime the context changes but now while executing the script)
  * 4. call execute() when you desire to compute a result from the script
- * 
+ *
  * @author Terry Packer
  *
  */
 public class CompiledMangoJavaScript {
-    
+
     private CompiledScript compiledScript;
-    
+
     private final ScriptPointValueSetter setter;
     private final ScriptLog log;
-    
+
     private final Map<String, Object> additionalContext;
     private final List<ScriptUtility> additionalUtilities;
     private final List<JsonImportExclusion> importExclusions;
@@ -61,16 +61,16 @@ public class CompiledMangoJavaScript {
     private final MangoJavaScriptResult result;
     private final List<ScriptUtility> utilities;
     private final PermissionHolder permissionHolder;
-    
+
     //State settings
     private boolean initialized;
     private boolean wrapInFunction;
-    
+
     /**
      * Create a compiled script container to run live
-     * 
+     *
      * NOTE: Script engine and utilities are run using permissions of vo
-     * 
+     *
      * @param vo
      * @param setter
      * @param log
@@ -89,14 +89,14 @@ public class CompiledMangoJavaScript {
         this.permissionHolder = vo.getPermissions();
         this.setter = setter;
     }
-    
 
-    
+
+
     /**
      * Create a compiled script to run live
-     * 
+     *
      * NOTE: Script engine and utilities are run using permissions of user
-     * 
+     *
      * @param setter
      * @param log
      * @param importExclusions
@@ -109,7 +109,7 @@ public class CompiledMangoJavaScript {
             PermissionHolder permissionHolder) {
         this(setter, log, null, null, importExclusions, false, Common.getBean(MangoJavaScriptService.class), permissionHolder);
     }
-    
+
     /**
      * Complete constructor for compiled script
      * @param setter
@@ -124,11 +124,11 @@ public class CompiledMangoJavaScript {
     public CompiledMangoJavaScript(
             ScriptPointValueSetter setter,
             ScriptLog log,
-            Map<String, Object> additionalContext, 
-            List<ScriptUtility> additionalUtilities, 
+            Map<String, Object> additionalContext,
+            List<ScriptUtility> additionalUtilities,
             List<JsonImportExclusion> importExclusions,
-            boolean testRun, 
-            MangoJavaScriptService service, 
+            boolean testRun,
+            MangoJavaScriptService service,
             PermissionHolder permissionHolder) {
         this.setter = setter;
         this.log = log;
@@ -142,10 +142,10 @@ public class CompiledMangoJavaScript {
         this.utilities = new ArrayList<>();
         this.permissionHolder = permissionHolder;
     }
-    
+
     /**
      * Compile the script and add global bindings
-     * 
+     *
      * @param script
      * @param wrapInFunction - Should the script be wrapped in a function call which is called during execution
      * @throws ScriptError
@@ -154,10 +154,10 @@ public class CompiledMangoJavaScript {
         this.compiledScript = service.compile(script, wrapInFunction, permissionHolder);
         this.wrapInFunction = wrapInFunction;
     }
-    
+
     /**
      * Clear the engine scope and initialize it with an expandable context which is returned
-     * 
+     *
      * @throws ScriptError
      * @throws ScriptPermissionsException
      */
@@ -166,11 +166,11 @@ public class CompiledMangoJavaScript {
         this.initialize(context);
         return context;
     }
-    
+
     /**
-     * 
+     *
      * Clear the engine scope and initialize it with an expandable context which is filled with the ScriptContextVariables and returned
-     * 
+     *
      * @param variables
      * @return
      * @throws ScriptPermissionsException - permission denied executing a command
@@ -207,13 +207,13 @@ public class CompiledMangoJavaScript {
         this.initialize(context);
         return context;
     }
-    
+
     /**
      * Clear the engine scope and initialize it with a known context which will be stored in the engine
-     * 
+     *
      * @throws ScriptException
-     * @throws DataPointStateException 
-     * @throws ScriptPermissionsException 
+     * @throws DataPointStateException
+     * @throws ScriptPermissionsException
      */
     public void initialize(Map<String, IDataPointValueSource> context) throws ScriptError, ScriptPermissionsException {
         Assert.notNull(compiledScript, "Script must be compiled first");
@@ -222,7 +222,7 @@ public class CompiledMangoJavaScript {
         this.service.initialize(this, context);
         this.initialized = true;
     }
-    
+
     /**
      * Execute this script expecting a PointValueTime result
      * @param runtime
@@ -240,7 +240,7 @@ public class CompiledMangoJavaScript {
         service.execute(this, runtime, timestamp, resultDataTypeId);
         return result;
     }
-    
+
     /**
      * Execute this script expecting any type of result
      * @param runtime
