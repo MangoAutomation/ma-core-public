901e643f14c69ace45c67693d82eddb1e82a7bc8
terrypacker <terrypacker@users.noreply.github.com>
2019-07-23 08:29:55 -1000

    Revert "Short circuit check for data source permission if user has global perm"
    This reverts commit 9330c4ca45939655d35567494d049d26074a9da3.

--- Core/src/com/serotonin/m2m2/vo/permission/Permissions.java
+++ Core/src/com/serotonin/m2m2/vo/permission/Permissions.java
@@ -109,32 +109,20 @@ public class Permissions {
     // Data source admin
     //
     public static void ensureDataSourcePermission(PermissionHolder user, int dsId) throws PermissionException {
-        try {
-            ensureDataSourcePermission(user);
-        }catch(PermissionException e) {
-            ensureDataSourcePermission(user, DataSourceDao.getInstance().get(dsId));
-        }
+        ensureDataSourcePermission(user, DataSourceDao.getInstance().get(dsId));
     }
 
     public static void ensureDataSourcePermission(PermissionHolder user, DataSourceVO<?> ds) throws PermissionException {
-        try {
-            ensureDataSourcePermission(user);
-        }catch(PermissionException e) {
-            if (!hasDataSourcePermission(user, ds))
-                throw new PermissionException(new TranslatableMessage("permission.exception.editDataSource", user.getPermissionHolderName()), user);
-        }
+        if (!hasDataSourcePermission(user, ds))
+            throw new PermissionException(new TranslatableMessage("permission.exception.editDataSource", user.getPermissionHolderName()), user);
     }
 
     public static boolean hasDataSourcePermission(PermissionHolder user, int dsId) throws PermissionException {
-        if(hasDataSourcePermission(user))
-            return true;
         String dsPermission = DataSourceDao.getInstance().getEditPermission(dsId);
         return hasAnyPermission(user, explodePermissionGroups(dsPermission));
     }
 
     public static boolean hasDataSourcePermission(PermissionHolder user, DataSourceVO<?> ds) throws PermissionException {
-        if(hasDataSourcePermission(user))
-            return true;
         return hasAnyPermission(user, explodePermissionGroups(ds.getEditPermission()));
     }
 
