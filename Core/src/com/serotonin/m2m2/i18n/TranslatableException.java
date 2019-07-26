4755d1fceef983a095909d5475c63ad6a7032bbe
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-07-26 14:57:14 -0600

    Remove constructors, check for null message

--- Core/src/com/serotonin/m2m2/i18n/TranslatableException.java
+++ Core/src/com/serotonin/m2m2/i18n/TranslatableException.java
@@ -4,6 +4,8 @@
  */
 package com.serotonin.m2m2.i18n;
 
+import java.util.Objects;
+
 import com.infiniteautomation.mango.util.exception.TranslatableExceptionI;
 import com.serotonin.m2m2.Common;
 
@@ -15,21 +17,13 @@ public class TranslatableException extends Exception implements TranslatableExce
 
     private TranslatableMessage translatableMessage = null;
 
-    public TranslatableException() {
-        super();
-    }
-
     public TranslatableException(TranslatableMessage message, Throwable cause) {
         super(cause);
-        this.translatableMessage = message;
+        this.translatableMessage = Objects.requireNonNull(message);
     }
 
     public TranslatableException(TranslatableMessage message) {
-        this.translatableMessage = message;
-    }
-
-    public TranslatableException(Throwable cause) {
-        super(cause);
+        this.translatableMessage = Objects.requireNonNull(message);
     }
 
     @Override
