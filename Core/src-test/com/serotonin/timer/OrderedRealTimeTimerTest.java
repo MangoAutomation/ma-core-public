9ed2454cc8e907c85190868894f01685da7a5ae0
terrypacker <terrypacker@users.noreply.github.com>
2019-09-11 16:38:38 +0100

    Add additional test for multi-threading

--- Core/src-test/com/serotonin/timer/OrderedRealTimeTimerTest.java
+++ Core/src-test/com/serotonin/timer/OrderedRealTimeTimerTest.java
@@ -4,7 +4,9 @@
  */
 package com.serotonin.timer;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -210,6 +212,81 @@ public class OrderedRealTimeTimerTest {
         Assert.assertEquals(taskQueueSize + 1, runs.get());
         Assert.assertEquals(totalExecutions - (taskQueueSize + 1), rejections.get());
         
+    }
+    
+    /**
+     * Test multiple threads submitting the same task
+     */
+    @Test
+    public void testMultipleSubmitThreads() {
+        
+        int executionsPerThread = 100;
+        int threads = 5;
+        int taskDuration = 50;
+        int queueSize = 5;
+        
+        SimulationTimer simTimer = new SimulationTimer(true);
+        ThreadPoolExecutor executor = new OrderedThreadPoolExecutor(
+                5, 
+                100, 
+                30L, 
+                TimeUnit.SECONDS, 
+                new SynchronousQueue<Runnable>(), 
+                false,
+                simTimer.getTimeSource());
+        
+        simTimer.init(executor);
+        
+        AtomicInteger runs = new AtomicInteger();
+        AtomicInteger rejections = new AtomicInteger();
+
+        List<Integer> taskOrder = new ArrayList<>();
+        AtomicInteger taskOrderId = new AtomicInteger();
+                
+        for(int i=0; i<threads; i++) {
+            new Thread() {
+                public void run() {
+                    for(int j=0; j<executionsPerThread; j++) {
+                        Task task = new Task("task1", "1", queueSize) {
+                            @Override
+                            public void run(long runtime) {
+                                runs.incrementAndGet();
+                                taskOrder.add(taskOrderId.incrementAndGet());
+                                try { Thread.sleep(taskDuration); } catch (InterruptedException e) { }
+                            }
+    
+                            @Override
+                            public void rejected(RejectedTaskReason reason) {
+                                rejections.incrementAndGet();
+                            }
+                        };
+                        simTimer.execute(task);
+                    }
+                }; 
+            }.start();
+        }
+        
+        int totalExecutions = threads * executionsPerThread;
+        int waits = 10;
+        while(waits > 0) {
+            if(runs.get() + rejections.get() == totalExecutions)
+                break;
+            try { Thread.sleep(100); }catch(InterruptedException e) {  }
+            waits--;
+        }
+        if(waits == 0)
+            Assert.fail("Didn't execute all scheduled tasks.");
+        
+        Assert.assertEquals(totalExecutions, runs.get() + rejections.get());
+        Assert.assertEquals(queueSize + 1, runs.get());
+
+        //Ensure order
+        Assert.assertEquals(1, (int)taskOrder.get(0));
+        Assert.assertEquals(2, (int)taskOrder.get(1));
+        Assert.assertEquals(3, (int)taskOrder.get(2));
+        Assert.assertEquals(4, (int)taskOrder.get(3));
+        Assert.assertEquals(5, (int)taskOrder.get(4));
+        Assert.assertEquals(6, (int)taskOrder.get(5));
     }
 	
 }
