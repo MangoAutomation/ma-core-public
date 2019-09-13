7671e35d6186afaac9a15d1b2dbde74a815b9e80
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-09-13 15:26:10 -0600

    Add method to get jetty server status

---
+++ Core/src/com/serotonin/m2m2/ServerStatus.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 Infinite Automation Software. All rights reserved.
+ */
+package com.serotonin.m2m2;
+
+/**
+ * @author Jared Wiltshire
+ */
+public class ServerStatus {
+    public static final ServerStatus NOT_RUNNING = new ServerStatus(0, 0, 0);
+
+    private final int threads;
+    private final int idleThreads;
+    private final int queueSize;
+
+    public ServerStatus(int threads, int idleThreads, int queueSize) {
+        super();
+        this.threads = threads;
+        this.idleThreads = idleThreads;
+        this.queueSize = queueSize;
+    }
+
+    public int getThreads() {
+        return threads;
+    }
+    public int getIdleThreads() {
+        return idleThreads;
+    }
+    public int getQueueSize() {
+        return queueSize;
+    }
+}
