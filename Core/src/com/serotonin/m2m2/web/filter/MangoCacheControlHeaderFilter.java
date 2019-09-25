619008fdc008b537fc285465a695510e1973cf5d
terrypacker <terrypacker@users.noreply.github.com>
2019-09-25 12:19:29 +0100

    Implement cache control header filter and overriding
    As per #1458
    
    Still need to discuss default settings for avoiding cache

--- Core/src/com/serotonin/m2m2/web/filter/MangoCacheControlHeaderFilter.java
+++ Core/src/com/serotonin/m2m2/web/filter/MangoCacheControlHeaderFilter.java
@@ -5,66 +5,52 @@
 package com.serotonin.m2m2.web.filter;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.RequestDispatcher;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.DigestUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.Constants;
 
 /**
- * Filter to apply ETag to static resources and cache control headers for all requests
+ * Filter to apply cache control headers for all requests
  * 
- * Configured via web.xml for now
+ * This filter is configured via web.xml for now
  * 
  * @author Terry Packer
  */
-public class MangoCacheControlHeaderFilter extends OncePerRequestFilter {
+public class MangoCacheControlHeaderFilter implements Filter {
 
+    public static final String CACHE_OVERRIDE_SETTING = "CACHE_OVERRIDE_SETTING";
+    public static final String CUSTOM_CONTROL_LEVEL = "CUSTOM_CONTROL_LEVEL";
     public static final String MAX_AGE_TEMPLATE = "max-age=%d, must-revalidate";
-    public static final String NO_STORE = "no-cache, no-store, max-age=0, must-revalidate";
-    private static final String DIRECTIVE_NO_STORE = "no-store";
     public static final Pattern VERSION_QUERY_PARAMETER = Pattern.compile("(?:^|&)v=");
-    private static final String HEADER_ETAG = "ETag";
-    private static final String HEADER_CACHE_CONTROL = "Cache-Control";
-    private static final String HEADER_IF_NONE_MATCH = "If-None-Match";
     
     final RequestMatcher restMatcher;
     final RequestMatcher resourcesMatcher;
     final RequestMatcher getMatcher;
-    //So we don't filter on the UI servlet requests we will filter on the forwarded request
     final RequestMatcher uiServletMatcher; 
-    final String defaultHeader;
-    final String restHeader;
-    final String resourcesHeader;
-    final String versionedResourcesHeader;
     
-    /**
-     * Set whether the ETag value written to the response should be weak, as per RFC 7232.
-     * <p>Should be configured using an {@code <init-param>} for parameter name
-     * "writeWeakETag" in the filter definition in {@code web.xml}.
-     * @since 4.3
-     * @see <a href="https://tools.ietf.org/html/rfc7232#section-2.3">RFC 7232 section 2.3</a>
-     */
-    private boolean writeWeakETag = false;
+    final CacheControl defaultNoCache;
 
     public MangoCacheControlHeaderFilter() {
         getMatcher = new AntPathRequestMatcher("/**", HttpMethod.GET.name());
         restMatcher = new AntPathRequestMatcher("/rest/**", HttpMethod.GET.name());
-
         resourcesMatcher = new OrRequestMatcher(
                 new AntPathRequestMatcher("/resources/**", HttpMethod.GET.name()),
                 new AntPathRequestMatcher("/modules/*/web/**", HttpMethod.GET.name()),
@@ -73,18 +59,10 @@ public class MangoCacheControlHeaderFilter extends OncePerRequestFilter {
                 new AntPathRequestMatcher("/dwr/**", HttpMethod.GET.name()));
 
         uiServletMatcher = new AntPathRequestMatcher("/ui/**", HttpMethod.GET.name());
-        
-        boolean noStore = Common.envProps.getBoolean("web.cache.noStore", false);
-        boolean restNoStore = Common.envProps.getBoolean("web.cache.noStore.rest", true);
-        boolean resourcesNoStore = Common.envProps.getBoolean("web.cache.noStore.resources", false);
-
-        defaultHeader = noStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge", 0L));
-        restHeader = restNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.rest", 0L));
-        resourcesHeader = resourcesNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.resources", 86400L));
-        versionedResourcesHeader = resourcesNoStore ? NO_STORE : String.format(MAX_AGE_TEMPLATE, Common.envProps.getLong("web.cache.maxAge.versionedResources", 31536000L));
+        //TODO This class is not designed to use no-store and no-cache via builder, do we really want to use those together here along with the max-age header?
+        defaultNoCache = CacheControl.noStore().mustRevalidate();
     }
 
-    @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         if(uiServletMatcher.matches(request)) {
             return true;
@@ -94,105 +72,143 @@ public class MangoCacheControlHeaderFilter extends OncePerRequestFilter {
     }
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String header = null;
-        if (restMatcher.matches(request)) {
-            header = restHeader;
-        } else if (resourcesMatcher.matches(request)) {
-            String queryString = request.getQueryString();
-            if (queryString != null && VERSION_QUERY_PARAMETER.matcher(queryString).find()) {
-                header = versionedResourcesHeader;
-            } else {
-                header = resourcesHeader;
-            }
-        } else if (getMatcher.matches(request)) {
-            header = defaultHeader;
-        } else {
-            header = NO_STORE;
+        if (!(servletRequest instanceof HttpServletRequest) || !(servletResponse instanceof HttpServletResponse)) {
+            throw new ServletException("MangoCacheControlHeaderFilter just supports HTTP requests");
         }
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.addHeader(HttpHeaders.CACHE_CONTROL, header);
-        
-        //Shall we generate an ETag?
-        if(isEligibleForEtag(request, response, response.getStatus())) {
-            String path = request.getRequestURI();
-            if(path.startsWith("/")) {
-                path = path.substring(1, path.length());
-            }
-            Path resourcePath = Common.MA_HOME_PATH.resolve(Constants.DIR_WEB).resolve(path);
-            if(resourcePath.toFile().exists()) {
-                //Compute hash of last modified since as ETag
-                long lastModified = resourcePath.toFile().lastModified();
-                String eTag = generateETagHeaderValue(lastModified, writeWeakETag);
-                response.setHeader(HEADER_ETAG, eTag);
-                //Compare ETag from client if provided and send not modified if same
-                String requestETag = request.getHeader(HEADER_IF_NONE_MATCH);
-                if (requestETag != null && ("*".equals(requestETag) || compareETagHeaderValue(requestETag, eTag))) {
-                    response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+        if(shouldNotFilter(request)) {
+            // Proceed without invoking this filter...
+            filterChain.doFilter(request, response);
+        }else {
+            String header = null;
+            
+            //Detect the type of settings to use, allow overriding the matching
+            CacheControlLevel override = null;
+            if(request.getAttribute(CACHE_OVERRIDE_SETTING) != null) {
+                Object o = request.getAttribute(CACHE_OVERRIDE_SETTING);
+                if(o instanceof CacheControlLevel) {
+                    override = (CacheControlLevel)o;
+                }
+            }else {
+                if (restMatcher.matches(request)) {
+                    override = CacheControlLevel.REST;
+                }else if (resourcesMatcher.matches(request)) {
+                    String queryString = request.getQueryString();
+                    if (queryString != null && VERSION_QUERY_PARAMETER.matcher(queryString).find()) {
+                        override = CacheControlLevel.VERSIONED_RESOURCE;
+                    }else {
+                        override = CacheControlLevel.RESOURCE;
+                    }
+                }else if (getMatcher.matches(request)) {
+                    override = CacheControlLevel.WEB;
+                }else {
+                    override = CacheControlLevel.DEFAULT;
                 }
             }
-            //If this was a forwarded 
-            String originalRequestURI = (String)request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI);
-            if (originalRequestURI != null) {
-                
+            
+            switch(override) {
+                case REST:
+                    if(Common.envProps.getBoolean("web.cache.noStore.rest", true)) {
+                        header = defaultNoCache.getHeaderValue();
+                    }else {
+                        header = CacheControl.maxAge(Common.envProps.getLong("web.cache.maxAge.rest", 0L), TimeUnit.SECONDS).mustRevalidate().getHeaderValue();
+                    }
+                    break;
+                case WEB:
+                    if(Common.envProps.getBoolean("web.cache.noStore", false)) {
+                        header = defaultNoCache.getHeaderValue();
+                    }else {
+                        header = CacheControl.maxAge(Common.envProps.getLong("web.cache.maxAge", 0L), TimeUnit.SECONDS).mustRevalidate().getHeaderValue();
+                    }
+                    break;
+                case VERSIONED_RESOURCE:
+                    if(Common.envProps.getBoolean("web.cache.noStore.resources", false)) {
+                        header = defaultNoCache.getHeaderValue();
+                    }else {
+                        header = CacheControl.maxAge(Common.envProps.getLong("web.cache.maxAge.versionedResources", 31536000L), TimeUnit.SECONDS).mustRevalidate().getHeaderValue();
+                    }
+                    break;
+                case RESOURCE:
+                    if(Common.envProps.getBoolean("web.cache.noStore.resources", false)) {
+                        header = defaultNoCache.getHeaderValue();
+                    }else {
+                        header = CacheControl.maxAge(Common.envProps.getLong("web.cache.maxAge.resources", 86400L), TimeUnit.SECONDS).mustRevalidate().getHeaderValue();
+                    }
+                    break;
+                case CUSTOM:
+                    header = (String)request.getAttribute(CUSTOM_CONTROL_LEVEL);
+                    Objects.requireNonNull(header, "Must supply CUSTOM_CONTROL_LEVEL attribute on request");
+                    break;
+                case DEFAULT:
+               default:
+                   header = defaultNoCache.getHeaderValue();
+                   break;
             }
+            response.setHeader(HttpHeaders.CACHE_CONTROL, header);
+            filterChain.doFilter(request, response);            
         }
-        filterChain.doFilter(request, response);
     }
-    
-    /**
-     * Indicates whether the given request and response are eligible for ETag generation.
-     * <p>The default implementation returns {@code true} if all conditions match:
-     * <ul>
-     * <li>response status codes in the {@code 2xx} series</li>
-     * <li>request method is a GET</li>
-     * <li>response Cache-Control header is not set or does not contain a "no-store" directive</li>
-     * </ul>
-     * @param request the HTTP request
-     * @param response the HTTP response
-     * @param responseStatusCode the HTTP response status code
-     * @return {@code true} if eligible for ETag generation, {@code false} otherwise
-     */
-    protected boolean isEligibleForEtag(HttpServletRequest request, HttpServletResponse response,
-            int responseStatusCode) {
 
-        String method = request.getMethod();
-        if (responseStatusCode >= 200 && responseStatusCode < 300 && HttpMethod.GET.matches(method)) {
-            String cacheControl = response.getHeader(HEADER_CACHE_CONTROL);
-            return (cacheControl == null || !cacheControl.contains(DIRECTIVE_NO_STORE));
-        }
-        return false;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+        
     }
     
-    /**
-     * Generate the ETag header value from the given response body byte array.
-     * <p>The default implementation generates an MD5 hash.
-     * @param inputStream the response body as an InputStream
-     * @param isWeak whether the generated ETag should be weak
-     * @return the ETag header value
-     * @see org.springframework.util.DigestUtils
-     */
-    protected String generateETagHeaderValue(long lastModified, boolean isWeak) throws IOException {
-        // length of W/ + " + 0 + 32bits md5 hash + "
-        StringBuilder builder = new StringBuilder(37);
-        if (isWeak) {
-            builder.append("W/");
-        }
-        builder.append("\"0");
-        builder.append(DigestUtils.md5DigestAsHex(Long.toString(lastModified).getBytes()));
-        builder.append('"');
-        return builder.toString();
-    }
-    
-    private boolean compareETagHeaderValue(String requestETag, String responseETag) {
-        if (requestETag.startsWith("W/")) {
-            requestETag = requestETag.substring(2);
-        }
-        if (responseETag.startsWith("W/")) {
-            responseETag = responseETag.substring(2);
-        }
-        return requestETag.equals(responseETag);
+    public static enum CacheControlLevel {
+        
+        /**
+         * Settings for all unmatched requests 
+         *  no-store, must-revalidate
+         */
+        DEFAULT,
+        
+        /**
+         * REST requests can be set by env properties
+         *  web.cache.noStore.rest=true -> no-store, must-revalidate
+         *  web.cache.maxAge.rest=0 -> max-age=0, must-revalidate
+         * Defaults: not-store, must-revalidate
+         */
+        REST,
+        
+        /**
+         * Web requests can be set by env properties
+         *  web.cache.noStore=true -> no-store, must-revalidate
+         *  web.cache.maxAge=0 -> max-age=0, must-revalidate
+         * Defaults: max-age=0, must-revalidate
+         */
+        WEB,
+        
+        /**
+         * Resource requests can be set by env properties
+         *  web.cache.noStore.resources=true -> no-store, must-revalidate
+         *  web.cache.maxAge.resources=86400 -> max-age=86400, must-revalidate
+         * Defaults: max-age=86400, must-revalidate
+         */
+        RESOURCE,
+        
+        /**
+         * Versioned resource requests can be set by env properties
+         *  web.cache.noStore.versionedResources=true -> no-store, must-revalidate
+         *  web.cache.maxAge.versionedResources=31536000 -> max-age=31536000, must-revalidate
+         * Defaults: max-age=31536000, must-revalidate
+         */
+        VERSIONED_RESOURCE,
+        
+        /**
+         * Allows to set the contents of the Cache-Control header by placing a String
+         *  into the CUSTOM_CONTROL_LEVEL attribute of the response
+         */
+        CUSTOM;
+       
     }
 }
