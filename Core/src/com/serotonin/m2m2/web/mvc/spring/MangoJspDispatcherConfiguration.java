99549328a9d3724fa64a65740888d00f3fa6e314
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-10 09:39:41 -0600

    Move BlabberUrlHandlerMapping from core-private

--- Core/src/com/serotonin/m2m2/web/mvc/spring/MangoJspDispatcherConfiguration.java
+++ Core/src/com/serotonin/m2m2/web/mvc/spring/MangoJspDispatcherConfiguration.java
@@ -4,6 +4,8 @@
  */
 package com.serotonin.m2m2.web.mvc.spring;
 
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +13,19 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.infiniteautomation.mango.spring.MangoCommonConfiguration;
+import com.serotonin.m2m2.Constants;
+import com.serotonin.m2m2.module.ControllerMappingDefinition;
+import com.serotonin.m2m2.module.HandlerInterceptorDefinition;
+import com.serotonin.m2m2.module.UriMappingDefinition;
+import com.serotonin.m2m2.module.UrlMappingDefinition;
 import com.serotonin.m2m2.web.mvc.BlabberUrlHandlerMapping;
+import com.serotonin.m2m2.web.mvc.UrlHandler;
+import com.serotonin.m2m2.web.mvc.UrlHandlerController;
 import com.serotonin.m2m2.web.mvc.interceptor.CommonDataInterceptor;
 import com.serotonin.m2m2.web.mvc.spring.security.MangoMethodSecurityConfiguration;
 import com.serotonin.propertyEditor.DefaultMessageCodesResolver;
@@ -31,6 +42,13 @@ import com.serotonin.propertyEditor.DefaultMessageCodesResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.serotonin.m2m2.web.mvc.controller" })
 public class MangoJspDispatcherConfiguration implements WebMvcConfigurer {
+
+    private final ListableBeanFactory beanFactory;
+
+    @Autowired
+    public MangoJspDispatcherConfiguration(ListableBeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
 
     @Bean(name="viewResolver")
     public InternalResourceViewResolver internalResourceViewResolver(){
@@ -49,10 +67,54 @@ public class MangoJspDispatcherConfiguration implements WebMvcConfigurer {
         return new CommonDataInterceptor();
     }
 
+    @SuppressWarnings("deprecation")
     @Bean(name="mappings")
-    public BlabberUrlHandlerMapping blabberUrlHandlerMapping(CommonDataInterceptor commonDataInterceptor){
+    public BlabberUrlHandlerMapping blabberUrlHandlerMapping(CommonDataInterceptor commonDataInterceptor) {
         BlabberUrlHandlerMapping mapping = new BlabberUrlHandlerMapping();
         mapping.addInterceptor(commonDataInterceptor);
+
+        // Add interceptors
+        for (HandlerInterceptorDefinition def : beanFactory.getBeansOfType(HandlerInterceptorDefinition.class).values()) {
+            mapping.addInterceptor(def.getInterceptor());
+        }
+        mapping.initInterceptors();
+
+        for (UriMappingDefinition def : beanFactory.getBeansOfType(UriMappingDefinition.class).values()) {
+
+            String modulePath;
+            if (def.getModule() == null) // Core mapping
+                modulePath = "/";
+            else
+                modulePath = "/" + Constants.DIR_MODULES + "/" + def.getModule().getName();
+
+            String viewName = null;
+            if (def.getJspPath() != null)
+                viewName = modulePath + "/" + def.getJspPath();
+
+            UrlHandler handler = def.getHandler();
+            Controller controller = new UrlHandlerController(handler, modulePath, viewName);
+
+            mapping.registerHandler(def.getPath(), controller);
+        }
+
+        //Add The ControllerMappingDefinitions
+        for(ControllerMappingDefinition def: beanFactory.getBeansOfType(ControllerMappingDefinition.class).values()) {
+            mapping.registerHandler(def.getPath(), def.getController());
+        }
+
+        // Add url mappings, TODO Remove this when we finally get rid of UrlMappingDefinition
+        for (UrlMappingDefinition def : beanFactory.getBeansOfType(UrlMappingDefinition.class).values()) {
+            String modulePath = "/" + Constants.DIR_MODULES + "/" + def.getModule().getName();
+            String viewName = null;
+            if (def.getJspPath() != null)
+                viewName = modulePath + "/" + def.getJspPath();
+
+            UrlHandler handler = def.getHandler();
+            Controller controller = new UrlHandlerController(handler, modulePath, viewName);
+
+            mapping.registerHandler(def.getUrlPath(), controller);
+        }
+
         return mapping;
     }
 
