4a82d2345ed60361502f1fdb584a931dd2ee458a
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-10 10:28:00 -0600

    Add configuration for JSP servlet

---
+++ Core/src/com/infiniteautomation/mango/spring/webapp/JspServletInitializer.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.spring.webapp;
+
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jared Wiltshire
+ */
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Order(300)
+public class JspServletInitializer extends JettyJasperInitializer {
+
+    private final Environment env;
+
+    @Autowired
+    private JspServletInitializer(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void onStartup(Set<Class<?>> types, ServletContext context) throws ServletException {
+        super.onStartup(types, context);
+
+        //ServletRegistration.Dynamic holderJsp = context.addServlet("jsp", JettyJspServlet.class);
+        ServletRegistration holderJsp = context.getServletRegistration("jsp");
+        //holderJsp.addMapping("*.jsp");
+        //holderJsp.setAsyncSupported(env.getProperty("web.jsp.async", Boolean.class, true));
+        //holderJsp.setLoadOnStartup(0);
+
+        //Get the descriptions here: http://www.eclipse.org/jetty/documentation/9.2.10.v20150310/configuring-jsp.html
+        holderJsp.setInitParameter("xpoweredBy", "false");
+        holderJsp.setInitParameter("compilerTargetVM", "1.8");
+        holderJsp.setInitParameter("compilerSourceVM", "1.8");
+
+        //Env Configurable
+        holderJsp.setInitParameter("fork", env.getProperty("web.jsp.fork", "false"));
+        holderJsp.setInitParameter("keepgenerated", env.getProperty("web.jsp.keepgenerated", "true"));
+        holderJsp.setInitParameter("development", env.getProperty("web.jsp.development", "true"));
+        holderJsp.setInitParameter("modificationTestInterval", env.getProperty("web.jsp.modificationTestInterval", "4"));
+        holderJsp.setInitParameter("genStringAsCharArray", env.getProperty("web.jsp.genStringAsCharArray", "true"));
+        holderJsp.setInitParameter("trimSpaces", env.getProperty("web.jsp.trimSpaces", "false"));
+        holderJsp.setInitParameter("classdebuginfo", env.getProperty("web.jsp.classdebuginfo", "false"));
+        holderJsp.setInitParameter("suppressSmap", env.getProperty("web.jsp.suppressSmap", "true"));
+
+        /*
+         * org.apache.jasper.compiler.AntCompiler (see below for options)
+         * or
+         * org.apache.jasper.compiler.JDTCompiler (no compiler options)
+         */
+        holderJsp.setInitParameter("compilerClassName", env.getProperty("web.jsp.compilerClassName", "org.apache.jasper.compiler.JDTCompiler"));
+        /*
+         * Compiler Options for AntCompiler
+         * classic - legacy JDK compiler for older 1.1/1.2 java versions
+         * modern - Standard JDK Compiler
+         * extJavac - JDK Compiler in JVM of its own
+         * jikes - Jikes compiler
+         * kjc - kopi compiler
+         * gcj - gcj compiler from gcc
+         */
+        holderJsp.setInitParameter("compiler", env.getProperty("web.jsp.compiler", "modern"));
+    }
+}
