c62897339236c51aec6616b214e2152e0e6d9a7d
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-10 12:43:42 -0600

    Add a common interface

---
+++ Core/src/com/serotonin/m2m2/module/DwrClassHolder.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2019 Infinite Automation Software. All rights reserved.
+ */
+package com.serotonin.m2m2.module;
+
+/**
+ * @author Jared Wiltshire
+ */
+public interface DwrClassHolder {
+
+    /**
+     * The class of a DWR proxy. Called once upon startup, the class is then registered in the DWR controller. May be
+     * null.
+     *
+     * @return the DWR proxy
+     */
+    Class<?> getDwrClass();
+
+}
