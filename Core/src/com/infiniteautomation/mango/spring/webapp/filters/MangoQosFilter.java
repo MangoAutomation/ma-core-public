a59c26b55a4173b6b199fb20784fe79124afd793
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-10 10:45:56 -0600

    Add Terry's change
    infiniteautomation/ma-core-private@4f1604dec87646b0d27046e6f2210c86de36d636

--- Core/src/com/infiniteautomation/mango/spring/webapp/filters/MangoQosFilter.java
+++ Core/src/com/infiniteautomation/mango/spring/webapp/filters/MangoQosFilter.java
@@ -3,13 +3,22 @@
  */
 package com.infiniteautomation.mango.spring.webapp.filters;
 
+import java.security.Principal;
+
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.eclipse.jetty.servlets.QoSFilter;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.vo.User;
+import com.serotonin.m2m2.vo.permission.Permissions;
 
 /**
  * https://www.eclipse.org/jetty/documentation/current/qos-filter.html
@@ -31,4 +40,42 @@ import org.springframework.stereotype.Component;
 @Order(200)
 public class MangoQosFilter extends QoSFilter {
     public static final String NAME = "mangoQosFilter";
+
+    /**
+     * Computes the request priority.
+     * <p>
+     * The default implementation assigns the following priorities:
+     * <ul>
+     * <li> 3 - for an authenticated superadmin request
+     * <li> 2 - for an authenticated request
+     * <li> 1 - for a request with valid / non new session
+     * <li> 0 - for all other requests.
+     * </ul>
+     * This method may be overridden to provide application specific priorities.
+     *
+     * @param request the incoming request
+     * @return the computed request priority
+     */
+    @Override
+    protected int getPriority(ServletRequest request) {
+        // Override default priority levels to ensure superadmin gets the highest priority
+        HttpServletRequest baseRequest = (HttpServletRequest)request;
+        Principal principle = baseRequest.getUserPrincipal();
+        if (principle != null) {
+            //are we a mango user
+            User user = Common.getHttpUser();
+            if(user != null) {
+                return Permissions.hasAdminPermission(user) ? 3 : 2;
+            }else {
+                return 2;
+            }
+        } else {
+            HttpSession session = baseRequest.getSession(false);
+            if (session != null && !session.isNew()) {
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+    }
 }
