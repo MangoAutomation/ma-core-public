687a9e49c657d3201a142e5eeef3df7457d64668
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-16 15:17:34 -0600

    Fix concurrency issues

--- Core/src/com/serotonin/m2m2/vo/systemSettings/SystemSettingsEventDispatcher.java
+++ Core/src/com/serotonin/m2m2/vo/systemSettings/SystemSettingsEventDispatcher.java
@@ -4,10 +4,11 @@
  */
 package com.serotonin.m2m2.vo.systemSettings;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.rt.maint.work.WorkItem;
@@ -20,25 +21,26 @@ import com.serotonin.timer.RejectedTaskReason;
  */
 public class SystemSettingsEventDispatcher {
 
-	private final static int SETTING_SAVED = 1;
-	private final static int SETTING_REMOVED = 2;
-	
-    private final static Map<String, List<SystemSettingsListener>> LISTENER_MAP = new ConcurrentHashMap<String, List<SystemSettingsListener>>();
-	
+    private enum SystemSettingEventType {
+        SETTING_SAVED, SETTING_REMOVED
+    }
+
+    private final static Map<String, Set<SystemSettingsListener>> LISTENER_MAP = new ConcurrentHashMap<String, Set<SystemSettingsListener>>();
+
     /**
      * Add the listener to whatever keys it should listen to
      * @param l
      */
     public static void addListener(SystemSettingsListener l) {
         //Add the listener for all supported keys
-    	List<SystemSettingsListener> list;
         for (String key : l.getKeys()) {
-            list = LISTENER_MAP.get(key);
-            if (list == null) {
-                list = new CopyOnWriteArrayList<SystemSettingsListener>();
-                LISTENER_MAP.put(key, list);
-            }
-            list.add(l);
+            LISTENER_MAP.compute(key, (k, v) -> {
+                if (v == null) {
+                    v = new HashSet<>();
+                }
+                v.add(l);
+                return v;
+            });
         }
     }
 
@@ -48,17 +50,15 @@ public class SystemSettingsEventDispatcher {
      */
     public static void removeListener(SystemSettingsListener l) {
         //Remove the listener for any keys
-    	List<SystemSettingsListener> list;
-    	for(String key : l.getKeys()){
-    		list = LISTENER_MAP.get(key);
-    		if(list != null){
-    			//Remove me from the list
-    			list.remove(l);
-    			//If the list is empty the remove it from the map
-    			if(list.size() == 0)
-    				LISTENER_MAP.remove(key);
-    		}
-    	}
+        for(String key : l.getKeys()){
+            LISTENER_MAP.computeIfPresent(key, (k, v) -> {
+                v.remove(l);
+                if (v.isEmpty()) {
+                    return null;
+                }
+                return v;
+            });
+        }
     }
 
     /**
@@ -68,12 +68,11 @@ public class SystemSettingsEventDispatcher {
      * @param newValue
      */
     public static void fireSystemSettingSaved(String key, String oldValue, String newValue) {
-        List<SystemSettingsListener> listeners = LISTENER_MAP.get(key);
-        if (listeners != null)
-            for (SystemSettingsListener l : listeners) {
-                Common.backgroundProcessing.addWorkItem(
-                        new DispatcherExecution(l, key, oldValue, newValue, SETTING_SAVED));
-            }
+        Set<SystemSettingsListener> listeners = LISTENER_MAP.getOrDefault(key, Collections.emptySet());
+        for (SystemSettingsListener l : listeners) {
+            Common.backgroundProcessing.addWorkItem(
+                    new DispatcherExecution(l, key, oldValue, newValue, SystemSettingEventType.SETTING_SAVED));
+        }
     }
 
     /**
@@ -82,23 +81,23 @@ public class SystemSettingsEventDispatcher {
      * @param lastValue
      */
     public static void fireSystemSettingRemoved(String key, String lastValue, String defaultValue) {
-        List<SystemSettingsListener> listeners = LISTENER_MAP.get(key);
-        if (listeners != null)
-            for (SystemSettingsListener l : listeners)
-                Common.backgroundProcessing.addWorkItem(
-                        new DispatcherExecution(l, key, lastValue, defaultValue, SETTING_REMOVED));
+        Set<SystemSettingsListener> listeners = LISTENER_MAP.getOrDefault(key, Collections.emptySet());
+        for (SystemSettingsListener l : listeners) {
+            Common.backgroundProcessing.addWorkItem(
+                    new DispatcherExecution(l, key, lastValue, defaultValue, SystemSettingEventType.SETTING_REMOVED));
+        }
     }
 
     static class DispatcherExecution implements WorkItem {
-    	private final String description = "System settings event dispatcher for: ";
+        private final String description = "System settings event dispatcher for: ";
         private final SystemSettingsListener l;
         private final String key;
         private final String oldValue;
         private final String newValue;
-        private final int operation;
+        private final SystemSettingEventType operation;
 
-        public DispatcherExecution(SystemSettingsListener l, String key,  String oldValue, String newValue, int operation) {
-        	this.l = l;
+        public DispatcherExecution(SystemSettingsListener l, String key,  String oldValue, String newValue, SystemSettingEventType operation) {
+            this.l = l;
             this.key = key;
             this.oldValue = oldValue;
             this.newValue = newValue;
@@ -117,45 +116,31 @@ public class SystemSettingsEventDispatcher {
             }
         }
 
-		/* (non-Javadoc)
-		 * @see com.serotonin.m2m2.rt.maint.work.WorkItem#getPriority()
-		 */
-		@Override
-		public int getPriority() {
-			return WorkItem.PRIORITY_MEDIUM;
-		}
+        @Override
+        public int getPriority() {
+            return WorkItem.PRIORITY_MEDIUM;
+        }
 
-		/* (non-Javadoc)
-		 * @see com.serotonin.m2m2.rt.maint.work.WorkItem#getDescription()
-		 */
-		@Override
-		public String getDescription() {
-			return description + key;
-		}
+        @Override
+        public String getDescription() {
+            return description + key;
+        }
 
-		private final String TASK_ID = "SYSTEM_SETTINGS_LISTENER_";
-		/* (non-Javadoc)
-		 * @see com.serotonin.m2m2.rt.maint.work.WorkItem#getTaskId()
-		 */
-		@Override
-		public String getTaskId() {
-			return TASK_ID + key ;
-		}
+        private final String TASK_ID = "SYSTEM_SETTINGS_LISTENER_";
 
-		/* (non-Javadoc)
-		 * @see com.serotonin.m2m2.rt.maint.work.WorkItem#getQueueSize()
-		 */
-		@Override
-		public int getQueueSize() {
-			return 100;
-		}
+        @Override
+        public String getTaskId() {
+            return TASK_ID + key ;
+        }
 
-		/* (non-Javadoc)
-		 * @see com.serotonin.m2m2.rt.maint.work.WorkItem#rejected(com.serotonin.timer.RejectedTaskReason)
-		 */
-		@Override
-		public void rejected(RejectedTaskReason reason) {
-			//No special handling
-		}
+        @Override
+        public int getQueueSize() {
+            return 100;
+        }
+
+        @Override
+        public void rejected(RejectedTaskReason reason) {
+            //No special handling
+        }
     }
 }
