344d099e19db974ffb0034cf66621b0764f25568
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-10-17 12:15:17 -0600

    Add note

--- Core/src/com/serotonin/m2m2/vo/systemSettings/SystemSettingsListener.java
+++ Core/src/com/serotonin/m2m2/vo/systemSettings/SystemSettingsListener.java
@@ -14,26 +14,27 @@ import java.util.List;
  */
 public interface SystemSettingsListener {
 
-	/**
-	 * A setting is being saved
-	 * @param key
-	 * @param oldValue
-	 * @param newValue
-	 */
-	public void systemSettingsSaved(String key, String oldValue, String newValue);
-	
-	/**
-	 * A setting is being removed
-	 * @param key
-	 * @param lastValue
-	 * @param the default value for the setting, which is now the value
-	 */
-	public void systemSettingsRemoved(String key, String lastValue, String defaultValue);
+    /**
+     * A setting is being saved
+     * @param key
+     * @param oldValue
+     * @param newValue
+     */
+    public void systemSettingsSaved(String key, String oldValue, String newValue);
 
-	/**
-	 * Return a list of any settings you want to listen for changes to
-	 * @return
-	 */
-	public List<String> getKeys();
-	
+    /**
+     * A setting is being removed
+     * @param key
+     * @param lastValue
+     * @param the default value for the setting, which is now the value
+     */
+    public void systemSettingsRemoved(String key, String lastValue, String defaultValue);
+
+    /**
+     * Return a list of any settings you want to listen for changes to.
+     * Must remain constant across invocations.
+     * @return
+     */
+    public List<String> getKeys();
+
 }
