d301d5be1a2b0e8ea49764e21a9bf75284877cb6
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-11-06 12:36:59 -0700

    Resolve keystore relative to MA_HOME
    Fix quoting around variables and change get_prop function

--- Core/scripts/getenv.sh
+++ Core/scripts/getenv.sh
@@ -11,77 +11,63 @@ if [ -z "$SCRIPT_DIR" ]; then
 	SCRIPT="$0"
 	[ -x "$(command -v greadlink)" ] && alias readlink=greadlink
 	[ -x "$(command -v readlink)" ] && SCRIPT="$(readlink -f "$SCRIPT")"
-	SCRIPT_DIR="$(dirname "$SCRIPT")"
+	SCRIPT_DIR="$(dirname -- "$SCRIPT")"
 fi
 
-# function for getting values from .properties file
-getProp() {
-  awk -F "=" "/^$2=/ {print "'$2'"; exit; }" "$1"
+# function for getting values from env.properties file
+# does not support multiline properties or properties containing = sign
+get_prop() {
+	awk -v name="$1" -v dval="$2" -F = '$1==name{print $2; f=1; exit} END{if(f!=1){print dval}}' "$MA_ENV_PROPERTIES"
 }
 
-if [ -z "$MA_HOME" ]
-then
-    possible_ma_home="$(dirname "$SCRIPT_DIR")"
-    if [ -e "$possible_ma_home/release.signed" ] || [ -e "$possible_ma_home/release.properties" ]
-    then
-        MA_HOME="$possible_ma_home"
-    else
-        MA_HOME=/opt/mango
-    fi
-fi
+is_relative() {
+	case "$1" in /*) return 1;; esac
+}
 
-if [ -z "$MA_ENV_PROPERTIES" ]
-then
-    MA_ENV_PROPERTIES="$MA_HOME/overrides/properties/env.properties"
-fi
+resolve_path() {
+	if is_relative "$2"; then
+		printf %s/ "$1"
+	fi
+	printf %s "$2"
+}
 
-if [ -z "$MA_KEYSTORE" ]
-then
-    if [ -e "$MA_ENV_PROPERTIES" ]
-    then
-        MA_KEYSTORE=$(getProp "$MA_ENV_PROPERTIES" "ssl.keystore.location")
-    fi
-
-    if [ -z "$MA_KEYSTORE" ]
-	then
-	    MA_KEYSTORE="$MA_HOME/overrides/keystore.jks"
+if [ -z "$MA_HOME" ]; then
+	possible_ma_home="$(dirname -- "$SCRIPT_DIR")"
+	if [ -e "$possible_ma_home/release.signed" ] || [ -e "$possible_ma_home/release.properties" ]; then
+		MA_HOME="$possible_ma_home"
+	else
+		MA_HOME=/opt/mango
 	fi
 fi
 
-if [ -z "$MA_KEYSTORE_PASSWORD" ]
-then
-    if [ -e "$MA_ENV_PROPERTIES" ]
-    then
-        MA_KEYSTORE_PASSWORD=$(getProp "$MA_ENV_PROPERTIES" "ssl.keystore.password")
-    fi
-
-    if [ -z "$MA_KEYSTORE_PASSWORD" ]
-    then
-        MA_KEYSTORE_PASSWORD=freetextpassword
-    fi
+if [ -z "$MA_ENV_PROPERTIES" ]; then
+	MA_ENV_PROPERTIES="$MA_HOME/overrides/properties/env.properties"
 fi
 
-if [ -z "$MA_KEY_PASSWORD" ]
-then
-    if [ -e "$MA_ENV_PROPERTIES" ]
-    then
-        MA_KEY_PASSWORD=$(getProp "$MA_ENV_PROPERTIES" "ssl.key.password")
-    fi
-
-    if [ -z "$MA_KEY_PASSWORD" ]
-    then
-        MA_KEY_PASSWORD="$MA_KEYSTORE_PASSWORD"
-    fi
+if [ ! -e "$MA_ENV_PROPERTIES" ]; then
+	echo "Can't find env.properties file"
+	exit 1
 fi
 
-if [ -z "$MA_KEY_ALIAS" ]
-then
-    MA_KEY_ALIAS=mango
+if [ -z "$MA_KEYSTORE" ]; then
+	keystore="$(get_prop "ssl.keystore.location" "$MA_HOME/overrides/keystore.jks")"
+	MA_KEYSTORE="$(resolve_path "$MA_HOME" "$keystore")"
+fi
+
+if [ -z "$MA_KEYSTORE_PASSWORD" ]; then
+	MA_KEYSTORE_PASSWORD="$(get_prop 'ssl.keystore.password' 'freetextpassword')"
+fi
+
+if [ -z "$MA_KEY_PASSWORD" ]; then
+	MA_KEY_PASSWORD="$(get_prop 'ssl.key.password' "$MA_KEYSTORE_PASSWORD")"
+fi
+
+if [ -z "$MA_KEY_ALIAS" ]; then
+	MA_KEY_ALIAS=mango
 fi
 
 keytool_cmd="$JAVA_HOME/bin/keytool"
-if [ -z "$JAVA_HOME" ] || [ ! -x "$keytool_cmd" ]
-then
-    # use keytool from PATH
-    keytool_cmd=keytool
+if [ -z "$JAVA_HOME" ] || [ ! -x "$keytool_cmd" ]; then
+	# use keytool from PATH
+	keytool_cmd=keytool
 fi
