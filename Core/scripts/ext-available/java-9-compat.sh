3944137e19fd1d78d425d116af5778d6c0ceb3db
terrypacker <terrypacker@users.noreply.github.com>
2019-11-20 12:02:36 -1000

    Remove Java 9 Compatibility scripts as they are not necessary anymore
    As per #1298

--- Core/scripts/ext-available/java-9-compat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-case "$1" in
-    init)
-    	# Add required java modules not inherently loaded in Java 9, comma separated
-        JAVAOPTS="$JAVAOPTS ---add-modules java.activation,java.xml.bind"
-        ;;
-esac
