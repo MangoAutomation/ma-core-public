e92812c47a39418a05735550f33cbca0a5feb32c
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-12-09 17:02:30 -0700

    Allow multiple values for ConditionalOnProperty annotation

--- Core/src/com/infiniteautomation/mango/spring/ConditionalOnProperty.java
+++ Core/src/com/infiniteautomation/mango/spring/ConditionalOnProperty.java
@@ -17,22 +17,36 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 import com.infiniteautomation.mango.spring.ConditionalOnProperty.ConditionalOnPropertyCondition;
 
 /**
+ * Provides a way to conditionally enable a Spring component/service based on a property (env.properties, environment variable, Java system property)
+ *
  * @author Jared Wiltshire
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Conditional(ConditionalOnPropertyCondition.class)
 public @interface ConditionalOnProperty {
-    String value();
+    /**
+     * @return Values are resolved via the Spring {@link org.springframework.core.env.PropertyResolver#resolveRequiredPlaceholders(String) PropertyResolver}
+     * and parsed as boolean values. If multiple values are specified they must all evaluate to true in order to enable the component.<br>
+     * e.g. <code>${testing.enabled:false}</code>
+     */
+    String[] value();
 
     public static class ConditionalOnPropertyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
             try {
                 Map<String, Object> annotationProperties = metadata.getAnnotationAttributes(ConditionalOnProperty.class.getName());
-                String value = (String) annotationProperties.get("value");
-                String result = context.getEnvironment().resolveRequiredPlaceholders(value);
-                return Boolean.parseBoolean(result);
+                String[] values = (String[]) annotationProperties.get("value");
+
+                for (String value : values) {
+                    String result = context.getEnvironment().resolveRequiredPlaceholders(value);
+                    if (!Boolean.parseBoolean(result)) {
+                        return false;
+                    }
+                }
+
+                return true;
             } catch (Exception e) {
                 return false;
             }
