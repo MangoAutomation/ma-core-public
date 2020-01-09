ab26555940e11229a1d74e4f6b3434fe541b68cb
terrypacker <terrypacker@users.noreply.github.com>
2020-01-09 13:35:36 -1000

    Make functional interface

--- Core/src/com/serotonin/db/MappedRowCallback.java
+++ Core/src/com/serotonin/db/MappedRowCallback.java
@@ -1,5 +1,14 @@
+/**
+ * Copyright (C) 2016 Infinite Automation Software. All rights reserved.
+ */
 package com.serotonin.db;
 
+/**
+ * Callback for a row mapped to an Object
+ *
+ *
+ */
+@FunctionalInterface
 public interface MappedRowCallback<T> {
     void row(T item, int index);
 }
