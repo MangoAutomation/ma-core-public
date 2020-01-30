6c6f7c262e94f5497d68f028f1d64ce4e17f4b08
terrypacker <terrypacker@users.noreply.github.com>
2020-01-29 15:15:07 -1000

    Fix up double joining in customizedQuery

--- Core/src/com/serotonin/m2m2/db/dao/AbstractBasicDao.java
+++ Core/src/com/serotonin/m2m2/db/dao/AbstractBasicDao.java
@@ -387,7 +387,6 @@ public abstract class AbstractBasicDao<T extends AbstractBasicVO, TABLE extends 
     @Override
     public void customizedQuery(ConditionSortLimit conditions, MappedRowCallback<T> callback) {
         SelectJoinStep<Record> select = getJoinedSelectQuery();
-        select = joinTables(select, conditions);
         customizedQuery(select, conditions.getCondition(), conditions.getSort(), conditions.getLimit(), conditions.getOffset(), callback);
     }
 
