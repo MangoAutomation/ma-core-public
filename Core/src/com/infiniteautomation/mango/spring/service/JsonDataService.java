5ee9d8eb3c436895771e26bfbf215c49d4a053c2
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-02-06 17:01:27 -0700

    Add methods for getting/setting using JSON pointer

--- Core/src/com/infiniteautomation/mango/spring/service/JsonDataService.java
+++ Core/src/com/infiniteautomation/mango/spring/service/JsonDataService.java
@@ -4,9 +4,15 @@
 package com.infiniteautomation.mango.spring.service;
 
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.MissingNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.infiniteautomation.mango.spring.db.JsonDataTableDefinition;
 import com.infiniteautomation.mango.util.exception.NotFoundException;
 import com.serotonin.m2m2.db.dao.JsonDataDao;
@@ -91,4 +97,71 @@ public class JsonDataService extends AbstractVOService<JsonDataVO, JsonDataTable
         return result;
     }
 
+    private static final Pattern UNESCAPE_JSON_PTR_SLASH = Pattern.compile("~1");
+    private static final Pattern UNESCAPE_JSON_PTR_TILDE = Pattern.compile("~0");
+
+    /**
+     * Get data inside an item using a JSON pointer
+     *
+     * @param xid
+     * @param pointer RFC 6901 JSON pointer
+     * @return
+     */
+    public JsonNode getDataAtPointer(String xid, String pointer) {
+        JsonNode data = this.get(xid).getJsonData();
+
+        if (data == null || data instanceof MissingNode) {
+            throw new NotFoundException();
+        }
+
+        if (pointer == null || pointer.isEmpty()) {
+            return data;
+        }
+
+        JsonPointer ptr = JsonPointer.compile(pointer);
+        JsonNode dataAtPtr = data.at(ptr);
+        if (dataAtPtr instanceof MissingNode) {
+            throw new NotFoundException();
+        }
+        return dataAtPtr;
+    }
+
+    /**
+     * Set data inside an item using a JSON pointer
+     *
+     * @param xid
+     * @param pointer RFC 6901 JSON pointer
+     * @param data
+     */
+    public void setDataAtPointer(String xid, String pointer, JsonNode data) {
+        JsonDataVO item = this.get(xid);
+
+        if (pointer == null || pointer.isEmpty()) {
+            item.setJsonData(data);
+            this.update(xid, item);
+            return;
+        }
+
+        JsonPointer ptr = JsonPointer.compile(pointer);
+        JsonNode existing = item.getJsonData();
+        JsonNode parent = existing.at(ptr.head());
+
+        if (parent instanceof MissingNode) {
+            throw new NotFoundException();
+        }
+
+        String property = ptr.last().toString().substring(1);
+        property = UNESCAPE_JSON_PTR_SLASH.matcher(property).replaceAll("/");
+        property = UNESCAPE_JSON_PTR_TILDE.matcher(property).replaceAll("~");
+
+        if (parent instanceof ObjectNode) {
+            ((ObjectNode) parent).set(property, data);
+        } else if (parent instanceof ArrayNode) {
+            ((ArrayNode) parent).set(Integer.parseInt(property), data);
+        } else {
+            throw new RuntimeException("Cant set property of " + parent.getClass().getSimpleName());
+        }
+
+        this.update(xid, item);
+    }
 }
