46c01641e88489d66f35bb0cbca0b7d0aa76ce74
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-02-10 13:31:22 -0700

    Allow object/array values

--- Core/src/com/infiniteautomation/mango/db/query/pojo/RQLFilterJsonNode.java
+++ Core/src/com/infiniteautomation/mango/db/query/pojo/RQLFilterJsonNode.java
@@ -3,6 +3,12 @@
  */
 package com.infiniteautomation.mango.db.query.pojo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import net.jazdw.rql.parser.ASTNode;
@@ -22,8 +28,25 @@ public class RQLFilterJsonNode extends RQLFilter<JsonNode> {
     @Override
     protected Object getProperty(JsonNode item, String property) {
         JsonNode node = item.get(property);
+        return toJavaNative(node);
+    }
 
-        if (node.isTextual()) {
+    public static Object toJavaNative(JsonNode node) {
+        if (node.isArray()) {
+            ArrayList<Object> list = new ArrayList<>(node.size());
+            for (JsonNode child : node) {
+                list.add(toJavaNative(child));
+            }
+            return list;
+        } else if (node.isObject()) {
+            Map<String, Object> map = new HashMap<>(node.size());
+            Iterator<Entry<String, JsonNode>> it = node.fields();
+            while (it.hasNext()) {
+                Entry<String, JsonNode> entry = it.next();
+                map.put(entry.getKey(), toJavaNative(entry.getValue()));
+            }
+            return map;
+        } else if (node.isTextual()) {
             return node.textValue();
         } else if (node.isNumber()) {
             return node.numberValue();
