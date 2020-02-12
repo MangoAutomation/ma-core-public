026036df217c98908faa4e1268e4006ee211342c
terrypacker <terrypacker@users.noreply.github.com>
2020-02-11 14:22:42 -1000

    Clean up formatting

--- Core/src/com/infiniteautomation/mango/io/serial/virtual/SerialSocketBridgeConfig.java
+++ Core/src/com/infiniteautomation/mango/io/serial/virtual/SerialSocketBridgeConfig.java
@@ -17,86 +17,78 @@ import com.serotonin.m2m2.i18n.ProcessResult;
  */
 public class SerialSocketBridgeConfig extends VirtualSerialPortConfig{
 
-	@JsonProperty
-	private String address;
-	
-	@JsonProperty
-	private int port;
-	
-	@JsonProperty
-	private int timeout;//in milliseconds
-	
-	/**
-	 * 
-	 * @param name
-	 * @param address
-	 * @param port
-	 * @param timeout (ms)
-	 */
-	public SerialSocketBridgeConfig(String xid, String name, String address, int port, int timeout){
-		super(xid, name, SerialPortTypes.SERIAL_SOCKET_BRIDGE);
-		this.address = address;
-		this.port = port;
-		this.timeout = timeout;
-	}
-	
-	public SerialSocketBridgeConfig() {}
-	
-	
-	@Override
-	public void validate(ProcessResult response){
-		super.validate(response);
-		if (timeout < 0)
+    @JsonProperty
+    private String address;
+
+    @JsonProperty
+    private int port;
+
+    @JsonProperty
+    private int timeout;//in milliseconds
+
+    /**
+     *
+     * @param name
+     * @param address
+     * @param port
+     * @param timeout (ms)
+     */
+    public SerialSocketBridgeConfig(String xid, String name, String address, int port, int timeout){
+        super(xid, name, SerialPortTypes.SERIAL_SOCKET_BRIDGE);
+        this.address = address;
+        this.port = port;
+        this.timeout = timeout;
+    }
+
+    public SerialSocketBridgeConfig() {}
+
+
+    @Override
+    public void validate(ProcessResult response){
+        super.validate(response);
+        if (timeout < 0)
             response.addContextualMessage("timeout", "validate.cannotBeNegative");
-		
-		if (port < 0)
+
+        if (port < 0)
             response.addContextualMessage("port", "validate.cannotBeNegative");
-		
+
         if (StringUtils.isBlank(address))
             response.addContextualMessage("address", "validate.required");
-	}
+    }
 
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
 
-	public int getTimeout() {
-		return timeout;
-	}
+    public int getTimeout() {
+        return timeout;
+    }
 
 
-	public void setTimeout(int timeout) {
-		this.timeout = timeout;
-	}
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
 
+    @Override
+    public SerialPortProxy createProxy(SerialPortIdentifier id) {
+        return new SerialSocketBridge(id, address, port, timeout);
+    }
 
-	/* (non-Javadoc)
-	 * @see com.infiniteautomation.mango.io.serial.virtual.VirtualSerialPortConfig#createProxy()
-	 */
-	@Override
-	public SerialPortProxy createProxy(SerialPortIdentifier id) {
-		return new SerialSocketBridge(id, address, port, timeout);
-	}
-	
-	
-	
-	
-	
 }
