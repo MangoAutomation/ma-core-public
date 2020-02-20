8dfe4aecf837e5f099d7f063cf477ce839a87b39
terrypacker <terrypacker@users.noreply.github.com>
2020-02-19 14:43:33 -1000

    Make process result serializable (it is part of the session)

--- Core/src/com/serotonin/m2m2/i18n/ProcessResult.java
+++ Core/src/com/serotonin/m2m2/i18n/ProcessResult.java
@@ -4,6 +4,7 @@
  */
 package com.serotonin.m2m2.i18n;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,7 +24,10 @@ import com.serotonin.m2m2.i18n.ProcessMessage.Level;
  *
  * @author Matthew Lohbihler
  */
-public class ProcessResult {
+public class ProcessResult implements Serializable {
+
+    private static final long serialVersionUID = 50970866870464592L;
+
     private List<ProcessMessage> messages = new ArrayList<ProcessMessage>();
     private Map<String, Object> data = new HashMap<String, Object>();
 
