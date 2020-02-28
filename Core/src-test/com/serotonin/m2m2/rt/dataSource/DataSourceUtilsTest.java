b031c92c0705bfeaf634aa7ffa52c73f1a5c45bb
terrypacker <terrypacker@users.noreply.github.com>
2020-02-28 09:00:12 -1000

    Add test for data source utils numeric value format

---
+++ Core/src-test/com/serotonin/m2m2/rt/dataSource/DataSourceUtilsTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2020  Infinite Automation Software. All rights reserved.
+ */
+
+package com.serotonin.m2m2.rt.dataSource;
+
+import java.text.DecimalFormat;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.serotonin.m2m2.DataTypes;
+import com.serotonin.m2m2.MangoTestBase;
+import com.serotonin.m2m2.i18n.TranslatableException;
+import com.serotonin.m2m2.rt.dataImage.types.DataValue;
+import com.serotonin.m2m2.view.text.TextRenderer;
+
+/**
+ * Test the data source utils class
+ * @author Terry Packer
+ */
+public class DataSourceUtilsTest extends MangoTestBase {
+
+    @Test
+    public void testHexRendering() throws TranslatableException {
+
+        Pattern valuePattern = Pattern.compile("result=([0-9]*)");
+        int valueGroup = 1;
+        TextRenderer renderer = null;
+        DecimalFormat valueFormat = new DecimalFormat("00");
+        DataValue value = DataSourceUtils.getValue(valuePattern, valueGroup, "result=15" , DataTypes.NUMERIC,
+                "false", renderer, valueFormat, "Test Point Name");
+
+        Double expected = new Double(15);
+        Assert.assertEquals(expected.toString(), Double.toString(value.getDoubleValue()));
+
+    }
+
+}
