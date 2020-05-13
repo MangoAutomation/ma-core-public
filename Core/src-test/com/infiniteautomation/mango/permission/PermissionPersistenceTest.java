878b0a84ca10e7b777a5eae4918d5141e4338d1d
terrypacker <terrypacker@users.noreply.github.com>
2020-05-13 11:05:30 -1000

    Add get by id for MangoPermission in PermissionDao

---
+++ Core/src-test/com/infiniteautomation/mango/permission/PermissionPersistenceTest.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (C) 2020  Infinite Automation Software. All rights reserved.
+ */
+
+package com.infiniteautomation.mango.permission;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.MangoTestBase;
+import com.serotonin.m2m2.db.dao.PermissionDao;
+import com.serotonin.m2m2.vo.role.Role;
+
+/**
+ *
+ * @author Terry Packer
+ */
+public class PermissionPersistenceTest extends MangoTestBase {
+
+    @Test
+    public void testAndPermission() {
+
+        PermissionDao dao = Common.getBean(PermissionDao.class);
+
+        //Create some roles
+        Set<Role> roles = this.createRoles(2).stream().map(r -> r.getRole()).collect(Collectors.toSet());
+        //insert the permission
+        Integer permissionId = dao.permissionId(MangoPermission.createAndSet(roles));
+        MangoPermission read = dao.get(permissionId);
+
+        assertEquals(1, read.getRoles().size());
+        Iterator<Set<Role>> it = read.getRoles().iterator();
+        assertEquals(2, it.next().size());
+    }
+
+    @Test
+    public void testOrPermission() {
+
+        PermissionDao dao = Common.getBean(PermissionDao.class);
+
+        //Create some roles
+        Set<Role> roles = this.createRoles(2).stream().map(r -> r.getRole()).collect(Collectors.toSet());
+        //insert the permission
+        Integer permissionId = dao.permissionId(MangoPermission.createOrSet(roles));
+        MangoPermission read = dao.get(permissionId);
+
+        assertEquals(2, read.getRoles().size());
+        Iterator<Set<Role>> it = read.getRoles().iterator();
+        assertEquals(1, it.next().size());
+        assertEquals(1, it.next().size());
+    }
+
+
+    @Test
+    public void testComplexPermission() {
+
+        PermissionDao dao = Common.getBean(PermissionDao.class);
+
+        //Create some roles
+        Set<Role> minterm1 = this.createRoles(1).stream().map(r -> r.getRole()).collect(Collectors.toSet());
+        Set<Role> minterm2 = this.createRoles(2).stream().map(r -> r.getRole()).collect(Collectors.toSet());
+        Set<Role> minterm3 = this.createRoles(3).stream().map(r -> r.getRole()).collect(Collectors.toSet());
+
+        //insert the permission
+        Set<Set<Role>> permission = new HashSet<>();
+        permission.add(minterm1);
+        permission.add(minterm2);
+        permission.add(minterm3);
+
+        Integer permissionId = dao.permissionId(new MangoPermission(permission));
+        MangoPermission read = dao.get(permissionId);
+
+        assertEquals(3, read.getRoles().size());
+        Iterator<Set<Role>> it = read.getRoles().iterator();
+
+        while(it.hasNext()) {
+            Set<Role> minterm = it.next();
+            switch(minterm.size()) {
+                case 1:
+                    assertEquals(1, minterm.size());
+                    Role minterm1Role1 = minterm.iterator().next();
+                    assertTrue(minterm1.contains(minterm1Role1));
+                    break;
+                case 2:
+                    assertEquals(2, minterm.size());
+                    Iterator<Role> it2 = minterm.iterator();
+                    Role minterm2Role1 = it2.next();
+                    Role minterm2Role2 = it2.next();
+                    assertTrue(minterm2.contains(minterm2Role1));
+                    assertTrue(minterm2.contains(minterm2Role2));
+                    break;
+                case 3:
+                    assertEquals(3, minterm.size());
+                    Iterator<Role> it3 = minterm.iterator();
+                    Role minterm3Role1 = it3.next();
+                    Role minterm3Role2 = it3.next();
+                    Role minterm3Role3 = it3.next();
+                    assertTrue(minterm3.contains(minterm3Role1));
+                    assertTrue(minterm3.contains(minterm3Role2));
+                    assertTrue(minterm3.contains(minterm3Role3));
+                    break;
+            }
+        }
+    }
+
+    @Test
+    public void testOrphanedPermission() {
+        //Create permission with a single role
+        //Delete all roles in minterm
+    }
+
+    //Test when removing a role we also delete any orphaned permission (has no roles in minterms)
+    // Delete role then check each used minterms to see if empty
+
+    //Test when removing/modifying a permission ensure
+    // change permissionId mappings from ON DELETE CASCADE to ON DELETE RESTRICT
+    // then if deleted then check for any orphaned minterms (not mapped to permission)
+
+}
--- Core/src/com/serotonin/m2m2/db/dao/PermissionDao.java
+++ Core/src/com/serotonin/m2m2/db/dao/PermissionDao.java
@@ -3,15 +3,28 @@
  */
 package com.serotonin.m2m2.db.dao;
 
-import static com.serotonin.m2m2.db.dao.tables.MintermMappingTable.*;
-import static com.serotonin.m2m2.db.dao.tables.MintermTable.*;
-import static com.serotonin.m2m2.db.dao.tables.PermissionMappingTable.*;
-import static com.serotonin.m2m2.db.dao.tables.PermissionTable.*;
-import static org.jooq.impl.DSL.*;
+import static com.serotonin.m2m2.db.dao.tables.MintermMappingTable.MINTERMS_MAPPING;
+import static com.serotonin.m2m2.db.dao.tables.MintermTable.MINTERMS;
+import static com.serotonin.m2m2.db.dao.tables.PermissionMappingTable.PERMISSIONS_MAPPING;
+import static com.serotonin.m2m2.db.dao.tables.PermissionTable.PERMISSIONS;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.default_;
+import static org.jooq.impl.DSL.when;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.SelectSeekStep2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import com.infiniteautomation.mango.permission.MangoPermission;
@@ -22,6 +35,73 @@ import com.serotonin.m2m2.vo.role.Role;
  */
 @Repository
 public class PermissionDao extends BaseDao {
+
+    private final RoleDao roleDao;
+
+    @Autowired
+    PermissionDao(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    /**
+     * Get a MangoPermission by id
+     * @param id
+     * @return
+     */
+    public MangoPermission get(Integer id) {
+        List<Field<?>> fields = new ArrayList<>();
+        fields.add(roleDao.getTable().getAlias("id"));
+        fields.add(roleDao.getTable().getAlias("xid"));
+        fields.add(PERMISSIONS_MAPPING.mintermId);
+
+        SelectSeekStep2<Record, Integer, Integer> select = create.select(fields).from(PERMISSIONS_MAPPING)
+                .join(MINTERMS_MAPPING).on(PERMISSIONS_MAPPING.mintermId.eq(MINTERMS_MAPPING.mintermId))
+                .join(roleDao.getTable().getTableAsAlias()).on(roleDao.getTable().getAlias("id").eq(MINTERMS_MAPPING.roleId))
+                .where(PERMISSIONS_MAPPING.permissionId.eq(id))
+                .orderBy(PERMISSIONS_MAPPING.permissionId.asc(), PERMISSIONS_MAPPING.mintermId.asc());
+
+        String sql = select.getSQL();
+        List<Object> arguments = select.getBindValues();
+        Object[] argumentsArray = arguments.toArray(new Object[arguments.size()]);
+
+        return this.query(sql, argumentsArray, new ResultSetExtractor<MangoPermission>() {
+
+            private int roleIdIndex = 1;
+            private int roleXidIndex = 2;
+            private int minterIdIndex = 3;
+            @Override
+            public MangoPermission extractData(ResultSet rs)
+                    throws SQLException, DataAccessException {
+
+                if(rs.next()) {
+                    Set<Set<Role>> roleSet = new HashSet<>();
+                    Set<Role> minTerm = new HashSet<>();
+                    roleSet.add(minTerm);
+                    minTerm.add(new Role(rs.getInt(roleIdIndex), rs.getString(roleXidIndex)));
+
+                    int mintermId = rs.getInt(minterIdIndex);
+                    while(rs.next()) {
+                        if(rs.getInt(minterIdIndex) == mintermId) {
+                            //Add to current minterm
+                            minTerm.add(new Role(rs.getInt(roleIdIndex), rs.getString(roleXidIndex)));
+                        }else {
+                            //Add to next minterm
+                            minTerm = new HashSet<>();
+                            roleSet.add(minTerm);
+                            minTerm.add(new Role(rs.getInt(roleIdIndex), rs.getString(roleXidIndex)));
+                            mintermId = rs.getInt(minterIdIndex);
+                        }
+                    }
+                    MangoPermission permission = new MangoPermission(roleSet);
+                    permission.setId(id);
+                    return permission;
+                }else {
+                    return null;
+                }
+            }
+
+        });
+    }
 
     public Integer permissionId(MangoPermission permission) {
         if (permission.getRoles().isEmpty()) {
