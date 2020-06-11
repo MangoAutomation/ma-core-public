625122c324f0fb55c2b0ca8e2c81292dbae42c3e
terrypacker <terrypacker@users.noreply.github.com>
2020-06-10 16:47:19 -1000

    Format change

--- Core/src/com/serotonin/timer/OrderedRealTimeTimer.java
+++ Core/src/com/serotonin/timer/OrderedRealTimeTimer.java
@@ -10,37 +10,37 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
- * Real time timer that ensures all tasks of the same type are 
+ * Real time timer that ensures all tasks of the same type are
  * run in order of the time they were submitted via the OrderedThreadPoolExecutor
- * 
+ *
  * @author Terry Packer
  *
  */
-public class OrderedRealTimeTimer extends RealTimeTimer{
-	
-	public void init(OrderedThreadPoolExecutor executorService, int threadPriority){
-		OrderedTimerThread timer = new OrderedTimerThread(queue, executorService, timeSource);
+public class OrderedRealTimeTimer extends RealTimeTimer {
+
+    public void init(OrderedThreadPoolExecutor executorService, int threadPriority){
+        OrderedTimerThread timer = new OrderedTimerThread(queue, executorService, timeSource);
         timer.setName("Ordered RealTime Timer");
         timer.setDaemon(false);
         timer.setPriority(threadPriority);
         super.init(timer);
-	}
-	
-	@Override
-	public void init(ExecutorService executor){
-		//Enforce the rule of requiring ordered thread pool 
-		OrderedThreadPoolExecutor otpExecutor = (OrderedThreadPoolExecutor) executor;
-		this.init(otpExecutor, Thread.MAX_PRIORITY);
-	}
-	
-	@Override
+    }
+
+    @Override
+    public void init(ExecutorService executor){
+        //Enforce the rule of requiring ordered thread pool
+        OrderedThreadPoolExecutor otpExecutor = (OrderedThreadPoolExecutor) executor;
+        this.init(otpExecutor, Thread.MAX_PRIORITY);
+    }
+
+    @Override
     public void init() {
         this.init(new OrderedThreadPoolExecutor(0, 1000, 30L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), false, timeSource));
     }
-	
-	@Override
+
+    @Override
     public void init(TimerThread timer){
-		//Check on cast
-	    super.init((OrderedTimerThread)timer);
+        //Check on cast
+        super.init(timer);
     }
 }
