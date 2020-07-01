f29d5f0c08ab50d216ba0550edde80d0da0fa20d
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-07-01 11:22:43 -0600

    Remove imports

--- Core/src/com/serotonin/m2m2/web/mvc/spring/MangoLocaleResolver.java
+++ Core/src/com/serotonin/m2m2/web/mvc/spring/MangoLocaleResolver.java
@@ -4,18 +4,13 @@
 package com.serotonin.m2m2.web.mvc.spring;
 
 import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.i18n.TranslatableMessage;
 import com.serotonin.m2m2.vo.User;
-import com.serotonin.m2m2.vo.permission.PermissionException;
-import com.serotonin.m2m2.vo.permission.PermissionHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
 import java.util.Locale;
 
 /**
