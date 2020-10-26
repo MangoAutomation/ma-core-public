c32d3cdd3439020333140145d72962ad8a03df4d
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-10-26 17:37:49 -0600

    Add basic tests

---
+++ Core/src-test/com/infiniteautomation/mango/cache/BidirectionalCacheTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Infinite Automation Systems Inc. All rights reserved.
+ */
+
+package com.infiniteautomation.mango.cache;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Jared Wiltshire
+ */
+public class BidirectionalCacheTest {
+
+    @Test
+    public void canPut() {
+        BidirectionalCache<Integer, String> cache = new BidirectionalCache<>(1);
+        BidirectionalCache<String, Integer> inverse = cache.inverse();
+        cache.put(1, "one");
+        assertEquals(1, cache.size());
+        assertEquals(1, inverse.size());
+        assertEquals("one", cache.get(1));
+        assertEquals(1, (long) inverse.get("one"));
+    }
+
+    @Test
+    public void canPutInverse() {
+        BidirectionalCache<Integer, String> cache = new BidirectionalCache<>(1);
+        BidirectionalCache<String, Integer> inverse = cache.inverse();
+        inverse.put("one", 1);
+        assertEquals(1, cache.size());
+        assertEquals(1, inverse.size());
+        assertEquals("one", cache.get(1));
+        assertEquals(1, (long) inverse.get("one"));
+    }
+
+    @Test
+    public void capacity() {
+        BidirectionalCache<Integer, String> cache = new BidirectionalCache<>(1);
+        BidirectionalCache<String, Integer> inverse = cache.inverse();
+        cache.put(1, "one");
+        cache.put(2, "two");
+        assertEquals(1, cache.size());
+        assertEquals(1, inverse.size());
+        assertNull(cache.get(1));
+        assertEquals("two", cache.get(2));
+        assertNull(inverse.get("one"));
+        assertEquals(2, (long) inverse.get("two"));
+    }
+
+    @Test
+    public void capacityInverse() {
+        BidirectionalCache<Integer, String> cache = new BidirectionalCache<>(1);
+        BidirectionalCache<String, Integer> inverse = cache.inverse();
+        inverse.put("one", 1);
+        inverse.put("two", 2);
+        assertEquals(1, cache.size());
+        assertEquals(1, inverse.size());
+        assertNull(cache.get(1));
+        assertEquals("two", cache.get(2));
+        assertNull(inverse.get("one"));
+        assertEquals(2, (long) inverse.get("two"));
+    }
+}
