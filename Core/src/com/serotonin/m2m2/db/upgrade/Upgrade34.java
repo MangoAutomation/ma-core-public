dae12f4c855ab9036f5d12f69621778542a2005c
Terry Packer <terrypacker@users.noreply.github.com>
2020-12-30 09:02:49 -1000

    Improve performance of upgrade 34

--- Core/src/com/serotonin/m2m2/db/upgrade/Upgrade34.java
+++ Core/src/com/serotonin/m2m2/db/upgrade/Upgrade34.java
@@ -34,33 +34,21 @@ public class Upgrade34 extends DBUpgrade {
             createTimeSeries.put(DatabaseProxy.DatabaseType.MSSQL.name(), createTimeSeriesTableMSSQL);
             runScript(createTimeSeries, out);
 
+            //Add column
             runScript(Collections.singletonMap(DEFAULT_DATABASE_TYPE, new String[] {
                     "ALTER TABLE dataPoints ADD COLUMN seriesId INT;",
-                    "ALTER TABLE dataPoints ADD CONSTRAINT dataPointsFk5 FOREIGN KEY (seriesId) REFERENCES timeSeries(id);",
             }), out);
 
-            //Do for all data points, insert timeSeries and set on data point
-            //Add seriesId ID column to dataPoints
-            final int batchSize = Common.envProps.getInt("db.in.maxOperands", 1000);
-            final String update = "UPDATE dataPoints SET seriesId=? WHERE id=?";
-            final List<Object[]> batchArgs = new ArrayList<>();
-            final AtomicInteger currentBatchSize = new AtomicInteger();
+            //Insert/Update ids
+            runScript(Collections.singletonMap(DEFAULT_DATABASE_TYPE, new String[] {
+                    "INSERT INTO timeSeries (id) SELECT id FROM dataPoints;",
+                    "UPDATE dataPoints SET seriesId = id;"
+            }), out);
 
-            ejt.query("SELECT id FROM dataPoints", rch -> {
-                int id = rch.getInt(1);
-                int seriesId = ejt.doInsert("INSERT INTO timeSeries VALUES(?)", new Object[] {id});
-                batchArgs.add(new Object[]{seriesId, id});
-                if(currentBatchSize.incrementAndGet() >= batchSize) {
-                    ejt.batchUpdate(update, batchArgs);
-                    batchArgs.clear();
-                    currentBatchSize.set(0);
-                }
-            });
-
-            //Finish the batch
-            if(currentBatchSize.get() > 0) {
-                ejt.batchUpdate(update, batchArgs);
-            }
+            //Add in constraint
+            runScript(Collections.singletonMap(DEFAULT_DATABASE_TYPE, new String[] {
+                    "ALTER TABLE dataPoints ADD CONSTRAINT dataPointsFk5 FOREIGN KEY (seriesId) REFERENCES timeSeries(id);"
+            }), out);
 
             //Make NON-NULL
             HashMap<String, String[]> scripts = new HashMap<>();
