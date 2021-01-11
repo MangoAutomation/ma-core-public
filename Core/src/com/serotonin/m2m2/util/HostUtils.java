fdd07ee32a51ca4ba311ce77cb0cba92e37eeca8
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-01-11 10:48:11 -0700

    Remove dead code

--- Core/src/com/serotonin/m2m2/util/HostUtils.java
+++ Core/src/com/serotonin/m2m2/util/HostUtils.java
@@ -1,6 +1,5 @@
 package com.serotonin.m2m2.util;
 
-import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -8,32 +7,8 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
-
-import com.serotonin.epoll.ProcessEPoll;
-import com.serotonin.epoll.ProcessEpollUtils;
-import com.serotonin.provider.ProcessEPollProvider;
-import com.serotonin.provider.Providers;
 
 public class HostUtils {
-    public static boolean isWindows() {
-        return StringUtils.contains(System.getProperty("os.name"), "Windows");
-    }
-
-    public static boolean isLinux() {
-        return StringUtils.contains(System.getProperty("os.name"), "Linux");
-    }
-
-    private static final Pattern PATTERN_HWADDR = Pattern.compile("HWaddr\\s+(.*?)\\s");
-
-    // TODO this only works for linux
-    public static String getHwaddr(String iface) throws IOException {
-        ProcessEPoll pep = Providers.get(ProcessEPollProvider.class).getProcessEPoll();
-        String content = ProcessEpollUtils.getProcessInput(pep, 3000, "ifconfig", iface);
-        return com.serotonin.util.StringUtils.findGroup(PATTERN_HWADDR, content);
-    }
 
     public static List<NICInfo> getLocalInet4Addresses(boolean includeLoopback) throws SocketException {
         List<NICInfo> list = new ArrayList<NICInfo>();
--- Core/src/com/serotonin/provider/ProcessEPollProvider.java
+++
@@ -1,9 +0,0 @@
-package com.serotonin.provider;
-
-import com.serotonin.epoll.ProcessEPoll;
-
-public interface ProcessEPollProvider extends Provider {
-    ProcessEPoll getProcessEPoll();
-
-    void terminate(boolean waitForAll);
-}
