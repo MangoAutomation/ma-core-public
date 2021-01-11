ad48b0e220505b0717af867db52b171d52148b5f
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-01-11 09:45:09 -0700

    Extract interface

--- Core/src/com/infiniteautomation/mango/spring/components/RunAs.java
+++ Core/src/com/infiniteautomation/mango/spring/components/RunAs.java
@@ -4,64 +4,17 @@
 
 package com.infiniteautomation.mango.spring.components;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Proxy;
 import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.stereotype.Component;
-
-import com.infiniteautomation.mango.spring.service.PermissionService;
-import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.vo.permission.PermissionHolder;
 
-/**
- * See also {@link DelegatingSecurityContextRunnable}
- */
-@Component
-public class RunAs {
+public interface RunAs {
+    void runAs(PermissionHolder user, Runnable command);
 
-    private final PermissionService permissionService;
+    <T> T runAs(PermissionHolder user, Supplier<T> command);
 
-    @Autowired
-    public RunAs(PermissionService permissionService) {
-        this.permissionService = permissionService;
-    }
-
-    public void runAs(PermissionHolder user, Runnable command) {
-        SecurityContext original = getOriginalContext();
-        try {
-            SecurityContextHolder.setContext(newSecurityContext(user));
-            command.run();
-        } finally {
-            restoreSecurityContext(original);
-        }
-    }
-
-    public <T> T runAs(PermissionHolder user, Supplier<T> command) {
-        SecurityContext original = getOriginalContext();
-        try {
-            SecurityContextHolder.setContext(newSecurityContext(user));
-            return command.get();
-        } finally {
-            restoreSecurityContext(original);
-        }
-    }
-
-    public <T> T runAsCallable(PermissionHolder user, Callable<T> command) throws Exception {
-        SecurityContext original = getOriginalContext();
-        try {
-            SecurityContextHolder.setContext(newSecurityContext(user));
-            return command.call();
-        } finally {
-            restoreSecurityContext(original);
-        }
-    }
+    <T> T runAsCallable(PermissionHolder user, Callable<T> command) throws Exception;
 
     /**
      * Creates a proxy object for the supplied instance where every method invoked is run as the supplied user.
@@ -73,49 +26,5 @@ public class RunAs {
      * @return
      */
     @SuppressWarnings("unchecked")
-    public <T> T runAsProxy(PermissionHolder user, T instance) {
-        Class<?> clazz = instance.getClass();
-        SecurityContext runAsContext = newSecurityContext(user);
-
-        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), (proxy, method, args) -> {
-            SecurityContext original = getOriginalContext();
-            try {
-                SecurityContextHolder.setContext(runAsContext);
-                return method.invoke(instance, args);
-            } catch (InvocationTargetException e) {
-                throw e.getCause();
-            } finally {
-                restoreSecurityContext(original);
-            }
-        });
-    }
-
-    /**
-     * Creates a new security context for the supplied user
-     * @param user
-     * @return
-     */
-    private SecurityContext newSecurityContext(PermissionHolder user) {
-        SecurityContext newContext = SecurityContextHolder.createEmptyContext();
-        newContext.setAuthentication(new PreAuthenticatedAuthenticationToken(user, null));
-        return newContext;
-    }
-
-    private SecurityContext getOriginalContext() {
-        SecurityContext original = SecurityContextHolder.getContext();
-        if (original.getAuthentication() == null) {
-            return original;
-        }
-        permissionService.ensureAdminRole(Common.getUser());
-        return original;
-    }
-
-    private void restoreSecurityContext(SecurityContext original) {
-        SecurityContext emptyContext = SecurityContextHolder.createEmptyContext();
-        if (emptyContext.equals(original)) {
-            SecurityContextHolder.clearContext();
-        } else {
-            SecurityContextHolder.setContext(original);
-        }
-    }
+    <T> T runAsProxy(PermissionHolder user, T instance);
 }
---
+++ Core/src/com/infiniteautomation/mango/spring/components/RunAsImpl.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
+ */
+
+package com.infiniteautomation.mango.spring.components;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Proxy;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import com.infiniteautomation.mango.spring.service.PermissionService;
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.vo.permission.PermissionHolder;
+
+/**
+ * See also {@link DelegatingSecurityContextRunnable}
+ */
+@Component
+public class RunAsImpl implements RunAs {
+
+    private final PermissionService permissionService;
+
+    @Autowired
+    public RunAsImpl(PermissionService permissionService) {
+        this.permissionService = permissionService;
+    }
+
+    @Override
+    public void runAs(PermissionHolder user, Runnable command) {
+        SecurityContext original = getOriginalContext();
+        try {
+            SecurityContextHolder.setContext(newSecurityContext(user));
+            command.run();
+        } finally {
+            restoreSecurityContext(original);
+        }
+    }
+
+    @Override
+    public <T> T runAs(PermissionHolder user, Supplier<T> command) {
+        SecurityContext original = getOriginalContext();
+        try {
+            SecurityContextHolder.setContext(newSecurityContext(user));
+            return command.get();
+        } finally {
+            restoreSecurityContext(original);
+        }
+    }
+
+    @Override
+    public <T> T runAsCallable(PermissionHolder user, Callable<T> command) throws Exception {
+        SecurityContext original = getOriginalContext();
+        try {
+            SecurityContextHolder.setContext(newSecurityContext(user));
+            return command.call();
+        } finally {
+            restoreSecurityContext(original);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T runAsProxy(PermissionHolder user, T instance) {
+        Class<?> clazz = instance.getClass();
+        SecurityContext runAsContext = newSecurityContext(user);
+
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), (proxy, method, args) -> {
+            SecurityContext original = getOriginalContext();
+            try {
+                SecurityContextHolder.setContext(runAsContext);
+                return method.invoke(instance, args);
+            } catch (InvocationTargetException e) {
+                throw e.getCause();
+            } finally {
+                restoreSecurityContext(original);
+            }
+        });
+    }
+
+    /**
+     * Creates a new security context for the supplied user
+     * @param user
+     * @return
+     */
+    private SecurityContext newSecurityContext(PermissionHolder user) {
+        SecurityContext newContext = SecurityContextHolder.createEmptyContext();
+        newContext.setAuthentication(new PreAuthenticatedAuthenticationToken(user, null));
+        return newContext;
+    }
+
+    private SecurityContext getOriginalContext() {
+        SecurityContext original = SecurityContextHolder.getContext();
+        if (original.getAuthentication() == null) {
+            return original;
+        }
+        permissionService.ensureAdminRole(Common.getUser());
+        return original;
+    }
+
+    private void restoreSecurityContext(SecurityContext original) {
+        SecurityContext emptyContext = SecurityContextHolder.createEmptyContext();
+        if (emptyContext.equals(original)) {
+            SecurityContextHolder.clearContext();
+        } else {
+            SecurityContextHolder.setContext(original);
+        }
+    }
+}
