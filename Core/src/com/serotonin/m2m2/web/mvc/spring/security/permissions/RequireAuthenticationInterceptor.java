c75ec02cb2cf49666bc383a5e78206e037eeea52
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-01-12 11:41:10 -0700

    Add fix for async dispatch with token/basic auth

--- Core/src/com/serotonin/m2m2/web/mvc/spring/security/permissions/RequireAuthenticationInterceptor.java
+++ Core/src/com/serotonin/m2m2/web/mvc/spring/security/permissions/RequireAuthenticationInterceptor.java
@@ -4,6 +4,7 @@
 
 package com.serotonin.m2m2.web.mvc.spring.security.permissions;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +13,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -26,9 +29,23 @@ public class RequireAuthenticationInterceptor extends HandlerInterceptorAdapter 
         this.trustResolver = trustResolver;
     }
 
+    /**
+     * <p>We don't handle ASYNC dispatches as the {@link Authentication} is not populated correctly when using token/basic authentication.
+     * The authentication should already have been checked during the original REQUEST/FORWARD dispatch.</p>
+     *
+     * <p>The authentication is anonymous during ASYNC dispatch for token/basic auth as they use a stateless security configuration and do not use sessions.
+     * This configuration does not have a {@link SecurityContextRepository} and therefore has no way to persist the security context between the
+     * REQUEST and ASYNC dispatch (usually performed by {@link SecurityContextPersistenceFilter}).</p>
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (handler instanceof HandlerMethod) {
+        if (request.getDispatcherType() != DispatcherType.ASYNC && handler instanceof HandlerMethod) {
             HandlerMethod method = (HandlerMethod) handler;
             if (!method.hasMethodAnnotation(AnonymousAccess.class)) {
                 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -39,5 +56,4 @@ public class RequireAuthenticationInterceptor extends HandlerInterceptorAdapter 
         }
         return true;
     }
-
 }
