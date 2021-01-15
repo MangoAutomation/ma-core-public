eafc66559977ee62023fe8bb7e44671f007c18e3
Terry Packer <terrypacker@users.noreply.github.com>
2021-01-15 11:40:25 -1000

    Create SqlPointValueCacheProxy as default implementation

---
+++ Core/src/com/serotonin/m2m2/module/definitions/actions/com/serotonin/m2m2/module/definitions/pointValueCacheProxy/SqlPointValueCacheProxy.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
+ * @Author Terry Packer
+ *
+ */
+
+package com.serotonin.m2m2.module.definitions.actions.com.serotonin.m2m2.module.definitions.pointValueCacheProxy;
+
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.db.PointValueCacheProxy;
+import com.serotonin.m2m2.db.dao.PointValueCacheDao;
+
+/**
+ * Default implementation of proxy to use if none are module defined
+ */
+public class SqlPointValueCacheProxy extends PointValueCacheProxy {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public PointValueCacheDao getDao() {
+        return (PointValueCacheDao) Common.databaseProxy.newPointValueDao();
+    }
+
+    @Override
+    public int getOrder() {
+        //Lowest priority
+        return Integer.MAX_VALUE;
+    }
+}
