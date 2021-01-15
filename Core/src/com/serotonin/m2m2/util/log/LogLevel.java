c4e003d37fbc053a821d8043e2bdb5e5286b70c0
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-01-15 10:32:09 -0700

    Ability to change log level

--- Core/src/com/serotonin/m2m2/util/log/LogLevel.java
+++ Core/src/com/serotonin/m2m2/util/log/LogLevel.java
@@ -3,6 +3,8 @@
  */
 package com.serotonin.m2m2.util.log;
 
+import org.apache.logging.log4j.Level;
+
 import com.infiniteautomation.mango.util.ReverseEnum;
 import com.infiniteautomation.mango.util.ReverseEnumMap;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
@@ -13,22 +15,23 @@ import com.serotonin.m2m2.i18n.TranslatableMessage;
  */
 public enum LogLevel implements ReverseEnum<Integer> {
         
-    TRACE(1, new TranslatableMessage("common.logging.trace")),
-    DEBUG(2, new TranslatableMessage("common.logging.debug")),
-    INFO(3, new TranslatableMessage("common.logging.info")),
-    WARN(4, new TranslatableMessage("common.logging.warn")),
-    ERROR(5, new TranslatableMessage("common.logging.error")),
-    FATAL(6, new TranslatableMessage("common.logging.fatal")),
-    NONE(10, new TranslatableMessage("common.logging.none"));
-    
+    TRACE(1, new TranslatableMessage("common.logging.trace"), Level.TRACE),
+    DEBUG(2, new TranslatableMessage("common.logging.debug"), Level.DEBUG),
+    INFO(3, new TranslatableMessage("common.logging.info"), Level.INFO),
+    WARN(4, new TranslatableMessage("common.logging.warn"), Level.WARN),
+    ERROR(5, new TranslatableMessage("common.logging.error"), Level.ERROR),
+    FATAL(6, new TranslatableMessage("common.logging.fatal"), Level.FATAL),
+    NONE(10, new TranslatableMessage("common.logging.none"), Level.OFF);
 
     private static ReverseEnumMap<Integer, LogLevel> map = new ReverseEnumMap<>(LogLevel.class);
     private final int value;
     private final TranslatableMessage description;
+    private final Level log4jLevel;
 
-    private LogLevel(int value, TranslatableMessage description) {
+    LogLevel(int value, TranslatableMessage description, Level log4jLevel) {
         this.value = value;
         this.description = description;
+        this.log4jLevel = log4jLevel;
     }
 
     @Override
@@ -46,5 +49,9 @@ public enum LogLevel implements ReverseEnum<Integer> {
 
     public static LogLevel fromName(String name) {
         return Enum.valueOf(LogLevel.class, name);
+    }
+
+    public Level getLog4jLevel() {
+        return log4jLevel;
     }
 }
