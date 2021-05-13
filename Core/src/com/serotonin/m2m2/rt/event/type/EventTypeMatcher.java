2c1543b5ca2e570c7dcfb66171be06913d2467b4
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-05-13 15:30:37 -0600

    Use empty string for wildcard

--- Core/src/com/serotonin/m2m2/rt/event/type/EventTypeMatcher.java
+++ Core/src/com/serotonin/m2m2/rt/event/type/EventTypeMatcher.java
@@ -6,6 +6,8 @@ package com.serotonin.m2m2.rt.event.type;
 
 import java.util.Objects;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 public class EventTypeMatcher {
 
     private String eventType;
@@ -16,20 +18,30 @@ public class EventTypeMatcher {
     public EventTypeMatcher() {
     }
 
-    public EventTypeMatcher(EventType other) {
+    public EventTypeMatcher(@NonNull EventType other) {
         this(other.getEventType(), other.getEventSubtype(), other.getReferenceId1(), other.getReferenceId2());
     }
 
-    public EventTypeMatcher(String eventType, String eventSubtype, int referenceId1, int referenceId2) {
+    /**
+     * @param eventType may be empty to match any event type, must be non null
+     * @param eventSubtype may be empty to match any event subtype, must be non null
+     * @param referenceId1 may be 0 to match any referenceId1
+     * @param referenceId2 may be 0 to match any referenceId2
+     */
+    public EventTypeMatcher(@NonNull String eventType, @NonNull String eventSubtype, int referenceId1, int referenceId2) {
         this.eventType = eventType;
         this.eventSubtype = eventSubtype;
         this.referenceId1 = referenceId1;
         this.referenceId2 = referenceId2;
     }
 
+    /**
+     * @param type event type to match
+     * @return true if event type matches
+     */
     public boolean matches(EventType type) {
-        return (eventType == null || eventType.equals(type.getEventType())) &&
-                (eventSubtype == null || eventSubtype.equals(type.getEventSubtype())) &&
+        return (eventType.isEmpty() || eventType.equals(type.getEventType())) &&
+                (eventSubtype.isEmpty() || eventSubtype.equals(type.getEventSubtype())) &&
                 (referenceId1 == 0 || referenceId1 == type.getReferenceId1()) &&
                 (referenceId2 == 0 || referenceId2 == type.getReferenceId2());
     }
