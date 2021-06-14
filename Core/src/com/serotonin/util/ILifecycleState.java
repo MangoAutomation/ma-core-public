b32a51ac217d68e4039743d187337a804c520c0a
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-06-14 16:10:38 -0600

    Add translation for state to model

--- Core/src/com/serotonin/util/ILifecycleState.java
+++ Core/src/com/serotonin/util/ILifecycleState.java
@@ -4,10 +4,16 @@
 
 package com.serotonin.util;
 
+import com.serotonin.m2m2.i18n.TranslatableMessage;
+
 public enum ILifecycleState {
     PRE_INITIALIZE,
     INITIALIZING,
     RUNNING,
     TERMINATING,
-    TERMINATED
+    TERMINATED;
+
+    public TranslatableMessage getMessage() {
+        return new TranslatableMessage("common.lifecycle.state." + name());
+    }
 }
