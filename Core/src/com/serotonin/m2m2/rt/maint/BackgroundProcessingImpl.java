5a3c7e4c8264dd0f2bcd30c94efc075d88deaab8
terrypacker <terrypacker@users.noreply.github.com>
2021-06-28 14:34:24 -1000

    Exit on OOM errors in Timer, high/med/low pools
    As per #RAD-931

--- Core/src/com/serotonin/m2m2/rt/maint/BackgroundProcessingImpl.java
+++ Core/src/com/serotonin/m2m2/rt/maint/BackgroundProcessingImpl.java
@@ -505,7 +505,7 @@ public class BackgroundProcessingImpl implements BackgroundProcessing {
      * @author Terry Packer
      *
      */
-    public class RejectableWorkItemRunnable extends Task{
+    public class RejectableWorkItemRunnable extends Task {
 
         final WorkItem item;
         final TaskRejectionHandler rejectionHandler;
@@ -521,7 +521,7 @@ public class BackgroundProcessingImpl implements BackgroundProcessing {
             try {
                 item.execute();
             }
-            catch (Throwable t) {
+            catch (Exception t) {
                 log.error("Error in work item", t);
             }
         }
@@ -567,7 +567,7 @@ public class BackgroundProcessingImpl implements BackgroundProcessing {
             SecurityContextHolder.setContext(this.delegateSecurityContext);
             try {
                 item.execute();
-            } catch (Throwable t) {
+            } catch (Exception t) {
                 String message = "Error in work item ";
                 if (item.getDescription() != null) {
                     message += item.getDescription();
