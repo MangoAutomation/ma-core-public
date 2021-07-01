7059427d159fec4058e5028f1abce427e8fa3a81
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-07-01 15:02:50 -0600

    Update for new paths and refactor

--- Bootstrap/src/com/infiniteautomation/mango/bootstrap/CoreUpgrade.java
+++ Bootstrap/src/com/infiniteautomation/mango/bootstrap/CoreUpgrade.java
@@ -6,6 +6,7 @@ package com.infiniteautomation.mango.bootstrap;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -17,22 +18,19 @@ import java.util.zip.ZipFile;
  */
 public class CoreUpgrade {
 
-    private final Path maHome;
+    private final Path installationDirectory;
 
-    public CoreUpgrade(Path maHome) {
-        this.maHome = maHome;
+    public CoreUpgrade(Path installationDirectory) {
+        this.installationDirectory = installationDirectory;
     }
 
     public void upgrade() {
         try {
-            Files.list(maHome).filter(f -> {
+            Files.list(installationDirectory).filter(f -> {
                 String filename = f.getFileName().toString();
                 return filename.startsWith("m2m2-core-") && filename.endsWith(".zip");
             }).sorted().forEach(this::extractZip);
         } catch (Exception e) {
-            if (e instanceof CoreUpgradeException) {
-                throw (CoreUpgradeException) e;
-            }
             throw new CoreUpgradeException(e);
         }
     }
@@ -49,18 +47,18 @@ public class CoreUpgrade {
 
             Files.delete(zipPath);
         } catch (IOException e) {
-            throw new CoreUpgradeException(e);
+            throw new UncheckedIOException(e);
         }
     }
 
     private void deleteOldFiles() {
         try {
-            Files.deleteIfExists(maHome.resolve("release.properties"));
-            Files.deleteIfExists(maHome.resolve("release.signed"));
+            Files.deleteIfExists(installationDirectory.resolve("release.properties"));
+            Files.deleteIfExists(installationDirectory.resolve("release.signed"));
 
             // wont delete work as this is now configurable and no longer used for JSP
 
-            Path lib = maHome.resolve("lib");
+            Path lib = installationDirectory.resolve("lib");
             if (Files.isDirectory(lib)) {
                 Files.list(lib)
                 .filter(p -> p.getFileName().toString().endsWith(".jar"))
@@ -68,18 +66,18 @@ public class CoreUpgrade {
                     try {
                         Files.delete(p);
                     } catch (IOException e) {
-                        throw new CoreUpgradeException(e);
+                        throw new UncheckedIOException(e);
                     }
                 });
             }
         } catch (IOException e) {
-            throw new CoreUpgradeException(e);
+            throw new UncheckedIOException(e);
         }
     }
 
     private void processEntry(ZipFile zip, ZipEntry entry) {
         try {
-            Path entryPath = maHome.resolve(entry.getName());
+            Path entryPath = installationDirectory.resolve(entry.getName());
             String filename = entryPath.getFileName().toString();
 
             if (entry.isDirectory()) {
@@ -87,19 +85,31 @@ public class CoreUpgrade {
             } else {
                 try (InputStream in = zip.getInputStream(entry)) {
                     // overwrite files in place
-                    try (OutputStream out = Files.newOutputStream(entryPath, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-                        BootstrapUtils.copy(in, out);
+                    try (OutputStream out = Files.newOutputStream(entryPath, StandardOpenOption.WRITE,
+                            StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+                        copy(in, out);
                     }
                 }
 
                 Files.setLastModifiedTime(entryPath, entry.getLastModifiedTime());
                 if (filename.endsWith(".sh")) {
+                    //noinspection ResultOfMethodCallIgnored
                     entryPath.toFile().setExecutable(true, false);
                 }
             }
         } catch (IOException e) {
-            throw new CoreUpgradeException(e);
+            throw new UncheckedIOException(e);
         }
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    public static long copy(InputStream in, OutputStream out) throws IOException {
+        long total = 0;
+        byte[] buffer = new byte[8192];
+        for (int bytesRead; (bytesRead = in.read(buffer)) != -1; total += bytesRead) {
+            out.write(buffer, 0, bytesRead);
+        }
+        return total;
     }
 
     public static class CoreUpgradeException extends RuntimeException {
--- Bootstrap/src/com/infiniteautomation/mango/bootstrap/MangoBootstrap.java
+++ Bootstrap/src/com/infiniteautomation/mango/bootstrap/MangoBootstrap.java
@@ -6,57 +6,99 @@ package com.infiniteautomation.mango.bootstrap;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
+ * Used to setup the classpath correctly then start Mango. Handles unzipping the core zip bundle if it is the
+ * installation directory.
+ *
  * @author Jared Wiltshire
  */
+@SuppressWarnings("unused")
 public class MangoBootstrap {
 
     public static void main(String[] args) throws Exception {
-        Path maHome = BootstrapUtils.maHome();
+        MangoBootstrap bootstrap = new MangoBootstrap()
+                .addJarsFromDirectory(Paths.get("lib"));
 
-        CoreUpgrade upgrade = new CoreUpgrade(maHome);
+        CoreUpgrade upgrade = new CoreUpgrade(bootstrap.getInstallationDirectory());
         upgrade.upgrade();
 
-        ClassLoader classLoader = new MangoBootstrap(maHome).createClassLoader();
+        bootstrap.startMango(args);
+    }
+
+    private final List<URL> urls = new ArrayList<>();
+    private final Path workingDirectory;
+    private final Path installationDirectory;
+    private final Path configFile;
+
+    public MangoBootstrap() {
+        this.workingDirectory = Paths.get(".").normalize().toAbsolutePath();
+        this.installationDirectory = installationDirectory();
+        this.configFile = configurationFile();
+    }
+
+    public Path getInstallationDirectory() {
+        return installationDirectory;
+    }
+
+    public Path getConfigFile() {
+        return configFile;
+    }
+
+    /**
+     * @return class loader that can be used to start Mango
+     */
+    public URLClassLoader createClassLoader() {
+        return createClassLoader(MangoBootstrap.class.getClassLoader());
+    }
+
+    /**
+     * @param parent parent classloader
+     * @return class loader that can be used to start Mango
+     */
+    public URLClassLoader createClassLoader(ClassLoader parent) {
+        return new URLClassLoader(urls.toArray(new URL[0]), parent);
+    }
+
+    /**
+     * Starts Mango without forking
+     * @param args passed to the Mango main method
+     */
+    public void startMango(String... args) throws Exception {
+        // ensure Mango Core can find Mango home
+        System.setProperty("mango.paths.home", installationDirectory.toString());
+        System.setProperty("mango.config", configFile.toString());
+
+        ClassLoader classLoader = createClassLoader();
         Class<?> mainClass = classLoader.loadClass("com.serotonin.m2m2.Main");
         Method mainMethod = mainClass.getMethod("main", String[].class);
         mainMethod.invoke(null, (Object) args);
     }
 
-    private List<URL> urls = new ArrayList<>();
-    private final Path maHome;
-
-    public MangoBootstrap(Path maHome) {
-        this.maHome = maHome;
-
-        addUrl(maHome.resolve("overrides").resolve("classes"));
-        addUrl(maHome.resolve("classes"));
-        addUrl(maHome.resolve("overrides").resolve("properties"));
-        addJars(maHome.resolve("overrides").resolve("lib"));
-        addJars(maHome.resolve("lib"));
-    }
-
-    public URLClassLoader createClassLoader() {
-        return createClassLoader(MangoBootstrap.class.getClassLoader());
-    }
-
-    public URLClassLoader createClassLoader(ClassLoader parent) {
-        return new URLClassLoader(urls.toArray(new URL[urls.size()]), parent);
-    }
-
-    public MangoBootstrap addJars(Path directory) {
-        if (Files.isDirectory(directory)) {
+    /**
+     * Add all jars from a directory to the class path.
+     * @param directory relative to installation directory
+     * @return this
+     */
+    public MangoBootstrap addJarsFromDirectory(Path directory) {
+        Path d = installationDirectory.resolve(directory);
+        if (Files.isDirectory(d)) {
             try {
-                Files.list(directory)
-                .filter(p -> p.toString().endsWith(".jar"))
-                .forEach(this::addUrl);
+                Files.list(d)
+                        .filter(p -> p.toString().endsWith(".jar"))
+                        .forEach(this::addPathInternal);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -64,23 +106,101 @@ public class MangoBootstrap {
         return this;
     }
 
-    public MangoBootstrap addUrl(Path p) {
+    /**
+     * Add a path to the class path
+     * @param path relative to installation directory
+     * @return this
+     */
+    public MangoBootstrap addPath(Path path) {
+        Path p = installationDirectory.resolve(path);
         if (Files.exists(p)) {
-            try {
-                urls.add(p.toUri().toURL());
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
+            addPathInternal(p);
         }
         return this;
     }
 
-    public List<URL> getUrls() {
-        return urls;
+    private void addPathInternal(Path path) {
+        try {
+            urls.add(path.toUri().toURL());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Path getMaHome() {
-        return maHome;
+    private Path configurationFile() {
+        Optional<Path> explicitlySpecified = Arrays.stream(new Path[]{
+                getPath(System.getProperty("mango.config")),
+                getPath(System.getenv("mango_config")),
+                getPath(System.getenv("MA_ENV_PROPERTIES"))
+        }).filter(Objects::nonNull)
+                .findFirst();
+
+        if (explicitlySpecified.isPresent()) {
+            return explicitlySpecified.get().toAbsolutePath().normalize();
+        }
+
+        // if data path is set via env variable, use that by default instead of install directory
+        Path dataDirectory = Arrays.stream(new Path[]{
+                getPath(System.getProperty("mango.paths.data"), installationDirectory),
+                getPath(System.getenv("mango_paths_data"), installationDirectory)
+        }).filter(Objects::nonNull)
+                .findFirst()
+                .orElse(installationDirectory);
+
+        Path userHome = getPath(System.getProperty("user.home"));
+
+        Optional<Path> existingPaths = Arrays.stream(new Path[]{
+                installationDirectory.resolve(Paths.get("overrides", "properties", "env.properties")),
+                installationDirectory.resolve(Paths.get("env.properties")),
+                dataDirectory.resolve(Paths.get("env.properties")),
+                dataDirectory.resolve(Paths.get("mango.properties")),
+                userHome != null ? userHome.resolve("mango.properties") : null,
+        }).filter(Objects::nonNull)
+                .filter(Files::exists)
+                .findFirst();
+
+        return existingPaths.orElse(dataDirectory.resolve("mango.properties")).toAbsolutePath().normalize();
     }
 
+    private Path installationDirectory() {
+        Path fromJarLocation;
+        try {
+            URI uri = MangoBootstrap.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+            Path jarPath = Paths.get(uri);
+            fromJarLocation = jarPath.toString().endsWith(".jar") ? jarPath.getParent().getParent() : null;
+        } catch (Exception e) {
+            fromJarLocation = null;
+        }
+
+        Optional<Path> installDirectory = Arrays.stream(new Path[]{
+                getPath(System.getProperty("mango.paths.home")),
+                getPath(System.getenv("mango_paths_home")),
+                getPath(System.getenv("MA_HOME")),
+                fromJarLocation,
+                getPath(".."), // double click jar file
+                getPath(".") // run java -jar boot/ma-bootstrap.jar from MA_HOME
+        }).filter(Objects::nonNull)
+                .filter(this::isInstallationDirectory)
+                .findFirst();
+
+        installDirectory.orElseThrow(() -> new RuntimeException("Can't find install directory, please set the environment variable 'mango_paths_home'"));
+        return installDirectory.get().toAbsolutePath().normalize();
+    }
+
+    private Path getPath(String path) {
+        return getPath(path, workingDirectory);
+    }
+
+    private Path getPath(String path, Path relativeTo) {
+        if (path == null) return null;
+        try {
+            return relativeTo.resolve(path).normalize();
+        } catch (InvalidPathException e) {
+            return null;
+        }
+    }
+
+    public boolean isInstallationDirectory(Path testMaHome) {
+        return Files.isRegularFile(testMaHome.resolve("release.properties")) || Files.isRegularFile(testMaHome.resolve("release.signed"));
+    }
 }
