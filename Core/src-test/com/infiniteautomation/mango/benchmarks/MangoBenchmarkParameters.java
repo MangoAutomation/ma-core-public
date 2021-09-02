5cf529aa60f947779979ac4a3d24fcf4925e0c42
terrypacker <terrypacker@users.noreply.github.com>
2021-09-01 16:05:31 -1000

    Refactor data point events benchmark example

---
+++ Core/src-test/com/infiniteautomation/mango/benchmarks/MangoBenchmarkParameters.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
+ *
+ *
+ */
+
+package com.infiniteautomation.mango.benchmarks;
+
+public abstract class MangoBenchmarkParameters {
+    //State variables
+    public MockMango mango = new MockMango();
+
+}
--- Core/src-test/com/infiniteautomation/mango/benchmarks/database/DataPointEventsBenchmarks.java
+++ Core/src-test/com/infiniteautomation/mango/benchmarks/database/DataPointEventsBenchmarks.java
@@ -4,38 +4,44 @@
 
 package com.infiniteautomation.mango.benchmarks.database;
 
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.Assert;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
+import com.infiniteautomation.mango.benchmarks.MangoBenchmark;
+import com.infiniteautomation.mango.benchmarks.MangoBenchmarkParameters;
 import com.infiniteautomation.mango.spring.components.RunAs;
 import com.infiniteautomation.mango.spring.service.DataPointService;
-import com.infiniteautomation.mango.spring.service.DataSourceService;
+import com.infiniteautomation.mango.spring.service.EventInstanceService;
 import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.MangoTestBase;
 import com.serotonin.m2m2.db.dao.EventDetectorDao;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
+import com.serotonin.m2m2.rt.EventManagerImpl;
 import com.serotonin.m2m2.rt.event.AlarmLevels;
 import com.serotonin.m2m2.rt.event.detectors.PointEventDetectorRT;
 import com.serotonin.m2m2.rt.event.type.DataPointEventType;
@@ -50,41 +56,95 @@ public class DataPointEventsBenchmarks {
     public static final String DATA_POINT = "dataPoint";
     public static final String LITERAL = "literal";
     public static final String BENCHMARK_DATA_POINT_EVENT_FOR = "Benchmark data point event for  ";
-    private MangoBenchmark base;
-
-    private DataSourceService dataSourceService;
-
-
-    public DataPointEventsBenchmarks() {
-        this.base = new MangoBenchmark();
-    }
-
-    @Test
-    public void runBenchmark() throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(this.getClass().getName())
-                .build();
-        new Runner(opt).run();
-    }
 
     @State(Scope.Benchmark)
-    public static class BenchmarkParams {
-        //@Param({ "1000", "10000", "100000" }) fails in my computer
-        @Param({"50", "100", "200"})
-        public int dataPointCount;
+    public static class BenchmarkParams extends MangoBenchmarkParameters {
+
+        @Param({"1", "100", "1000"})
+        public int dataPoints;
 
         @Param({"1"})
-        public int eventsPerDataPoint;
+        public int tagsPerPoint;
 
         @Param({"1"})
-        public int limit;
+        public int detectorsPerPoint;
 
+        @Param({"1"})
+        public int eventsPerEventDetector;
+
+        @Param({"false"})
+        public boolean loadReadPermission;
+
+        public EventInstanceService eventInstanceService;
+        public Map<String, String> tags = new HashMap<>();
+    }
+
+    @Setup(Level.Trial)
+    public void setupTrial(BenchmarkParams params) {
+        params.mango.setEventManager(new EventManagerImpl());
+        super.setupTrial(params);
+        params.eventInstanceService = Common.getBean(EventInstanceService.class);
+    }
+
+    @Setup(Level.Iteration)
+    public void setupIteration(BenchmarkParams params) {
+        DataPointService service = Common.getBean(DataPointService.class);
+        RunAs runAs = Common.getBean(RunAs.class);
+        AtomicInteger eventCount = new AtomicInteger();
+
+        runAs.runAs(runAs.systemSuperadmin(), () -> {
+            List<IDataPoint> points = null;
+            try {
+                for(int i=0; i<params.tagsPerPoint; i++) {
+                    params.tags.put("tag-" + i, UUID.randomUUID().toString());
+                }
+                points = params.mango.createMockDataPointsWithDetectors(params.dataPoints, params.tags, params.eventsPerEventDetector);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                fail(e.getMessage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                fail(e.getMessage());
+            }
+
+            for (IDataPoint point : points) {
+                List<AbstractPointEventDetectorVO> detectors = EventDetectorDao.getInstance().getWithSource(point.getId(), (DataPointVO) point);
+                if (detectors.size() > 0) {
+                    DataPointEventType type = new DataPointEventType((DataPointVO) point, detectors.get(0));
+                    HashMap context = new HashMap();
+                    context.put(PointEventDetectorRT.EVENT_DETECTOR_CONTEXT_KEY, detectors.get(0));
+                    context.put(PointEventDetectorRT.DATA_POINT_CONTEXT_KEY, point);
+
+                    for (int i = 0; i < params.eventsPerEventDetector; i++) {
+                        Common.eventManager.raiseEvent(type,
+                                Common.timer.currentTimeMillis(),
+                                false, AlarmLevels.INFORMATION,
+                                new TranslatableMessage(LITERAL, BENCHMARK_DATA_POINT_EVENT_FOR + point.getName()),
+                                context);
+
+                        eventCount.getAndIncrement();
+                    }
+                }
+            }
+        });
+    }
+
+    @TearDown(Level.Iteration)
+    public void tearDownIteration(BenchmarkParams params) {
+        super.tearDownIteration(params);
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDownTrial(BenchmarkParams params) {
+        super.tearDownTrial(params);
     }
 
     @Benchmark
     @Threads(1)
-    @Fork(value = 0, warmups = 0)
-    @BenchmarkMode(Mode.AverageTime)
+    @Fork(1)
+    @BenchmarkMode({Mode.SampleTime})
+    @Measurement(iterations = 1, batchSize = 5)
+    @Warmup(iterations = 0)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void insertSpeed(DataPointEventsBenchmarks.BenchmarkParams params) {
         DataPointService service = Common.getBean(DataPointService.class);
@@ -117,7 +177,10 @@ public class DataPointEventsBenchmarks {
                     }
                 }, params.limit, 0);
             }
+            count.getAndIncrement();
+            blackhole.consume(evt);
         });
+        Assert.assertEquals(params.dataPoints, count.get());
     }
 
     @BeforeClass
