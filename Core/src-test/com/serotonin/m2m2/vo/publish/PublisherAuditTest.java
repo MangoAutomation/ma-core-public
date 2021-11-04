eb2dbb0e3ddafc4e3229f0f6fc685f0489e0002f
MertCingoz <mertcingoz@gmail.com>
2021-11-04 15:11:09 +0300

    RAD-1352 Add more test for published points

--- Core/src-test/com/serotonin/m2m2/vo/publish/PublisherAuditTest.java
+++ Core/src-test/com/serotonin/m2m2/vo/publish/PublisherAuditTest.java
@@ -10,25 +10,21 @@ import java.util.List;
 import org.jooq.Condition;
 import org.jooq.impl.DSL;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.infiniteautomation.mango.db.query.ConditionSortLimit;
 import com.infiniteautomation.mango.db.tables.Audit;
 import com.infiniteautomation.mango.spring.service.AuditEventService;
 import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.DataTypes;
 import com.serotonin.m2m2.MangoTestBase;
 import com.serotonin.m2m2.MockEventManager;
 import com.serotonin.m2m2.MockMangoLifecycle;
+import com.serotonin.m2m2.db.dao.PublishedPointDao;
 import com.serotonin.m2m2.db.dao.PublisherDao;
 import com.serotonin.m2m2.rt.event.type.AuditEventType;
-import com.serotonin.m2m2.vo.DataPointVO;
-import com.serotonin.m2m2.vo.dataPoint.MockPointLocatorVO;
-import com.serotonin.m2m2.vo.dataSource.mock.MockDataSourceVO;
+import com.serotonin.m2m2.vo.IDataPoint;
 import com.serotonin.m2m2.vo.event.audit.AuditEventInstanceVO;
 import com.serotonin.m2m2.vo.publish.mock.MockPublishedPointVO;
-import com.serotonin.m2m2.vo.publish.mock.MockPublisherDefinition;
 import com.serotonin.m2m2.vo.publish.mock.MockPublisherVO;
 
 /**
@@ -37,73 +33,66 @@ import com.serotonin.m2m2.vo.publish.mock.MockPublisherVO;
  */
 public class PublisherAuditTest extends MangoTestBase {
 
-    private int pointCount = 100;
-
     public PublisherAuditTest() {
-    }
-
-    @BeforeClass
-    public static void addDefinitions() {
-        addModule("mockPublisher", new MockPublisherDefinition());
     }
 
     @Test
     public void testPublisherAuditTrail() {
-
         PublisherDao dao = PublisherDao.getInstance();
 
-        MockPublisherVO vo = new MockPublisherVO();
-        vo.setName("Name");
-        vo.setDefinition(new MockPublisherDefinition());
-        vo.setXid("PUB_TEST1");
+        MockPublisherVO vo = createMockPublisher(false);
+
+        //Update and delete
+        vo.setName("New Name");
+        dao.update(vo.getId(), vo);
+        dao.delete(vo.getId());
+
+        List<AuditEventInstanceVO> auditEvents = getAuditEvents(AuditEventType.TYPE_PUBLISHER, vo.getId());
+        Assert.assertEquals(3, auditEvents.size());
+    }
+
+    @Test
+    public void testPublishedPointAuditTrail() {
+        PublishedPointDao dao = PublishedPointDao.getInstance();
+
+        IDataPoint dp = createMockDataPoints(1).get(0);
+        MockPublisherVO publisher = createMockPublisher(false);
+        MockPublishedPointVO vo = publisher.getDefinition().createPublishedPointVO(publisher, dp);
+        vo.setName(dp.getName());
         vo.setEnabled(false);
         dao.insert(vo);
 
-        //Modify and re-save
+        //Update and delete
         vo.setName("New Name");
         dao.update(vo.getId(), vo);
+        dao.delete(vo.getId());
+
+        List<AuditEventInstanceVO> auditEvents = getAuditEvents(AuditEventType.TYPE_PUBLISHED_POINT, vo.getId());
+        Assert.assertEquals(3, auditEvents.size());
+    }
+
+    protected List<AuditEventInstanceVO> getAuditEvents(String typeName, int objectId) {
+        Audit auditTable = Audit.AUDIT;
+        Condition conditions = DSL.and(auditTable.typeName.eq(typeName), auditTable.objectId.eq(objectId));
+        ConditionSortLimit c = new ConditionSortLimit(conditions, Collections.singletonList(auditTable.id.asc()), null, null);
+
+        List<AuditEventInstanceVO> events = new ArrayList<>();
+        AuditEventService service = Common.getBean(AuditEventService.class);
 
         //Since these are raised in the background processing thread they may have not yet fired
         int retries = 4;
-        List<AuditEventInstanceVO> events = new ArrayList<>();
         while(retries > 0) {
-            AuditEventService service = Common.getBean(AuditEventService.class);
-            Audit auditTable = Audit.AUDIT;
-            Condition conditions = DSL.and(auditTable.typeName.eq(AuditEventType.TYPE_PUBLISHER), auditTable.objectId.eq(vo.getId()));
-            ConditionSortLimit c = new ConditionSortLimit(conditions, Collections.singletonList(auditTable.id.asc()), null, null);
-            service.customizedQuery(
-                    c,
-                    (evt) -> {
-                        events.add(evt);
-                    });
-            if (events.size() == 2) {
+            events.clear();
+            service.customizedQuery(c, events::add);
+            if (events.size() == 3) {
                 break;
-            } else {
-                events.clear();
             }
             retries--;
             try {
                 Thread.sleep(500);
-            } catch (InterruptedException e) {
-            }
+            } catch (InterruptedException ignored) {}
         }
-
-        Assert.assertEquals(2, events.size());
-    }
-
-    protected List<MockPublishedPointVO> createPoints(){
-        List<MockPublishedPointVO> points = new ArrayList<MockPublishedPointVO>(pointCount);
-
-        //Create data source
-        MockDataSourceVO ds = createMockDataSource();
-
-        for(int i=1; i<pointCount + 1; i++) {
-            DataPointVO dp = createMockDataPoint(ds, new MockPointLocatorVO(DataTypes.NUMERIC, true));
-            MockPublishedPointVO vo = new MockPublishedPointVO();
-            vo.setDataPointId(dp.getId());
-            points.add(vo);
-        }
-        return points;
+        return events;
     }
 
     @Override
