857885e965596c6d1401e541982f5e517792d3e5
terrypacker <terrypacker@users.noreply.github.com>
2021-11-16 12:36:39 -1000

    Fix DataPointRT set point value bugs
    * Fix bug in runtime where Update type data point events were not reliably fired
    * Fix bug in runtime where backdated values would be set as the current value of the point
    
    As per RAD-1495

--- Core/src/com/serotonin/m2m2/rt/dataImage/DataPointRT.java
+++ Core/src/com/serotonin/m2m2/rt/dataImage/DataPointRT.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.infiniteautomation.mango.pointvaluecache.PointValueCache;
 import com.infiniteautomation.mango.statistics.AnalogStatistics;
 import com.infiniteautomation.mango.statistics.StartsAndRuntime;
 import com.infiniteautomation.mango.statistics.StartsAndRuntimeList;
@@ -22,7 +23,6 @@ import com.serotonin.ShouldNeverHappenException;
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.DataTypes;
 import com.serotonin.m2m2.db.dao.DataPointDao;
-import com.infiniteautomation.mango.pointvaluecache.PointValueCache;
 import com.serotonin.m2m2.db.dao.PointValueDao;
 import com.serotonin.m2m2.db.dao.SystemSettingsDao;
 import com.serotonin.m2m2.rt.DataPointEventNotifyWorkItem;
@@ -273,7 +273,8 @@ public class DataPointRT implements IDataPointValueSource, ILifecycle {
             return;
         }
 
-        boolean backdated = pointValue.get() != null && newValue.getTime() < pointValue.get().getTime();
+        PointValueTime oldValue = pointValue.get();
+        boolean backdated = oldValue != null && newValue.getTime() < oldValue.getTime();
 
         // Determine whether the new value qualifies for logging.
         boolean logValue;
@@ -282,7 +283,7 @@ public class DataPointRT implements IDataPointValueSource, ILifecycle {
         switch (vo.getLoggingType()) {
             case LoggingTypes.ON_CHANGE_INTERVAL:
             case LoggingTypes.ON_CHANGE:
-                if (pointValue.get() == null) {
+                if (oldValue == null) {
                     logValue = true;
                     if(newValue.getValue() instanceof NumericValue) {
                         //Set the tolerance origin so the next value has
@@ -309,9 +310,9 @@ public class DataPointRT implements IDataPointValueSource, ILifecycle {
                         else
                             logValue = false;
                     } else if(newValue.getValue() instanceof ImageValue) {
-                        logValue = !((ImageValue)newValue.getValue()).equalDigests(((ImageValue)pointValue.get().getValue()).getDigest());
+                        logValue = !((ImageValue)newValue.getValue()).equalDigests(((ImageValue)oldValue.getValue()).getDigest());
                     } else
-                        logValue = !Objects.equals(newValue.getValue(), pointValue.get().getValue());
+                        logValue = !Objects.equals(newValue.getValue(), oldValue.getValue());
                 }
 
                 saveValue = logValue;
@@ -320,13 +321,13 @@ public class DataPointRT implements IDataPointValueSource, ILifecycle {
                 logValue = true;
                 break;
             case LoggingTypes.ON_TS_CHANGE:
-                if (pointValue.get() == null)
+                if (oldValue == null)
                     logValue = true;
                 else if (backdated)
                     // Backdated. Ignore it
                     logValue = false;
                 else
-                    logValue = newValue.getTime() != pointValue.get().getTime();
+                    logValue = newValue.getTime() != oldValue.getTime();
 
                 saveValue = logValue;
                 break;
@@ -353,9 +354,10 @@ public class DataPointRT implements IDataPointValueSource, ILifecycle {
                     newValue.getTime(), source.getSetPointSourceMessage());
         }
 
-        PointValueTime oldValue = pointValue.get();
-        pointValue.set(newValue);
-        fireEvents(oldValue, newValue, null, source != null, backdated, logValue, saveValue, false);
+        if(!backdated) {
+            pointValue.set(newValue);
+        }
+        fireEvents(oldValue, newValue, null, source != null, backdated, logValue, !backdated, false);
 
     }
 
