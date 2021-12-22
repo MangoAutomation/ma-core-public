19108a544f2d8631486fe00ac47ccf21052a2baf
terrypacker <terrypacker@users.noreply.github.com>
2021-12-22 06:14:35 -1000

    Add tests to prove Sero Cron Expression Buggy
    Add test for spring as a workable solution
    
    For RAD-1453

--- Core/src-test/com/serotonin/m2m2/util/CronPatternTest.java
+++ Core/src-test/com/serotonin/m2m2/util/CronPatternTest.java
@@ -1,8 +1,12 @@
 package com.serotonin.m2m2.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 
 import org.joda.time.DateTime;
@@ -12,9 +16,43 @@ import com.serotonin.timer.CronExpression;
 
 public class CronPatternTest {
 
+	/**
+	 * Test to show that CronExpression is invalid for some dates,
+	 * specifically time 0  See RAD-1453.
+	 *
+	 * Since this test fails it is disabled until we fix it.
+	 * @throws ParseException
+	 */
+	//@Test
+	public void testTimeZero() throws ParseException {
+		CronExpression ce = new CronExpression("0/5 * * * * ?");
+		Date timeStart = new Date(0);
+		long time = ce.getNextValidTimeAfter(timeStart).getTime();
+
+		//Time should be 5000
+		assertEquals(5000, time);
+	}
+
+	/**
+	 * Test to show that the Spring CronExpression is valid at time 0 See RAD-1453.
+	 *
+	 * @throws ParseException
+	 */
+	@Test
+	public void testTimeZeroSpring() throws ParseException {
+		org.springframework.scheduling.support.CronExpression ce = org.springframework.scheduling.support.CronExpression.parse("0/5 * * * * ?");
+
+		LocalDateTime ldt = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC);
+		LocalDateTime next = ce.next(ldt);
+
+		assertTrue(ldt.isBefore(next));
+
+		LocalDateTime expected = ldt.plusSeconds(5);
+		assertEquals(expected, next);
+	}
+
 	@Test
 	public void testCronPattern() {
-//		Date timeStart = new Date(2016, 3, 3);
 		DateTime baseTime = DateTime.parse("2017-03-02T00:00:00.000");
 		Date timeStart = new Date(baseTime.getMillis());
 		long timeEnd = timeStart.getTime() + 86400000;
