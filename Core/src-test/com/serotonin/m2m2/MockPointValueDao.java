c835f5e33f2e3bf408bb74f8d4bbfe4dfe70f35e
Jared Wiltshire <jazdw@users.noreply.github.com>
2022-01-19 16:40:20 -0700

    Fix up MockPointValueDao and implement test

--- Core/src-test/com/serotonin/m2m2/MockPointValueDao.java
+++ Core/src-test/com/serotonin/m2m2/MockPointValueDao.java
@@ -3,43 +3,39 @@
  */
 package com.serotonin.m2m2;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import com.infiniteautomation.mango.db.query.WideCallback;
 import com.serotonin.m2m2.db.dao.PointValueDao;
 import com.serotonin.m2m2.rt.dataImage.IdPointValueTime;
 import com.serotonin.m2m2.rt.dataImage.PointValueTime;
 import com.serotonin.m2m2.vo.DataPointVO;
 
 /**
- *
  * @author Terry Packer
+ * @author Jared Wiltshire
  */
-public class MockPointValueDao implements PointValueDao{
+public class MockPointValueDao implements PointValueDao {
 
-    protected Map<Integer, List<PointValueTime>> data = new ConcurrentHashMap<Integer, List<PointValueTime>>();
+    protected Map<Integer, NavigableMap<Long, PointValueTime>> data = new ConcurrentHashMap<>();
 
-    public Map<Integer, List<PointValueTime>> getData() {
-        return this.data;
+    private NavigableMap<Long, PointValueTime> newSeries(int seriesId) {
+        return Collections.synchronizedNavigableMap(new TreeMap<>());
     }
 
     @Override
     public PointValueTime savePointValueSync(DataPointVO vo, PointValueTime pointValue) {
-        List<PointValueTime> pvts = data.computeIfAbsent(vo.getId(), k -> new ArrayList<>());
-        pvts.add(pointValue);
-
-        //Keep in time order?
-        Collections.sort(pvts);
-
+        NavigableMap<Long, PointValueTime> values = data.computeIfAbsent(vo.getSeriesId(), this::newSeries);
+        values.put(pointValue.getTime(), pointValue);
         return pointValue;
     }
 
@@ -49,173 +45,119 @@ public class MockPointValueDao implements PointValueDao{
     }
 
     @Override
-    public List<PointValueTime> getPointValues(DataPointVO vo, long from) {
-        List<PointValueTime> pvts = new ArrayList<>();
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(PointValueTime pvt : existing) {
-                if(pvt.getTime() >= from)
-                    pvts.add(pvt);
+    public void getPointValuesPerPoint(Collection<? extends DataPointVO> vos,
+                                       @Nullable Long from,
+                                       @Nullable Long to,
+                                       @Nullable Integer limit,
+                                       TimeOrder sortOrder,
+                                       Consumer<? super IdPointValueTime> callback) {
+
+        PointValueDao.validateNotNull(vos);
+        PointValueDao.validateNotNull(sortOrder);
+        PointValueDao.validateNotNull(callback);
+        PointValueDao.validateTimePeriod(from, to);
+        PointValueDao.validateLimit(limit);
+
+        for (var vo : vos) {
+            var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+            //noinspection SynchronizationOnLocalVariableOrMethodParameter
+            synchronized (values) {
+                var filteredValues = filterValues(values, from, to);
+
+                if (sortOrder == TimeOrder.DESCENDING) {
+                    filteredValues = filteredValues.descendingMap();
+                }
+
+                var stream = filteredValues.values().stream();
+                if (limit != null) {
+                    stream = stream.limit(limit);
+                }
+                stream.map(v -> new IdPointValueTime(vo.getSeriesId(), v.getValue(), v.getTime()))
+                        .forEach(callback);
             }
         }
-        return pvts;
     }
 
     @Override
-    public List<PointValueTime> getPointValuesBetween(DataPointVO vo, long from, long to) {
-        List<PointValueTime> pvts = new ArrayList<>();
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(PointValueTime pvt : existing) {
-                if(pvt.getTime() >= from && pvt.getTime() < to)
-                    pvts.add(pvt);
-            }
-        }
-        return pvts;
-    }
+    public Optional<Long> deletePointValuesBetween(DataPointVO vo,
+                                                   @Nullable Long startTime,
+                                                   @Nullable Long endTime) {
 
-    @Override
-    public List<PointValueTime> getLatestPointValues(DataPointVO vo, int limit) {
-        List<PointValueTime> pvts = new ArrayList<>();
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(PointValueTime pvt : existing) {
-                pvts.add(pvt);
-                if(pvts.size() >= limit)
-                    break;
-            }
-        }
-        return pvts;
-    }
+        PointValueDao.validateNotNull(vo);
+        PointValueDao.validateTimePeriod(startTime, endTime);
 
-    @Override
-    public List<PointValueTime> getLatestPointValues(DataPointVO vo, long to, int limit) {
-        List<PointValueTime> pvts = new ArrayList<>();
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(int i=existing.size() -1; i>=0; i--) {
-                PointValueTime pvt = existing.get(i);
-                if(pvt.getTime() < to)
-                    pvts.add(pvt);
-                if(pvts.size() >= limit)
-                    break;
-            }
-        }
-        return pvts;
-    }
-
-    @Override
-    public Optional<PointValueTime> getLatestPointValue(DataPointVO vo) {
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            return Optional.of(existing.get(existing.size() -1));
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<PointValueTime> getPointValueBefore(DataPointVO vo, long time) {
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(int i=existing.size() -1; i>=0; i--) {
-                PointValueTime pvt = existing.get(i);
-                if(pvt.getTime() < time)
-                    return Optional.of(pvt);
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<PointValueTime> getPointValueAfter(DataPointVO vo, long time) {
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(PointValueTime pvt : existing) {
-                if(pvt.getTime() > time)
-                    return Optional.of(pvt);
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<PointValueTime> getPointValueAt(DataPointVO vo, long time) {
-        List<PointValueTime> existing = data.get(vo.getId());
-        if(existing != null) {
-            for(PointValueTime pvt : existing) {
-                if(pvt.getTime() == time)
-                    return Optional.of(pvt);
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public void getPointValuesBetween(DataPointVO vo, long from, long to,
-                                      Consumer<? super PointValueTime> callback) {
-
-    }
-
-    @Override
-    public void getPointValuesBetween(Collection<? extends DataPointVO> vos, long from, long to,
-                                      Consumer<? super IdPointValueTime> callback) {
-
-    }
-
-    @Override
-    public void wideQuery(DataPointVO vo, long from, long to,
-                          WideCallback<? super PointValueTime> callback) {
-
-    }
-
-    @Override
-    public Optional<Long> deletePointValuesBetween(DataPointVO vo, @Nullable Long startTime, @Nullable Long endTime) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Long> deletePointValuesBefore(DataPointVO vo, long endTime) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Long> deletePointValues(DataPointVO vo) {
-        return Optional.empty();
-    }
-
-
-    @Override
-    public Optional<Long> deleteAllPointData() {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Long> deleteOrphanedPointValues() {
+        var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+        var filteredValues = filterValues(values, startTime, endTime);
+        filteredValues.clear();
         return Optional.empty();
     }
 
     @Override
     public long dateRangeCount(DataPointVO vo, @Nullable Long from, @Nullable Long to) {
-        return 0;
+        PointValueDao.validateNotNull(vo);
+        PointValueDao.validateTimePeriod(from, to);
+
+        var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+        var filteredValues = filterValues(values, from, to);
+        return filteredValues.size();
     }
 
     @Override
     public Optional<Long> getInceptionDate(DataPointVO vo) {
-        return Optional.empty();
+        PointValueDao.validateNotNull(vo);
+
+        var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+        var entry = values.pollFirstEntry();
+        return Optional.ofNullable(entry).map(Entry::getKey);
     }
 
     @Override
     public Optional<Long> getStartTime(Collection<? extends DataPointVO> vos) {
-        return Optional.empty();
+        PointValueDao.validateNotNull(vos);
+
+        Long startTime = null;
+
+        for (var vo : vos) {
+            var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+            var value = values.pollFirstEntry();
+            if (value != null) {
+                startTime = startTime == null ? value.getKey() : Math.min(startTime, value.getKey());
+            }
+        }
+
+        return Optional.ofNullable(startTime);
     }
 
     @Override
     public Optional<Long> getEndTime(Collection<? extends DataPointVO> vos) {
-        return Optional.empty();
+        PointValueDao.validateNotNull(vos);
+
+        Long endTime = null;
+
+        for (var vo : vos) {
+            var values = data.getOrDefault(vo.getSeriesId(), Collections.emptyNavigableMap());
+            var value = values.pollLastEntry();
+            if (value != null) {
+                endTime = endTime == null ? value.getKey() : Math.max(endTime, value.getKey());
+            }
+        }
+
+        return Optional.ofNullable(endTime);
+    }
+
+    private NavigableMap<Long, PointValueTime> filterValues(NavigableMap<Long, PointValueTime> values, Long from, Long to) {
+        if (from != null && to != null) {
+            return values.subMap(from, true, to, false);
+        } else if (from != null) {
+            return values.tailMap(from, true);
+        } else if (to != null) {
+            return values.headMap(to, false);
+        }
+        return values;
     }
 
     @Override
-    public Optional<StartAndEndTime> getStartAndEndTime(Collection<? extends DataPointVO> vos) {
+    public Optional<Long> deleteOrphanedPointValues() {
         return Optional.empty();
     }
 
@@ -234,28 +176,4 @@ public class MockPointValueDao implements PointValueDao{
         return 0;
     }
 
-    @Override
-    public Optional<Long> deletePointValue(DataPointVO vo, long ts) {
-        return Optional.empty();
-    }
-
-    @Override
-    public void getPointValuesPerPoint(Collection<? extends DataPointVO> vos, @Nullable Long from, @Nullable Long to, @Nullable Integer limit, TimeOrder sortOrder, Consumer<? super IdPointValueTime> callback) {
-
-    }
-
-    @Override
-    public void getPointValuesCombined(Collection<? extends DataPointVO> vos, @Nullable Long from, @Nullable Long to, @Nullable Integer limit, TimeOrder sortOrder, Consumer<? super IdPointValueTime> callback) {
-
-    }
-
-    @Override
-    public void wideBookendQueryPerPoint(Collection<? extends DataPointVO> vos, long from, long to, @Nullable Integer limit, WideCallback<? super IdPointValueTime> callback) {
-
-    }
-
-    @Override
-    public void wideBookendQueryCombined(Collection<? extends DataPointVO> vos, long from, long to, @Nullable Integer limit, WideCallback<? super IdPointValueTime> callback) {
-
-    }
 }
---
+++ Core/src-test/com/serotonin/m2m2/dao/MockPointValueDaoTest.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
+ */
+package com.serotonin.m2m2.dao;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.Bean;
+
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.MangoTestBase;
+import com.serotonin.m2m2.MockMangoLifecycle;
+import com.serotonin.m2m2.MockPointValueDao;
+import com.serotonin.m2m2.db.dao.PointValueDao;
+import com.serotonin.m2m2.db.dao.PointValueDao.TimeOrder;
+import com.serotonin.m2m2.vo.DataPointVO;
+import com.serotonin.m2m2.vo.IDataPoint;
+
+/**
+ * @author Jared Wiltshire
+ */
+public class MockPointValueDaoTest extends MangoTestBase {
+
+    protected NumericPointValueDaoTestHelper helper;
+
+    @Override
+    protected MockMangoLifecycle getLifecycle() {
+        MockMangoLifecycle lifecycle = super.getLifecycle();
+        lifecycle.addRuntimeContextConfiguration(MockPointValueDaoSpringConfig.class);
+        return lifecycle;
+    }
+
+    private static class MockPointValueDaoSpringConfig {
+        @Bean
+        public PointValueDao pointValueDao() {
+            return new MockPointValueDao();
+        }
+    }
+
+    @Override
+    @Before
+    public void before() {
+        super.before();
+        List<IDataPoint> vos = createMockDataPoints(3);
+        this.helper = new NumericPointValueDaoTestHelper((DataPointVO)vos.get(0), (DataPointVO)vos.get(1), (DataPointVO)vos.get(2), Common.getBean(PointValueDao.class));
+        this.helper.before();
+    }
+
+    @Override
+    @After
+    public void after() {
+        this.helper.after();
+    }
+
+    //Latest Queries
+    @Test
+    public void testLatestExceptionInCallback() {
+        this.helper.testLatestExceptionInCallback();
+    }
+    @Test
+    public void testLatestNoDataInBothSeries() {
+        this.helper.testLatestNoDataInBothSeries();
+    }
+    @Test
+    public void testLatestNoDataInOneSeries() {
+        this.helper.testLatestNoDataInOneSeries();
+    }
+    @Test
+    public void testLatestMultiplePointValuesNoLimit() {
+        this.helper.testLatestMultiplePointValuesNoLimit();
+    }
+    @Test
+    public void testLatestMultiplePointValuesNoLimitOffsetSeries() {
+        this.helper.testLatestMultiplePointValuesNoLimitOffsetSeries();
+    }
+    @Test
+    public void testLatestMultiplePointValuesOrderByIdNoLimit() {
+        this.helper.testLatestMultiplePointValuesOrderByIdNoLimit();
+    }
+    @Test
+    public void testLatestMultiplePointValuesOrderByIdNoLimitOffsetSeries() {
+        this.helper.testLatestMultiplePointValuesOrderByIdNoLimitOffsetSeries();
+    }
+    @Test
+    public void testLatestMultiplePointValuesLimit() {
+        this.helper.testLatestMultiplePointValuesLimit();
+    }
+    @Test
+    public void testLatestMultiplePointValuesLimitOffsetSeries() {
+        this.helper.testLatestMultiplePointValuesLimitOffsetSeries();
+    }
+    @Test
+    public void testLatestMultiplePointValuesOrderByIdLimit() {
+        this.helper.testLatestMultiplePointValuesOrderByIdLimit();
+    }
+    @Test
+    public void testLatestMultiplePointValuesOrderByIdLimitOffsetSeries() {
+        this.helper.testLatestMultiplePointValuesOrderByIdLimitOffsetSeries();
+    }
+
+    //Values Between
+    @Test
+    public void testBetweenExceptionInCallback() {
+        this.helper.testBetweenExceptionInCallback();
+    }
+    @Test
+    public void testBetweenNoDataInBothSeries() {
+        this.helper.testBetweenNoDataInBothSeries();
+    }
+    @Test
+    public void testBetweenNoDataInOneSeries() {
+        this.helper.testBetweenNoDataInOneSeries();
+    }
+    @Test
+    public void testRangeMultiplePointValuesNoLimit() {
+        this.helper.testRangeMultiplePointValuesNoLimit();
+    }
+    @Test
+    public void testRangeMultiplePointValuesNoLimitOffsetSeries() {
+        this.helper.testRangeMultiplePointValuesNoLimitOffsetSeries();
+    }
+    @Test
+    public void testRangeMultiplePointValuesOrderByIdNoLimit() {
+        this.helper.testRangeMultiplePointValuesOrderByIdNoLimit();
+    }
+    @Test
+    public void testRangeMultiplePointValuesOrderByIdNoLimitOffsetSeries() {
+        this.helper.testRangeMultiplePointValuesOrderByIdNoLimitOffsetSeries();
+    }
+    @Test
+    public void testRangeMultiplePointValuesLimit() {
+        this.helper.testRangeMultiplePointValuesLimit();
+    }
+    @Test
+    public void testRangeMultiplePointValuesLimitOffsetSeries() {
+        this.helper.testRangeMultiplePointValuesLimitOffsetSeries();
+    }
+    @Test
+    public void testRangeMultiplePointValuesOrderByIdLimit() {
+        this.helper.testRangeMultiplePointValuesOrderByIdLimit();
+    }
+    @Test
+    public void testRangeMultiplePointValuesOrderByIdLimitOffsetSeries() {
+        this.helper.testRangeMultiplePointValuesOrderByIdLimitOffsetSeries();
+    }
+
+    //Wide
+    @Test
+    public void testWideQueryNoData() {
+        this.helper.testWideQueryNoData();
+    }
+
+    @Test public void testWideQueryNoBefore() {
+        this.helper.testWideQueryNoBefore();
+    }
+
+    @Test public void testWideQuery() {
+        this.helper.testWideQuery();
+    }
+
+    @Test public void testWideQueryNoAfter() {
+        this.helper.testWideQueryNoAfter();
+    }
+
+    //Bookend
+    @Test
+    public void testBookendExceptionInFirstValueCallback() {
+        this.helper.testBookendExceptionInFirstValueCallback();
+    }
+    @Test
+    public void testBookendExceptionInRowCallback() {
+        this.helper.testBookendExceptionInRowCallback();
+    }
+    @Test
+    public void testBookendExceptionInLastValueCallback() {
+        this.helper.testBookendExceptionInLastValueCallback();
+    }
+    @Test
+    public void testBookendNoDataInOneSeries() {
+        this.helper.testBookendNoDataInOneSeries();
+    }
+    @Test
+    public void testBookendNoDataInBothSeries() {
+        this.helper.testBookendNoDataInBothSeries();
+    }
+    @Test
+    public void testBookendEmptySeries() {
+        this.helper.testBookendEmptySeries();
+    }
+    @Test
+    public void testBookendMultiplePointValuesNoLimit() {
+        this.helper.testBookendMultiplePointValuesNoLimit();
+    }
+    @Test
+    public void testBookendMultiplePointValuesNoLimitOffsetSeries() {
+        this.helper.testBookendMultiplePointValuesNoLimitOffsetSeries();
+    }
+    @Test
+    public void testBookendMultiplePointValuesOrderByIdNoLimit() {
+        this.helper.testBookendMultiplePointValuesOrderByIdNoLimit();
+    }
+    @Test
+    public void testBookendMultiplePointValuesOrderByIdNoLimitOffsetSeries() {
+        this.helper.testBookendMultiplePointValuesOrderByIdNoLimitOffsetSeries();
+    }
+    @Test
+    public void testBookendMultiplePointValuesLimit() {
+        this.helper.testBookendMultiplePointValuesLimit();
+    }
+    @Test
+    public void testBookendMultiplePointValuesLimitOffsetSeries() {
+        this.helper.testBookendMultiplePointValuesLimitOffsetSeries();
+    }
+    @Test
+    public void testBookendMultiplePointValuesOrderByIdLimit() {
+        this.helper.testBookendMultiplePointValuesOrderByIdLimit();
+    }
+    @Test
+    public void testBookendMultiplePointValuesOrderByIdLimitOffsetSeries() {
+        this.helper.testBookendMultiplePointValuesOrderByIdLimitOffsetSeries();
+    }
+
+    @Test
+    public void testNoStartBookendOrderByIdLimit(){
+        this.helper.testNoStartBookendOrderByIdLimit();
+    }
+
+    @Test
+    public void testSeries1NoDataSeries2OneSampleOrderById() {
+        this.helper.testSeries1NoDataSeries2OneSampleOrderById();
+    }
+
+    @Test
+    public void testSeries1NoDataSeries2OneSample(){
+        this.helper.testSeries1NoDataSeries2OneSample();
+    }
+
+    @Test
+    public void testNoStartBookendLimit() {
+        this.helper.testNoStartBookendLimit();
+    }
+
+    @Test
+    public void streamPointValues() {
+        this.helper.streamPointValues(TimeOrder.ASCENDING);
+    }
+
+    @Test
+    public void streamPointValuesReverse() {
+        this.helper.streamPointValues(TimeOrder.DESCENDING);
+    }
+
+    @Test
+    public void streamPointValuesPerPoint() {
+        this.helper.streamPointValuesPerPoint(TimeOrder.ASCENDING);
+    }
+
+    @Test
+    public void streamPointValuesPerPointReverse() {
+        this.helper.streamPointValuesPerPoint(TimeOrder.DESCENDING);
+    }
+
+    @Test
+    public void streamPointValuesCombined() {
+        this.helper.streamPointValuesCombined(TimeOrder.ASCENDING);
+    }
+
+    @Test
+    public void streamPointValuesCombinedReverse() {
+        this.helper.streamPointValuesCombined(TimeOrder.DESCENDING);
+    }
+}
