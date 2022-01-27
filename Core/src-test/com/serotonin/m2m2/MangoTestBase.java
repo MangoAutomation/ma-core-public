c0d1955034005953446de2a99b3e0509523876cb
MertCingoz <mertcingoz@gmail.com>
2022-01-28 00:18:36 +0300

    RAD-1671 Add ability to use remote h2 database

--- Core/src-test/com/serotonin/m2m2/MangoTestBase.java
+++ Core/src-test/com/serotonin/m2m2/MangoTestBase.java
@@ -146,11 +146,11 @@ public class MangoTestBase {
                 break;
             case "h2:tcp":
                 testDbType = "h2";
-                properties.setProperty("db.url", "jdbc:h2:tcp://0.0.0.0/" + dbName + ";AUTO_SERVER=TRUE");
+                properties.setProperty("db.url", "jdbc:h2:tcp://0.0.0.0:1521/testDbs/" + dbName + ";MODE=MySQL;AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE");
                 break;
             case "h2:file":
                 testDbType = "h2";
-                properties.setProperty("db.url", "jdbc:h2:file://" + dbName+ ";AUTO_SERVER=TRUE");
+                properties.setProperty("db.url", "jdbc:h2:databases/" + dbName + ";AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE");
                 break;
             case "h2:mem":
             default:
