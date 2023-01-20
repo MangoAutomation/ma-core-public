922f413219aba4c2ef4fae393500b8a4d22262c4
Jared Wiltshire <jazdw@users.noreply.github.com>
2023-01-20 09:38:34 -0700

    Add gRPC support to core
    PI-1630

---
+++ Core/src/com/infiniteautomation/mango/spring/grpc/GrpcServerLifecycle.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 Radix IoT LLC. All rights reserved.
+ */
+
+package com.infiniteautomation.mango.spring.grpc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+/**
+ * @author Jared Wiltshire
+ */
+@Component
+public class GrpcServerLifecycle {
+
+    private final List<BindableService> services;
+    private final int port;
+    private Server server;
+
+    @Autowired
+    public GrpcServerLifecycle(List<BindableService> services, @Value("${grpc.server.port:9090}") int port) {
+        this.services = services;
+        this.port = port;
+    }
+
+    @PostConstruct
+    synchronized void postConstruct() throws IOException {
+        var builder = ServerBuilder.forPort(port);
+        services.forEach(builder::addService);
+        this.server = builder.build().start();
+    }
+
+    @PreDestroy
+    synchronized void preDestroy() throws InterruptedException {
+        if (server != null) {
+            server.shutdown();
+            server.awaitTermination();
+            server = null;
+        }
+    }
+
+}
