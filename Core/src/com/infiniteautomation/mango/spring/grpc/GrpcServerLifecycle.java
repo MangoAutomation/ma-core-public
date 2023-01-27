bac934217be5e005bb10a6b88f4686dbb872b3ee
Jared Wiltshire <jazdw@users.noreply.github.com>
2023-01-27 14:22:19 -0700

    Enable mTLS support for gRPC client/server
    Add properties to enable TLS for gRPC
    PI-1630

--- Core/src/com/infiniteautomation/mango/spring/grpc/GrpcServerLifecycle.java
+++ Core/src/com/infiniteautomation/mango/spring/grpc/GrpcServerLifecycle.java
@@ -5,44 +5,83 @@
 package com.infiniteautomation.mango.spring.grpc;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import com.infiniteautomation.mango.spring.ConditionalOnProperty;
+import com.serotonin.m2m2.Common;
+
 import io.grpc.BindableService;
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.ServerCredentials;
 import io.grpc.ServerInterceptor;
+import io.grpc.TlsServerCredentials;
+import io.grpc.TlsServerCredentials.ClientAuth;
 import io.grpc.util.TransmitStatusRuntimeExceptionInterceptor;
 
 /**
  * @author Jared Wiltshire
  */
 @Component
+@ConditionalOnProperty("${grpc.server.enabled}")
 public class GrpcServerLifecycle {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final List<BindableService> services;
     private final List<ServerInterceptor> interceptors;
     private final int port;
+    private final @Nullable Path certChain;
+    private final @Nullable Path privateKey;
+    private final @Nullable Path rootCerts;
+    private final ClientAuth clientAuth;
     private Server server;
 
     @Autowired
     public GrpcServerLifecycle(List<BindableService> services,
                                List<ServerInterceptor> interceptors,
-                               @Value("${grpc.server.port:9090}") int port) {
+                               @Value("${grpc.server.port}") int port,
+                               @Value("${grpc.server.certChain:#{null}}") @Nullable Path certChain,
+                               @Value("${grpc.server.privateKey:#{null}}") @Nullable Path privateKey,
+                               @Value("${grpc.server.rootCerts:#{null}}") @Nullable Path rootCerts,
+                               @Value("${grpc.server.clientAuth}") ClientAuth clientAuth) {
         this.services = services;
         this.interceptors = interceptors;
         this.port = port;
+        this.certChain = certChain == null ? null : Common.MA_DATA_PATH.resolve(certChain).normalize();
+        this.privateKey = privateKey == null ? null : Common.MA_DATA_PATH.resolve(privateKey).normalize();
+        this.rootCerts = rootCerts == null ? null : Common.MA_DATA_PATH.resolve(rootCerts).normalize();
+        this.clientAuth = clientAuth;
     }
 
     @PostConstruct
     synchronized void postConstruct() throws IOException {
-        var builder = ServerBuilder.forPort(port);
+        ServerCredentials serverCredentials;
+        if (certChain != null && privateKey != null) {
+            var tlsBuilder = TlsServerCredentials.newBuilder()
+                    .keyManager(certChain.toFile(), privateKey.toFile())
+                    .clientAuth(clientAuth);
+            if (rootCerts != null) {
+                tlsBuilder.trustManager(rootCerts.toFile());
+            }
+            serverCredentials = tlsBuilder.build();
+        } else {
+            log.warn("gRPC server is running without TLS! " +
+                    "You must supply a certificate chain and private key file to enable TLS.");
+            serverCredentials = InsecureServerCredentials.create();
+        }
+        var builder = Grpc.newServerBuilderForPort(port, serverCredentials);
         builder.intercept(TransmitStatusRuntimeExceptionInterceptor.instance());
         interceptors.forEach(builder::intercept);
         services.forEach(builder::addService);
