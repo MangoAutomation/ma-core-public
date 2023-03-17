415c77bee474c19d5a926cec866d83662dbcb0eb
Pier Puccini <pierpuccini@users.noreply.github.com>
2023-03-17 17:16:15 -0500

    #RAD-2271 added Better comenting around what the bug was and how this test exists to prevent it.

--- Core/src-test/com/serotonin/m2m2/db/dao/systemsettingsaccessor/SystemSettingsAccessorTest.java
+++ Core/src-test/com/serotonin/m2m2/db/dao/systemsettingsaccessor/SystemSettingsAccessorTest.java
@@ -15,12 +15,19 @@ import com.serotonin.m2m2.MangoTestBase;
 import com.serotonin.m2m2.db.dao.SystemSettingsDao;
 
 public class SystemSettingsAccessorTest extends MangoTestBase {
+    /**
+     * Here we load our schema definition which would represent a New module
+     */
     @BeforeClass
     public static void setupModule() {
         addModule("SysAccessorTestModule", new SystemSettingsAccessorSchemaDefinitionTest());
         loadModules();
     }
 
+    /**
+     * This test has been made because there was a bug where new modules getting installed for the first time wouldn't get added to the system settings table since the set method only has a create and not and update. A solution has been implemented to allow upserting (updating/creating) and this test is what confirms this bug is no longer around.
+     * So Once the testing module/SchemaDefinition has been loaded we need to check that the module has been saved to the system settings table with the first version
+     */
     @Test
     public void testInsertNewModules () {
         SystemSettingsDao dao =  Common.getBean(SystemSettingsDao.class);
